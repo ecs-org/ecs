5b62259385a121128d00d9fffd567bb3ee0c0a6a
rmt <rmt@localhost.localdomain>
2009-12-14 20:43:06 +0100

    Add FIXME comment regarding the format of settings.FEEDBACK_MODELS

--- mockup/ecs/feedback/utils.py
+++ mockup/ecs/feedback/utils.py
@@ -8,7 +8,16 @@ def lookup_model(model):
     For a model string or class object, return the model class
     with the provided primary key.
     """
+    # FIXME: we cannot import models directly in settings,
+    # so we should contain a modulename:class mapping in
+    # settings.FEEDBACK_MODELS, and we should look that up
+    # here
     if isinstance(model, basestring):
         models = getattr(settings, 'FEEDBACK_MODELS', {})
         model = models.get(model, None)
     return model
+
+def new_comment(request):
+    if request.method != 'POST':
+        return
+
