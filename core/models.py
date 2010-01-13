03127951140671d0a77988150583465b542b93a2
yacc <yacc@ep3.at>
2010-01-13 18:57:48 +0100

    discussion model added.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,88 @@
-from django.db import models
+# -*- coding: utf-8 -*- 
 
-# Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# metaclassen:
+#  master control programm model:
+#   date, time, user, uuid,  
+class Workflow(models.Model):
+    pass
+
+class Document(models.Model):
+    #  Verkettete Liste für verschiedene Versionen
+    uuid_doc_instanz
+    uuid_semantische_instanz
+    SELECT * FROM documents WHERE semantik=123;
+    pass
+
+class SubmissionForm(models.Model):
+     Name, Type, Bla, Blu  = Daten des Einreich formulares
+    pass
+
+class NotificationForm(models.Model):
+    pass
+
+class Checklist(models.Model):
+    pass
+
+class SubmissionSet(models.Model):
+    documents = models.ManyToManyField(Document)
+    submissionform = models.ForeignKey(SubmissionForm)
+
+class NotificationSet(models.Model):
+    documents = models.ManyToManyField(Document)
+    notificationform = models.ForeignKey(NotificationForm)
+
+class VoteReview(models.Model):   
+    workflow = models.ForeignKey(Workflow)
+
+class Vote(models.Model):
+    votereview = models.ForeignKey(VoteReview)
+    submissionset = models.ForeignKey(SubmissionSet)
+    checklists = models.ManyToManyField(Checklist)
+    workflow = models.ForeignKey(Workflow)
+
+class SubmissionReview(models.Model):   
+    workflow = models.ForeignKey(Workflow)
+
+class Submission(models.Model):
+    submissionset = models.ForeignKey(SubmissionSet)
+    submissionreview = models.ForeignKey(SubmissionReview)
+    vote = models.ForeignKey(Vote)
+    workflow = models.ForeignKey(Workflow)
+
+
+class NotificationAnswer(models.Model):
+    workflow = models.ForeignKey(Workflow)
+ 
+class Notification(models.Model):
+    submission = models.ForeignKey(Submission)
+    notificationset = models.ForeignKey(NotificationSet)
+    answer = models.ForeignKey(NotificationAnswer)
+    workflow = models.ForeignKey(Workflow)
+
+class Meeting(models.Model):
+    submissions = models.ManyToManyField(Submission)
+"""
+class Revision(models.Model):
+    pass
+
+class Message(models.Model):
+    pass
+
+class AuditLog(models.Model):
+    pass
+
+
+class User(models.Model):
+    pass
+
+class Reviewer(models.Model):
+    pass
+
+class Annotation(models.Model):
+    pass
+"""
+
