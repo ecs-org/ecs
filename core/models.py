57db652576a5993353c2f8c3f9677304be03b703
yacc <yacc@ep3.at>
2010-01-14 08:50:23 +0100

    added Commission table with manual many-to-many mapping, because we need to know which one is the main one for each submission form.

--- core/models.py
+++ core/models.py
@@ -11,16 +11,30 @@ class Workflow(models.Model):
     pass
 
 class Document(models.Model):
-    uuid_document = model.SlugField(max_length=32)
+    uuid_document = models.SlugField(max_length=32)
     # file path is derived from the uuid_document_revision
-    uuid_document_revision = model.SlugField(max_length=32)
+    uuid_document_revision = models.SlugField(max_length=32)
 
+    # FileField might be enough, OTOH we'll have really many files.
+    # So a more clever way of storage might be useful.
     def open(self, mode):
         """returns a binary file object for reading/writing of the document depending upon mode"""
         assert False, "not yet implemented"
 
+class EthicsCommission(models.Model):
+    name = models.CharField(max_length=60)
+    address_1 = models.CharField(max_length=60)
+    address_2 = models.CharField(max_length=60)
+    zip_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=40)
+
+class InvolvedCommissionsForSubmission(models.Model):
+    commission = models.ForeignKey(EthicsCommission)
+    submission = models.ForeignKey("SubmissionForm")
+    # is this the main commission?
+    main = models.BooleanField()
+
 class SubmissionForm(models.Model):
-    # ecsName, Type, Bla, Blu  = Daten des Einreich formulares
     pass
 
 class NotificationForm(models.Model):
@@ -30,7 +44,7 @@ class Checklist(models.Model):
     pass
 
 class SubmissionSet(models.Model):
-    submission = models.ForeignKey("Submission")
+    submission = models.ForeignKey("Submission", related_name="sets")
     documents = models.ManyToManyField(Document)
     submissionform = models.ForeignKey(SubmissionForm)
 
@@ -51,7 +65,6 @@ class SubmissionReview(models.Model):
     workflow = models.ForeignKey(Workflow)
 
 class Submission(models.Model):
-    submissionset = models.ForeignKey(SubmissionSet)
     submissionreview = models.ForeignKey(SubmissionReview)
     vote = models.ForeignKey(Vote)
     workflow = models.ForeignKey(Workflow)
