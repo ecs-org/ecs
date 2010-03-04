6df4390f4588ee7e77c3b1ea3f3182c11315ca02
yacc <yacc@ep3.at>
2010-03-04 11:24:41 +0100

    added unittests to make sure that urls/settings are importable.

---
+++ tests.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# (c) 2010 Medizinische Universität Wien
+#
+"""
+Unittests for the site
+"""
+
+def test_import():
+    "test if all modules are importable. Simple but quite useful."
+    import settings
+    import urls
+
+
