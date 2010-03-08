bc054faf9981138ed04b8b768718aca44a0058d5
Johannes Dollinger <emulbreh@googlemail.com>
2010-03-08 15:15:01 +0100

    Added max_length and required in form templates

--- static/js/forms.js
+++ static/js/forms.js
@@ -2,4 +2,27 @@ var ecs = {};
 
 jQuery(function($){
     $('form .field.DateField > input').datepicker({dateFormat: 'dd.mm.yy'});    
+    
+    $('form .field').each(function(){
+        var maxlength = $('input[type=text]', this).attr('maxlength');
+        var notes = [];
+        if($(this).hasClass('required')){
+            notes.push('required');
+        }        
+        if(maxlength){
+            notes.push('max. ' + maxlength + ' Zeichen');
+        }
+        if(notes.length){
+            $(this).append('<span class="notes">' + notes.join(', ') + '</span>')
+        }
+    });
+    
+    var formControls = $('form .field input, form .field textarea, form .field select');
+    formControls.focus(function(){
+        $(this).parent('.field').addClass('focus');
+    });
+    formControls.blur(function(){
+        $(this).parent('.field').removeClass('focus');
+    });
+    
 });
