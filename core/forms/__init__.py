e7ade1e1a9238091cd032c748989c6c4b1a1e722
Johannes Dollinger <emulbreh@googlemail.com>
2010-03-09 14:55:25 +0100

    Made ecs.core.forms a directory

---
+++ core/forms/__init__.py
@@ -0,0 +1,2 @@
+from ecs.core.forms.forms import BaseNotificationForm, ExtendedNotificationForm, SubmissionFormForm, DocumentForm, \
+    DocumentFormSet, ForeignParticipatingCenterFormSet, NonTestedUsedDrugFormSet, MeasureFormSet
