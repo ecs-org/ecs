2093c17dd86f4f6a65dbcc4685054c85113304f0
yacc <yacc@ep3.at>
2010-03-09 10:55:21 +0100

    fixed jsonifying the result.

--- docstash/views.py
+++ docstash/views.py
@@ -8,6 +8,14 @@ docstash API views.
 """
 
 from django.http import HttpResponse
+from django.utils.simplejson import dumps
 
+def jsonify(func):
+    def wrapper(*args, **kw):
+        res = func(*args, **kw)
+        return HttpResponse(dumps(res), "text/json")
+    return wrapper
+
+@jsonify
 def create(request):
-    return HttpResponse("meep" + request.POST["name"])
+    return ["meep"]
