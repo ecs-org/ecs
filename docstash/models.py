a571a8e460d74aa76afbb6bf235018d814516acb
yacc <yacc@ep3.at>
2010-03-09 09:11:40 +0100

    added docstash model

--- docstash/models.py
+++ docstash/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class DocStash(models.Model):
+    key = models.IntegerField(null=False)
+    token = models.IntegerField(null=False, primary_key=True)
+    value = models.TextField(null=False)
+    form = models.CharField(max_length=120, null=False)
+    name = models.CharField(max_length=120, null=False)
+
