5c3db593f00e8e34e8b1617a94a22832084f8411
Johannes Dollinger <emulbreh@googlemail.com>
2010-03-10 16:15:39 +0100

    Added missing test files

---
+++ core/tests/basics.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+
+def test_import():
+    "test if the urls module and the views are importable"
+    import ecs.urls
+    import ecs.core.urls
+    import ecs.core.views
+    import ecs.core.models
+    
+class CoreUrlsTest(TestCase):
+    def test_index(self):
+        response = self.client.get('/core/')
+        self.failUnlessEqual(response.status_code, 200)
+        
+    def test_submission_form_new(self):
+        response = self.client.get('/core/submission_form/new/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_notification_new(self):
+        response = self.client.get('/core/notification/new/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_notifications(self):
+        response = self.client.get('/core/notifications/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_submission_forms(self):
+        response = self.client.get('/core/submission_forms/')
+        self.failUnlessEqual(response.status_code, 200)
