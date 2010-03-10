1da013d2cb8573afd1da19e6a09b5eb4c8290c1c
Johannes Dollinger <emulbreh@googlemail.com>
2010-03-10 09:39:22 +0100

    Regrouped submission form elements

--- core/views.py
+++ core/views.py
@@ -11,6 +11,7 @@ import settings
 
 from ecs.core.models import Document, BaseNotificationForm, NotificationType, Submission, SubmissionForm
 from ecs.core.forms import DocumentFormSet, SubmissionFormForm, MeasureFormSet, NonTestedUsedDrugFormSet, ForeignParticipatingCenterFormSet
+from ecs.core.forms.layout import SUBMISSION_FORM_TABS
 from ecs.utils.htmldoc import htmldoc
 from ecs.core import paper_forms
 
@@ -108,92 +109,6 @@ def notification_pdf(request, notification_pk=None):
     
 
 # submissions
-
-# ((tab_label1, [(fieldset_legend11, [field111, field112, ..]), (fieldset_legend12, [field121, field122, ..]), ...]),
-#  (tab_label2, [(fieldset_legend21, [field211, field212, ..]), (fieldset_legend22, [field221, field222, ..]), ...]),
-# )
-SUBMISSION_FORM_TABS = (
-    (u'Eckdaten', [
-        (u'Titel', ['project_title', 'german_project_title', 'eudract_number', 'specialism', 'clinical_phase', 'already_voted',]),
-        (u'Art des Projekts', [
-            'project_type_non_reg_drug', 'project_type_reg_drug', 'project_type_reg_drug_within_indication', 'project_type_reg_drug_not_within_indication', 
-            'project_type_medical_method', 'project_type_medical_device', 'project_type_medical_device_with_ce', 'project_type_medical_device_without_ce',
-            'project_type_medical_device_performance_evaluation', 'project_type_basic_research', 'project_type_genetic_study', 'project_type_register',
-            'project_type_biobank', 'project_type_retrospective', 'project_type_questionnaire', 
-        ]),
-        (u'Weitere Angaben zur Art des Projekts', [
-            'project_type_education_context', 'project_type_misc',
-        ]),
-        (u'Zentren im Ausland', []),
-        (u'Arzneimittelstudie', ['pharma_checked_substance', 'pharma_reference_substance']),
-        (u'Medizinproduktestudie', ['medtech_checked_product', 'medtech_reference_substance']),
-        (u'Prüfungsteilnehmer', [
-            'subject_count', 'subject_minage', 'subject_maxage', 'subject_noncompetents', 'subject_males', 'subject_females', 
-            'subject_childbearing', 'subject_duration', 'subject_duration_active', 'subject_duration_controls', 'subject_planned_total_duration',
-        ]),
-    ]),
-    (u'Sponsor', [
-        (u'Sponsor', [
-            'sponsor_name', 'sponsor_contactname', 'sponsor_address1', 'sponsor_address2', 'sponsor_zip_code', 
-            'sponsor_city', 'sponsor_phone', 'sponsor_fax', 'sponsor_email',
-        ]),
-        (u'Rechnungsempfänger', [
-            'invoice_name', 'invoice_contactname', 'invoice_address1', 'invoice_address2', 'invoice_zip_code', 
-            'invoice_city', 'invoice_phone', 'invoice_fax', 'invoice_email',
-            'invoice_uid_verified_level1', 'invoice_uid_verified_level2',
-        ]),
-    ]),
-    (u'AMG', [
-        (u'AMG', [
-            'substance_registered_in_countries', 'substance_preexisting_clinical_tries', 
-            'substance_p_c_t_countries', 'substance_p_c_t_phase', 'substance_p_c_t_period', 
-            'substance_p_c_t_application_type', 'substance_p_c_t_gcp_rules', 'substance_p_c_t_final_report',
-        ]),
-    ]),
-    (u'MPG', [
-        (u'MPG', [
-            'medtech_product_name', 'medtech_manufacturer', 'medtech_certified_for_exact_indications', 'medtech_certified_for_other_indications', 
-            'medtech_ce_symbol', 'medtech_manual_included', 'medtech_technical_safety_regulations', 'medtech_departure_from_regulations',
-        ]),
-    ]),
-    (u'Versicherung', [
-        (u'Versicherung', [
-            'insurance_name', 'insurance_address_1', 'insurance_phone', 'insurance_contract_number', 'insurance_validity',
-        ]),
-    ]),
-    (u'Massnahmen', [
-        (u'Massnahmen', ['additional_therapy_info',]),
-    ]),
-    (u'Kurzfassung', [
-        (u'Kurzfassung', [
-            'german_summary', 'german_preclinical_results', 'german_primary_hypothesis', 'german_inclusion_exclusion_crit', 
-            'german_ethical_info', 'german_protected_subjects_info', 'german_recruitment_info', 'german_consent_info', 'german_risks_info', 
-            'german_benefits_info', 'german_relationship_info', 'german_concurrent_study_info', 'german_sideeffects_info', 
-            'german_statistical_info', 'german_dataprotection_info', 'german_aftercare_info', 'german_payment_info', 'german_abort_info', 'german_dataaccess_info',
-            'german_financing_info', 'german_additional_info',
-        ]),
-    ]),
-    (u'Biometrie', [
-        (u'Biometrie', [
-            'study_plan_alpha', 'study_plan_power', 'study_plan_statalgorithm', 'study_plan_multiple_test_correction_algorithm', 'study_plan_dropout_ratio',
-            'study_plan_population_intention_to_treat', 'study_plan_population_per_protocol', 'study_plan_abort_crit', 'study_plan_planned_statalgorithm', 
-            'study_plan_dataquality_checking', 'study_plan_datamanagement', 'study_plan_biometric_planning', 'study_plan_statistics_implementation', 
-            'study_plan_dataprotection_reason', 'study_plan_dataprotection_dvr', 'study_plan_dataprotection_anonalgoritm', 
-            'study_plan_blind', 'study_plan_observer_blinded', 'study_plan_randomized', 'study_plan_parallelgroups', 'study_plan_controlled', 
-            'study_plan_cross_over', 'study_plan_placebo', 'study_plan_factorized', 'study_plan_pilot_project', 'study_plan_equivalence_testing', 
-            'study_plan_misc', 'study_plan_number_of_groups', 'study_plan_stratification', 'study_plan_sample_frequency', 'study_plan_primary_objectives',
-            'study_plan_null_hypothesis', 'study_plan_alternative_hypothesis', 'study_plan_secondary_objectives',
-        ]),
-    ]),
-    (u'Unterlagen', []),
-    (u'Antragsteller', [
-        (u'Antragsteller', [
-            'submitter_name', 'submitter_organisation', 'submitter_jobtitle', 'submitter_is_coordinator', 'submitter_is_main_investigator', 'submitter_is_sponsor',
-            'submitter_is_authorized_by_sponsor', 'submitter_sign_date', 'submitter_agrees_to_publishing',
-        ]),
-    ]),
-    (u'Prüfer', []),
-)
 
 def create_submission_form(request):
     formsets = {}
