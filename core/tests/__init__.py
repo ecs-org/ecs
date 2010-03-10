d2fe52120b41c80fdb82ed47fa25ee099f2e3eeb
Johannes Dollinger <emulbreh@googlemail.com>
2010-03-10 18:49:14 +0100

    Added Document download test

--- core/tests/__init__.py
+++ core/tests/__init__.py
@@ -1,3 +1,4 @@
 from ecs.core.tests.basics import test_import, CoreUrlsTest
 from ecs.core.tests.submissions_and_notifications import SubmissionFormTest, NotificationFormTest
 from ecs.core.tests.reversion_integration import TestReversion
+from ecs.core.tests.documents import DocumentsTest
