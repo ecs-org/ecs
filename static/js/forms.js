b603edf2a6309c6d919740ab7e23ea4bcb43ec1f
Johannes Dollinger <emulbreh@googlemail.com>
2010-03-10 13:47:56 +0100

    Unified tabs and js for Notifications and Submissions

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,44 +1,54 @@
-var ecs = {};
+(function($){
+    var ecs = window.ecs = {};
+    ecs.setupFormFieldHelpers = function(context){
+        context = $(context || 'form');
+        $('li.DateField > input', context).datepicker({dateFormat: 'dd.mm.yy'});
+        $('li', context).each(function(){
+            var maxlength = $('input[type=text]', this).attr('maxlength');
+            var notes = [];
+            if($(this).hasClass('required')){
+                notes.push('required');
+            }        
+            if(maxlength){
+                notes.push('max. ' + maxlength + ' Zeichen');
+            }
+            if(notes.length){
+                $(this).append('<span class="notes">' + notes.join(', ') + '</span>')
+            }
 
-ecs.setupFormFieldHelpers = function(context){
-    context = $(context || 'form');
-    $('li.DateField > input', context).datepicker({dateFormat: 'dd.mm.yy'});
-    $('li', context).each(function(){
-        var maxlength = $('input[type=text]', this).attr('maxlength');
-        var notes = [];
-        if($(this).hasClass('required')){
-            notes.push('required');
-        }        
-        if(maxlength){
-            notes.push('max. ' + maxlength + ' Zeichen');
-        }
-        if(notes.length){
-            $(this).append('<span class="notes">' + notes.join(', ') + '</span>')
-        }
-
-        var formControls = $('li input, li textarea, li select', context);
-        formControls.focus(function(){
-            $(this).parent('li').addClass('focus');
-        });
-        formControls.blur(function(){
-            $(this).parent('li').removeClass('focus');
-        });
+            var formControls = $('li input, li textarea, li select', context);
+            formControls.focus(function(){
+                $(this).parent('li').addClass('focus');
+            });
+            formControls.blur(function(){
+                $(this).parent('li').removeClass('focus');
+            });
         
-    });
-};
+        });
+    };
 
-ecs.setupFormSetHelpers = function(prefix){
-    $('#' + prefix + '_formset .form').formset({
-        prefix: prefix,
-        formCssClass: prefix + '_formset',
-        added: function(row){
-            ecs.setupFormFieldHelpers(row);
-        }
-    });
+    ecs.setupFormSetHelpers = function(prefix){
+        $('#' + prefix + '_formset .form').formset({
+            prefix: prefix,
+            formCssClass: prefix + '_formset',
+            added: function(row){
+                ecs.setupFormFieldHelpers(row);
+            }
+        });
     
-};
+    };
 
+    $(function(){
+        $(".tab_headers > li:first-child").addClass('active');
+        $(".tab_headers").tabify();
+        ecs.setupFormFieldHelpers();
+    
+        $(".tab").each(function(){
+            if($('.errors', $(this)).length){ 
+                $('.tab_headers > li a[href=#' + this.id + '-tab]').parent('li').addClass('errors');
+            }
+        });
+    
+    }); 
 
-jQuery(function($){    
-    ecs.setupFormFieldHelpers();
-});
+})(jQuery);
