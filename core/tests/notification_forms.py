92a44638c95ff83e7f2f7e6bbd0543b46bf67268
Johannes Dollinger <emulbreh@googlemail.com>
2010-03-17 14:22:40 +0100

    Added document upload tests

--- core/tests/notification_forms.py
+++ core/tests/notification_forms.py
@@ -1,5 +1,6 @@
+import os
 from django.test import TestCase
-from ecs.core.models import NotificationType
+from ecs.core.models import NotificationType, DocumentType
 
 from ecs.core.tests.submissions_and_notifications import create_submission_form
 
@@ -61,5 +62,33 @@ class NotificationFormTest(TestCase):
         self.failUnlessEqual(obj.comments, 'foo comment')
         self.failUnlessEqual(obj.submission_forms.all()[0], submission_form)
         
+    def _setup_POST_url(self):
+        notification_type = NotificationType.objects.create(name='foo notif')
+        response = self.client.get('/core/notification/new/%s/' % notification_type.pk)
+        return response['Location']
+        
+    def test_document_upload(self):
+        url = self._setup_POST_url()
+        data = self._create_POST_data(save='save')
+        doctype = DocumentType.objects.create(name='foo doctype')
+        f = open(os.path.join(os.path.dirname(__file__), 'data', 'menschenrechtserklaerung.pdf'), 'rb')
+        data.update({
+            'document-0-file': f,
+            'document-0-doctype': doctype.pk,
+            'document-0-version': '3.1415',
+            'document-0-date': '17.03.2010',
+        })
+        response = self.client.post(url, data)
+        f.close()
+        self.failUnless('<form' in response.content)
+        documents = response.context['documents']
+        self.failUnlessEqual(len(documents), 1)
+        doc = documents[0]
+        self.failUnlessEqual(doc.version, '3.1415')
+        self.failUnless(doc.file)
+        
+        response = self.client.get('/core/document/%s/download/' % doc.pk)
+        self.failUnlessEqual(response.status_code, 200)
+        self.failUnlessEqual(response.content[:4], '%PDF')
         
 
