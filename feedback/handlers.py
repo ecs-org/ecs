ae279034f5529d1e61221be1fe789fd5c1500e8e
yacc <yacc@ep3.at>
2010-03-17 13:44:44 +0100

    backend for feedback.

---
+++ feedback/handlers.py
@@ -0,0 +1,68 @@
+from piston.handler import BaseHandler
+from piston.doc import generate_doc
+from feedbackend.models import Feedback
+import datetime
+
+class FeedbackHandler(BaseHandler):
+    model = Feedback
+    fields = ('id', 'feedbacktype', 'summary', 'description', 'origin', 'username', 'email', 'pub_date')
+    allowed_methods = ('GET', 'PUT')
+    
+    def read(self, request, pk):
+        return Feedback.objects.get(id=pk)
+    
+    def update(self, request, pk):
+        obj = Feedback.objects.get(id=pk)
+        for fieldname in "feedbacktype,summary,description,origin,username,email".split(","):
+            setattr(obj, fieldname, request.PUT.get(fieldname))
+            obj.save()
+        return obj
+
+class FeedbackCreate(BaseHandler):
+    model = Feedback
+    fields = ('id', 'feedbacktype', 'summary', 'description', 'origin', 'username', 'email', 'pub_date')
+
+    allowed_methods = ("POST", "GET")
+    def create(self, request):
+        args = {"pub_date": datetime.datetime.now()}
+        for fieldname in "feedbacktype,summary,description,origin,username,email".split(","):
+            args[fieldname] = request.POST.get(fieldname)
+        obj = Feedback(**args)
+        obj.save()
+        return obj
+
+    def read(self, request):
+        return Feedback.objects.all()
+
+class FeedbackSearch(BaseHandler):
+    model = Feedback
+    fields = ('id', 'feedbacktype', 'summary', 'description', 'origin', 'username', 'email', 'pub_date')
+    allowed_methods = ("GET", )
+    def read(self, request, type, offsetdesc):
+        objs = Feedback.objects.order_by("id")
+        if type != "all":
+            objs = objs.filter(feedbacktype=type)
+        offset = 0
+        count = 20
+        if offsetdesc:
+            offsets = offsetdesc.split("/")
+            if len(offsets) == 3:
+                objs = objs.filter(origin=offsets[0])
+                print "ORIGIN", offsets[0]
+                offsets = offsets[1:]
+            if len(offsets) == 2:
+                count = int(offsets[1])
+            if offsets[0] == "":
+                pass
+            elif offsets[0] == "last":
+                offset = -count
+            else:
+                offset = int(offsets[0])
+        print offset, count
+        if offset >= 0:
+            return objs.all()[offset:count]
+        else:
+            return list(objs.all())[offset:]
+
+                
+
---
+++ feedback/urls.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2.4
+# -*- coding: utf-8 -*-
+#
+# (c) 2009 Medizinische Universität Wien
+#
+"""
+URL map for feedback backend.
+"""
+
+from django.conf.urls.defaults import *
+from piston.resource import Resource
+
+from feedbackend.handlers import FeedbackHandler, FeedbackCreate, FeedbackSearch
+
+feedbackpost_resource = Resource(handler=FeedbackHandler)
+feedbackpost_create = Resource(handler=FeedbackCreate)
+feedbackpost_search = Resource(handler=FeedbackSearch)
+
+
+def test(*args, **kw):
+    print "args", args
+    print "kw", kw
+    return None
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', feedbackpost_create),
+    url(r'^(?P<pk>[^/]+)$', feedbackpost_resource), 
+    url(r'^(?P<type>[^/]+)/(?P<offsetdesc>.*)$', feedbackpost_search), 
+    #url(r'^(?P<pk>.*)$', test), 
+)
+
+#
+# monkey patching django piston so it can handle charsets in the content-type header.
+# (it's here because urls.py is imported for sure before the first request being processed.
+#
+
+from piston import utils
+
+def content_type(self):
+    """
+    Returns the content type of the request in all cases where it is
+    different than a submitted form - application/x-www-form-urlencoded
+    """
+    type_formencoded = "application/x-www-form-urlencoded"
+    
+    ctype = self.request.META.get('CONTENT_TYPE', type_formencoded).split(";")[0]
+    
+    if ctype == type_formencoded:
+        return None
+    
+    return ctype
+
+utils.Mimer.content_type = content_type
