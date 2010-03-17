db741cfb88c6ecf7395cbc24f48793740fe995b2
yacc <yacc@ep3.at>
2010-03-17 09:47:14 +0100

    backend model class moved.

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Feedback(models.Model):
+    FEEDBACK_TYPES=(('i', 'Idea'),('q','Question'))
+    feedbacktype = models.CharField(choices=FEEDBACK_TYPES, max_length=1)
+    summary= models.CharField(max_length=200) 
+    description = models.TextField()
+    origin = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    email = models.EmailField()
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
