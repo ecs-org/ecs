1140ade694b788a07b9d5d2f53e3b0cc87339e35
yacc <yacc@ep3.at>
2010-03-18 12:09:14 +0100

    added initial migration for feedback

---
+++ feedback/migrations/0001_initial.py
@@ -0,0 +1,88 @@
+
+from south.db import db
+from django.db import models
+from ecs.feedback.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Adding model 'Feedback'
+        db.create_table('feedback_feedback', (
+            ('id', orm['feedback.Feedback:id']),
+            ('feedbacktype', orm['feedback.Feedback:feedbacktype']),
+            ('summary', orm['feedback.Feedback:summary']),
+            ('description', orm['feedback.Feedback:description']),
+            ('origin', orm['feedback.Feedback:origin']),
+            ('question', orm['feedback.Feedback:question']),
+            ('pub_date', orm['feedback.Feedback:pub_date']),
+        ))
+        #        db.send_create_signal('feedback', ['Feedback'])
+        
+        # Adding ManyToManyField 'Feedback.me_too_votes'
+        db.create_table('feedback_feedback_me_too_votes', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('feedback', models.ForeignKey(orm.Feedback, null=False)),
+            ('user', models.ForeignKey(orm['auth.User'], null=False))
+        ))
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Deleting model 'Feedback'
+        db.delete_table('feedback_feedback')
+        
+        # Dropping ManyToManyField 'Feedback.me_too_votes'
+        db.delete_table('feedback_feedback_me_too_votes')
+        
+    
+    
+    models = {
+        'auth.group': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)"},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'feedback.feedback': {
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'feedbacktype': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'me_too_votes': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']"}),
+            'origin': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'question': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'summary': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+    
+    complete_apps = ['feedback']
