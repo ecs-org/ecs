0ff8e3704309eac87f8af8fe664d311688546f44
Felix Erkinger <wuxxin@ecsdev.ep3.at>
2010-03-20 20:06:59 +0100

    another swishmob and cleanup, and removed buildin jquery inside jquery.tools (was old also)<F2>

--- static/feedback/js/feedback.js
+++ static/feedback/js/feedback.js
@@ -265,7 +265,7 @@ $(function() {
   ///////////////////////////
 
   // setup ul.tabs to work as tabs for each div directly under div.panes 
-  $("ul.tabs").tabs("div.panes > div", {
+  $("ul.tabs").toolstabs("div.panes > div", {
    
     // when the tab is clicked (different tab, and at construction time)
     onClick: function (event, tabIndex) {
