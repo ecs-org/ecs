5ea9ce6d311ecf0f8c8f5d75eef049fdeab4bcfa
yacc <yacc@ep3.at>
2010-03-25 23:03:15 +0100

    redone initial migration for feedbacks.

--- feedback/migrations/0001_initial.py
+++ feedback/migrations/0001_initial.py
@@ -1,57 +1,59 @@
-
+# encoding: utf-8
+import datetime
 from south.db import db
+from south.v2 import SchemaMigration
 from django.db import models
-from ecs.feedback.models import *
 
-class Migration:
+class Migration(SchemaMigration):
     
     def forwards(self, orm):
         
         # Adding model 'Feedback'
         db.create_table('feedback_feedback', (
-            ('id', orm['feedback.Feedback:id']),
-            ('feedbacktype', orm['feedback.Feedback:feedbacktype']),
-            ('summary', orm['feedback.Feedback:summary']),
-            ('description', orm['feedback.Feedback:description']),
-            ('origin', orm['feedback.Feedback:origin']),
-            ('question', orm['feedback.Feedback:question']),
-            ('pub_date', orm['feedback.Feedback:pub_date']),
+            ('origin', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('question', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('summary', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('feedbacktype', self.gf('django.db.models.fields.CharField')(max_length=1)),
+            ('pub_date', self.gf('django.db.models.fields.DateTimeField')()),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
         ))
-        #        db.send_create_signal('feedback', ['Feedback'])
-        
-        # Adding ManyToManyField 'Feedback.me_too_votes'
+        db.send_create_signal('feedback', ['Feedback'])
+
+        # Adding M2M table for field me_too_votes on 'Feedback'
         db.create_table('feedback_feedback_me_too_votes', (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('feedback', models.ForeignKey(orm.Feedback, null=False)),
-            ('user', models.ForeignKey(orm['auth.User'], null=False))
+            ('feedback', models.ForeignKey(orm['feedback.feedback'], null=False)),
+            ('user', models.ForeignKey(orm['auth.user'], null=False))
         ))
-        
+        db.create_unique('feedback_feedback_me_too_votes', ['feedback_id', 'user_id'])
     
     
     def backwards(self, orm):
         
         # Deleting model 'Feedback'
         db.delete_table('feedback_feedback')
-        
-        # Dropping ManyToManyField 'Feedback.me_too_votes'
+
+        # Removing M2M table for field me_too_votes on 'Feedback'
         db.delete_table('feedback_feedback_me_too_votes')
-        
     
     
     models = {
         'auth.group': {
+            'Meta': {'object_name': 'Group'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
             'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
         },
         'auth.permission': {
-            'Meta': {'unique_together': "(('content_type', 'codename'),)"},
+            'Meta': {'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
         'auth.user': {
+            'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
@@ -67,13 +69,14 @@ class Migration:
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
         'contenttypes.contenttype': {
-            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
         'feedback.feedback': {
+            'Meta': {'object_name': 'Feedback'},
             'description': ('django.db.models.fields.TextField', [], {}),
             'feedbacktype': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
