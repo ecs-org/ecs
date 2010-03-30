4f8cddf1c7b368e1b460ca54c6bc5293467b2ac2
yacc <yacc@ep3.at>
2010-03-30 15:13:33 +0200

    fixed #221

--- core/models.py
+++ core/models.py
@@ -29,6 +29,22 @@ def upload_document_to(instance=None, filename=None):
     return os.path.join(settings.FILESTORE, *dirs)
 
 class DocumentFileStorage(FileSystemStorage):
+    def get_available_name(self, name):
+        """
+        Returns a filename that's free on the target storage system, and
+        available for new content to be written to.
+        Limit the length to some reasonable value.
+        """
+        dir_name, file_name = os.path.split(name)
+        file_root, file_ext = os.path.splitext(file_name)
+        # If the filename already exists, add _ with a 4 digit number till we get an empty slot.
+        counter = 0
+        while self.exists(name):
+            # file_ext includes the dot.
+            counter += 1
+            name = os.path.join(dir_name, "%s_%04d%s" % (file_root, counter, file_ext))
+        return name
+
     def path(self, name):
         # We need to overwrite the default behavior, because django won't let us save documents outside of MEDIA_ROOT
         return smart_str(os.path.normpath(name))
