b77f6e34d70288820cbfb6a949a09196b63f5978
yacc <yacc@ep3.at>
2010-04-01 15:22:25 +0100

    added submission pdf view.

--- core/views.py
+++ core/views.py
@@ -218,6 +218,20 @@ def view_submission_form(request, submission_form_pk=None):
         'documents': submission_form.documents.filter(deleted=False).order_by('doctype__name', '-date'),
     })
 
+
+def submission_pdf(request, submission_form_pk=None):
+    submission_form = get_object_or_404(SubmissionForm, pk=submission_form_pk)
+    html = render(request, 'submissions/xhtml2pdf/view.html', {
+        'paper_form_fields': paper_forms.get_field_info_for_model(SubmissionForm),
+        'submission_form': submission_form,
+        'documents': submission_form.documents.filter(deleted=False).order_by('doctype__name', '-date'),
+    })
+    pdf = xhtml2pdf(html.encode('utf-8'), webpage=True)
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment;filename=notification_%s.pdf' % notification_pk
+    return response
+
+
 def submission_form_list(request):
     return render(request, 'submissions/list.html', {
         'submission_forms': SubmissionForm.objects.all().order_by('project_title'),
