92ca1db32587f02f6f10f003669fd8383f9f084a
Johannes Dollinger <emulbreh@googlemail.com>
2010-04-15 11:03:34 +0200

    Commit a change missing from r805

--- core/forms/meetings.py
+++ core/forms/meetings.py
@@ -21,8 +21,14 @@ class BaseConstraintFormSet(BaseModelFormSet):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('queryset', Constraint.objects.none())
         super(BaseConstraintFormSet, self).__init__(*args, **kwargs)
+        
+class ConstraintForm(forms.ModelForm):
+    weight = forms.ChoiceField(choices=((0.5, u'ungünstig'), (1.0, u'unmöglich')))
 
-UserConstraintFormSet = modelformset_factory(Constraint, formset=BaseConstraintFormSet, extra=1, exclude = ('meeting', 'user'), can_delete=True)
+    class Meta:
+        model = Constraint
+
+UserConstraintFormSet = modelformset_factory(Constraint, formset=BaseConstraintFormSet, extra=1, exclude = ('meeting', 'user'), can_delete=True, form=ConstraintForm)
 
 ParticipationFormSet = modelformset_factory(Participation, extra=1, can_delete=True)
     
