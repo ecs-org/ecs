9e1fbc2f0db2797fdd13c92ffea066282caad528
Felix Erkinger <wuxxin@ecsdev.ep3.at>
2010-04-17 19:02:14 +0200

    fixes

--- utils/xhtml2pdf.py
+++ utils/xhtml2pdf.py
@@ -1,19 +1,20 @@
 import subprocess
 import killableprocess
 import tempfile
+import os
 
 def which(file, mode=os.F_OK | os.X_OK, path=None):
-	"""Locate a file in the user's path, or a supplied path. The function
-	yields full paths in which the given file matches a file in a directory on
-	the path.
-	"""
-	if not path:
+    """Locate a file in the user's path, or a supplied path. The function
+    yields full paths in which the given file matches a file in a directory on
+    the path.
+    """
+    if not path:
         path = os.environ.get("PATH", os.defpath)
 
-	for dir in path.split(os.pathsep):
-		full_path = os.path.join(dir, file)
-		if os.path.exists(full_path) and os.access(full_path, mode):
-			yield full_path
+    for dir in path.split(os.pathsep):
+        full_path = os.path.join(dir, file)
+        if os.path.exists(full_path) and os.access(full_path, mode):
+            yield full_path
 
 def xhtml2pdf(html, **options):
     """ 
