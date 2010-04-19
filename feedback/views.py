65a89c64bc3d4f57ff293a699f9f1f69af497841
Marc van Woerkom <mvanwoerkom@acm.org>
2010-04-19 13:23:47 +0200

    - added /feedback/input/ view

--- feedback/views.py
+++ feedback/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+from ecs.core.views.utils import render, redirect_to_next_url
+
+def feedback_input(request):
+    return render(request, 'input.html', {
+    })
+
