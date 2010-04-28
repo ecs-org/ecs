1b4a2d29909e61710364e672363c6151970d70f7
Johannes Dollinger <emulbreh@googlemail.com>
2010-04-28 11:15:31 +0200

    Added genetic_sort test

---
+++ utils/tests/genetic_sort.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from ecs.utils.genetic_sort import GeneticSorter, swap_mutation
+
+class GeneticSorterTest(TestCase):
+    def test_list_sort(self):
+        N = 20
+        data = random.sample(xrange(N), N)
+        f = lambda p: sum(1.0 / (abs(i-x) + 1) for i, x in enumerate(p))
+        sorter = GeneticSorter(data, evaluation_func=f, population_size=50, mutations={swap_mutation: 0.02})
+        # This is fragile: if we don't find a solution in 10^6 generations this test will fail.
+        for i in xrange(1000):
+            result = sorter.run(1000)
+            if result == range(N):
+                break
+        self.failUnlessEqual(result, tuple(xrange(N)))
+        
