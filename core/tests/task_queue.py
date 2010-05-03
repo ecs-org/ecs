6adb1930bdab3cd9687ddfee5de62b81fa6a7698
Martin Natano <natano@natano.net>
2010-05-03 19:59:33 +0200

    extend celery unit test

--- core/tests/task_queue.py
+++ core/tests/task_queue.py
@@ -10,4 +10,6 @@ class CeleryTest(TestCase):
     def test_celery(self):
         retval = basic_test.delay()
         self.failUnlessEqual(retval.get(), 'success')
+        self.failUnlessEqual(retval.result, 'success')
+        self.failUnless(retval.successful())
 
