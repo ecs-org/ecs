41667e1a7aef13001195c78441147b9c4442afc7
Marc van Woerkom <mvanwoerkom@acm.org>
2010-05-05 04:23:08 +0200

    - moving information into the view..

--- pdfviewer/views.py
+++ pdfviewer/views.py
@@ -2,6 +2,7 @@
 
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
+from django.conf import settings
 
 from ecs.core.views.utils import render, redirect_to_next_url
 
@@ -30,7 +31,21 @@ def show(request, id=1, page=1):
         return HttpResponse("Error: invalid parameter page = '%s'!" % page)
     page = int(page)
 
+    images = [
+        { 'page': 1, 'url': settings.MEDIA_URL + 'pdfviewer/images/Meld_512.png' }, 
+        { 'page': 2, 'url': settings.MEDIA_URL + 'pdfviewer/images/Meld_511.png' },
+    ]
+    pages = len(images)
+
+    thumbs = [
+        { 'page': 1, 'url': settings.MEDIA_URL + 'pdfviewer/images/Meld_512.png' }, 
+        { 'page': 2, 'url': settings.MEDIA_URL + 'pdfviewer/images/Meld_511.png' },
+    ]
+
     return render(request, 'show.html', {
         'id': id,
         'page': page,
+        'pages': pages,
+        'images': images,
+        'thumbs': thumbs,
     })
