f05e1dd39d58cf6f7abd27e1878c77a817768e8f
Johannes Dollinger <emulbreh@googlemail.com>
2010-05-11 17:44:32 +0200

    Added Roar notifications for new messages

---
+++ messages/templatetags/messages.py
@@ -0,0 +1,32 @@
+import datetime
+from django.template import Library, Node, TemplateSyntaxError
+from django.core.cache import cache
+from ecs.messages.models import Message
+
+register = Library()
+
+class FetchMessagesNode(Node):
+    def __init__(self, varname):
+        super(FetchMessagesNode, self).__init__()
+        self.varname = varname
+
+    def render(self, context):
+        user = context['request'].user
+        cache_key = "ecs.messages:last_pull:%s" % user.pk
+        last_pull = cache.get(cache_key)
+        if last_pull:
+            messages = Message.objects.filter(receiver=user, timestamp__gt=last_pull)
+        else:
+            messages = Message.objects.filter(receiver=user, unread=True)
+        cache.set(cache_key, datetime.datetime.now(), 7*24*3600)
+        context[self.varname] = messages
+        return u''
+        
+
+@register.tag
+def fetch_messages(parser, token):
+    try:
+        name, as_, varname = token.split_contents()
+    except ValueError:
+        raise TemplateSyntaxError('{% fetch_messages as VAR %} expected')
+    return FetchMessagesNode(varname)
