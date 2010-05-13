4ba536953270bb522452f6bb920df5153b1b7c40
Martin Natano <natano@natano.net>
2010-05-13 18:22:22 +0200

    first draft of the activities widget

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,12 @@
 from ecs.core.views.utils import render
 from ecs.messages.models import Message
 from django.shortcuts import get_object_or_404
+from ecs.tasks.models import Task
 
 def view_dashboard(request):
     return render(request, 'dashboard/dashboard.html', {
         'message_list': Message.objects.select_related('sender', 'receiver').filter(receiver=request.user).order_by('-timestamp'),
+        'tasks': Task.objects.filter(closed_at=None, assigned_to=request.user, accepted=True),
     })
 
 def read_message(request, message_pk):
