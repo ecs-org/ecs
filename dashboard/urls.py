d8f061643f3726a06009fa58d43a3675ab3b318d
Martin Natano <natano@natano.net>
2010-05-13 17:05:34 +0200

    open dialog window to read a message (dashboard)

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ecs.dashboard.views',
     url(r'^$', 'view_dashboard'),
+    url(r'^message/(?P<message_pk>\d+)/read/$', 'read_message'),
 )
