2b50562ece93c9496647b7936b87c32b794d6aaf
Marc van Woerkom <mvanwoerkom@acm.org>
2010-05-18 02:29:03 +0200

    checklist ordering, comments, initial values

--- core/forms/checklist.py
+++ core/forms/checklist.py
@@ -1,11 +1,17 @@
 from django import forms
+from django.utils.datastructures import SortedDict
+
 from ecs.core.models import ChecklistBlueprint, Checklist, ChecklistAnswer, ChecklistQuestion
 
 
 def make_checklist_form(checklist):
-    fields = { }
+    fields = SortedDict()
     blueprint = checklist.blueprint
-    for question in blueprint.questions.all():
-        fields['question%s' % question.pk] = forms.NullBooleanField(label=question.text, required=False)
+    i = 0
+    for question in blueprint.questions.order_by('id'):
+        i = i + 1
+        answer = checklist.answers.get(question=question)
+        fields['f_%s_q' % i] = forms.NullBooleanField(initial=answer.answer, label='%s. %s' % (i, question.text), help_text=question.description, required=False)
+        fields['f_%s_c' % i] = forms.CharField(label='Kommentar', required=False)
     form_class = type('Checklist%sForm' % blueprint.pk, (forms.BaseForm,), {'base_fields': fields})
     return form_class
