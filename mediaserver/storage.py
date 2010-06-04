78248c92fb2c58af5178502fba08c7b6570caabb
Marc van Woerkom <mvanwoerkom@acm.org>
2010-06-04 21:14:18 +0200

    mediaserver: memcached/memcachedb configuration, warning about critical size

--- mediaserver/storage.py
+++ mediaserver/storage.py
@@ -6,20 +6,28 @@ import time
 
 class PageData(object):
     def __init__(self, png_name):
+        crit_size = 200 * 1024
         self.png_name = png_name
-        f = open(png_name, 'rb')
+        f = open(self.png_name, 'rb')
         self.png_data = f.read()
         f.close()
+        self.png_data_size = len(self.png_data)
+        if self.png_data_size >= crit_size:
+            delta = (100.0 * (self.png_data_size - crit_size)) / crit_size
+            print "Warning: (len(%s): %d) >= (crit_size: %d) [+%0.2f%%]" % (self.png_name, self.png_data_size, crit_size, delta)
         self.png_time = time.time()
 
     def __str__(self):
-        return '(%s, length %d, %s)' % (self.png_name, len(self.png_data), time.ctime(self.png_time))
+        return '(%s, size %d, %s)' % (self.png_name, self.png_data_size, time.ctime(self.png_time))
 
 
 class Storage(object):
     def __init__(self):
         self.ns = 'media'
-        self.mc = memcache.Client(['127.0.0.1:21201'], debug=0)
+        port_memcached = 11211
+        port_memcachedb = 21201
+        host = '127.0.0.1'
+        self.mc = memcache.Client(['%s:%d' % (host, port_memcachedb)], debug=0)
 
     def get_key(self, id, bigpage, zoom):
         return '%s:%s:%s:%s' % (self.ns, id, bigpage, zoom)
