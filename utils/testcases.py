f81a133485ab8226efc9ba08bdb6b4a1a7270159
Felix Erkinger <wuxxin@ecsdev.ep3.at>
2010-06-08 23:14:36 +0200

    work in progress branch

--- utils/testcases.py
+++ utils/testcases.py
@@ -8,6 +8,7 @@ class LoginTestCase(TestCase):
         user.set_password('password')
         user.save()
         self.client.login(username='unittest', password='password')
+        print ("login setup was called", user, self.client)
     
     def tearDown(self):
         self.client.logout()
