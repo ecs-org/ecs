6833494e441c8f5f1d96bfade229e13fbad57bb6
Marc van Woerkom <mvanwoerkom@acm.org>
2010-06-10 13:09:28 +0200

    embedded pdf-as

--- mediaserver/urls.py
+++ mediaserver/urls.py
@@ -5,5 +5,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns(
     '',
+    url(r'^signpdf$', 'ecs.mediaserver.views.sign_pdf'),
+    url(r'^sendpdf$', 'ecs.mediaserver.views.send_pdf'),
+    url(r'^signpdferror$', 'ecs.mediaserver.views.sign_pdf_error'),
+    url(r'^receivepdf$', 'ecs.mediaserver.views.receive_pdf'),
     url(r'^(?P<id>\d+)/(?P<bigpage>\d+)/(?P<zoom>[^/]+)/$', 'ecs.mediaserver.views.get_image'),
 )
