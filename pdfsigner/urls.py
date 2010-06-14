d22949826a9b5d4750b1c8a359708e1aedb8db41
Marc van Woerkom <mvanwoerkom@acm.org>
2010-06-14 21:21:58 +0200

    * some refactoring  * adding low grade access protection by generating random ids and checking them at access time

--- pdfsigner/urls.py
+++ pdfsigner/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns(
     '',
     url(r'^demo$', 'ecs.pdfsigner.views.demo'),
-    url(r'^sign_pdf$', 'ecs.pdfsigner.views.sign_pdf'),
-    url(r'^send_pdf$', 'ecs.pdfsigner.views.send_pdf'),
-    url(r'^sign_pdf_error$', 'ecs.pdfsigner.views.sign_pdf_error'),
-    url(r'^receive_pdf$', 'ecs.pdfsigner.views.receive_pdf'),
-    url(r'^receive_pdf;jsessionid=(?P<jsessionid>[^?]*)$', 'ecs.pdfsigner.views.receive_pdf'),
+    url(r'^demo_sign_pdf$', 'ecs.pdfsigner.views.demo_sign_pdf'),
+    url(r'^demo_send_pdf$', 'ecs.pdfsigner.views.demo_send_pdf'),
+    url(r'^demo_sign_pdf_error$', 'ecs.pdfsigner.views.demo_sign_pdf_error'),
+    url(r'^demo_receive_pdf$', 'ecs.pdfsigner.views.demo_receive_pdf'),
+    url(r'^demo_receive_pdf;jsessionid=(?P<jsessionid>[^?]*)$', 'ecs.pdfsigner.views.demo_receive_pdf'),
 )
--- pdfsigner/views.py
+++ pdfsigner/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import random
 import urllib
 import urllib2
 
@@ -10,17 +11,20 @@ from django.conf import settings
 from ecs.utils import forceauth
 
 
-def send_pdf_is_authorized(request):
+demo_store = [ ]
+
+
+def demo_send_pdf_is_authorized(request):
     # TODO think about security implications
     return True
 
 
-def receive_pdf_is_authorized(request):
+def demo_receive_pdf_is_authorized(request):
     # TODO think about security implications
     return True
 
 
-def get_pdf_data():
+def demo_get_pdf_data():
     pdf_name = 'test-pdf-14-seitig.pdf'
     pdf_path = os.path.join(settings.MEDIA_ROOT, 'pdfsigner', 'images', pdf_name)
     f = open(pdf_path, 'rb')
@@ -31,16 +35,23 @@ def get_pdf_data():
 
 
 @forceauth.exempt     
-def send_pdf(request):
-    if send_pdf_is_authorized(request) is False:
+def demo_send_pdf(request):
+    if demo_send_pdf_is_authorized(request) is False:
         return HttpResponseForbidden('<h1>Access denied</h1>')
-    pdf_data, pdf_data_size, pdf_name = get_pdf_data()
-    response = HttpResponse(pdf_data, mimetype='application/pdf')
-    return response
+    if request.REQUEST.has_key('pdf-id'):
+        pdf_id = request.REQUEST['pdf-id']
+    else:
+        pdf_id = ''
+    try:
+        demo_store.remove(pdf_id)
+    except ValueError:
+        return HttpResponseForbidden('<h1>Invalid pdf-id</h1>')
+    pdf_data, pdf_data_size, pdf_name = demo_get_pdf_data()
+    return HttpResponse(pdf_data, mimetype='application/pdf')
 
 
 @forceauth.exempt
-def sign_pdf_error(request):
+def demo_sign_pdf_error(request):
     if request.REQUEST.has_key('error'):
         error = urllib.unquote_plus(request.REQUEST['error'])
     else:
@@ -49,33 +60,34 @@ def sign_pdf_error(request):
         cause = urllib.unquote_plus(request.REQUEST['cause'])  # FIXME can't deal with UTF-8 encoded Umlauts
     else:
         cause = ''
-    return HttpResponse(u'sign_pdf_error: error=[%s], cause=[%s]' % (error, cause))
+    return HttpResponse('demo_sign_pdf_error: error=[%s], cause=[%s]' % (error, cause))
 
 
 @forceauth.exempt
-def receive_pdf(request, jsessionid=''):
-    if receive_pdf_is_authorized(request) is False:
+def demo_receive_pdf(request, jsessionid=''):
+    if demo_receive_pdf_is_authorized(request) is False:
         return HttpResponseForbidden('<h1>Access denied</h1>')
-    # ..
-    print 'receive_pdf jsessionid="%s"' % jsessionid
+    print 'demo_receive_pdf jsessionid="%s"' % jsessionid
     if request.REQUEST.has_key('pdf-url') and request.REQUEST.has_key('pdf-id') and request.REQUEST.has_key('num-bytes') and request.REQUEST.has_key('pdfas-session-id'):
        pdf_url = request.REQUEST['pdf-url']
        pdf_id = request.REQUEST['pdf-id']
        num_bytes = request.REQUEST['num-bytes']
        pdfas_session_id = request.REQUEST['pdfas-session-id']
-       url = 'http://ecsdev.ep3.at:4780/pdf-as/%s?pdf-id=%s&num-bytes=%s&pdfas-session-id=%s' % (pdf_url, pdf_id, num_bytes, pdfas_session_id)
+       pdfas_service = 'http://ecsdev.ep3.at:4780/pdf-as/'
+       url = '%s%s?pdf-id=%s&num-bytes=%s&pdfas-session-id=%s' % (pdfas_service, pdf_url, pdf_id, num_bytes, pdfas_session_id)
        return HttpResponse('<h1>Download your signed PDF</h1><a href="%s">download link</a>' % url)
-    return HttpResponse('receive_pdf got [%s]' % request)
+    return HttpResponse('demo_receive_pdf got [%s]' % request)
 
 
-def demo(request):
-    url = request.build_absolute_uri('/pdfsigner/sign_pdf')
-    return HttpResponse('<h1>Start the Online PDF Signing Demo</h1><a href="%s">start link</a>' % url)
-
-
-def sign_pdf(request):
-    pdf_data , pdf_data_size, pdf_name = get_pdf_data()
-    url = 'http://ecsdev.ep3.at:4780/pdf-as/Sign'
+def demo_sign_pdf(request):
+    pdf_data , pdf_data_size, pdf_name = demo_get_pdf_data()
+    pdfas_service = 'http://ecsdev.ep3.at:4780/pdf-as/'
+    url_sign = '%sSign' % pdfas_service
+    pdf_id = '%s' % random.randint(1, 10e18)
+    demo_store.append(pdf_id)
+    url_send = request.build_absolute_uri('demo_send_pdf')
+    url_receive = request.build_absolute_uri('demo_receive_pdf')
+    url_error = request.build_absolute_uri('demo_sign_pdf_error')
     values = {
         'preview': 'false',
         'connector': 'moc',  # undocumented feature! selects ONLINE CCE/BKU
@@ -84,14 +96,21 @@ def sign_pdf(request):
         'inline': 'false',
         'filename': pdf_name,
         'num-bytes': '%s' % pdf_data_size,
-        'pdf-url': request.build_absolute_uri('/pdfsigner/send_pdf'), 
-        'pdf-id': '1956507909008215134',
-        'invoke-app-url': request.build_absolute_uri('/pdfsigner/receive_pdf'),
-        'invoke-app-error-url': request.build_absolute_uri('/pdfsigner/sign_pdf_error'),
+        'pdf-url': url_send,
+        'pdf-id': pdf_id,
+        'invoke-app-url': url_receive, 
+        'invoke-app-error-url': url_error,
         # session-id=9085B85B364BEC31E7D38047FE54577D
         'locale': 'de',
     }
     data = urllib.urlencode(values)
-    redirect = '%s?%s' % (url, data)
-    print 'sign_pdf: redirect to [%s]' % redirect
+    print "data:", data
+    redirect = '%s?%s' % (url_sign, data)
+    print 'demo_sign_pdf: redirect to [%s]' % redirect
     return HttpResponseRedirect(redirect)
+
+
+def demo(request):
+    url = request.build_absolute_uri('demo_sign_pdf')
+    return HttpResponse('<h1>Start the Online PDF Signing Demo</h1><a href="%s">start link</a>' % url)
+
