2552a268f307cc4d55a62bb76c1b4ebd8f2a9590
Marc van Woerkom <mvanwoerkom@acm.org>
2010-06-18 03:20:02 +0200

    * refactor from os.system to subprocess.Popen  * fix removal bug in renderer  * renderer can now render full rendersets and single pages / bigpages  * minimized ghostscript usage  * compensated bloody ghostscript numbering

--- mediaserver/renderer.py
+++ mediaserver/renderer.py
@@ -1,43 +1,54 @@
 # -*- coding: utf-8 -*-
 
 import os
+import subprocess
 
 from ecs.mediaserver.storage import Storage
 
 
 class Renderer(object):
-    def render_pages(self, pdf_name, pages, zoom, res_x, res_y, pdf_fname):
-        cmd = \
-            'gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -dGraphicsAlphaBits=4 ' + \
-            '-dPDFFitPage -dTextAlphaBits=4 -sPAPERSIZE=a4 ' + \
-            '-r%.5fx%.5f ' % (res_x, res_y) + \
-            '-dFirstPage=1 -dLastPage=%s ' % pages + \
-            '-sOutputFile=%s_%s_%%04d.png ' % (pdf_fname, zoom) + \
-            pdf_name
-        print cmd
-        os.system(cmd)
+    def render_pages(self, pdf_name, first_page, last_page, zoom, res_x, res_y, pdf_fname):
+        args = [ \
+            'gs', '-dSAFER', '-dBATCH', '-dNOPAUSE', '-sDEVICE=png16m', '-dGraphicsAlphaBits=4', \
+            '-dPDFFitPage', '-dTextAlphaBits=4', '-sPAPERSIZE=a4', \
+            '-r%.5fx%.5f' % (res_x, res_y), \
+            '-dFirstPage=%s' % first_page, '-dLastPage=%s' % last_page, \
+            '-sOutputFile=%s_%s_%%04d.png' % (pdf_fname, zoom), \
+            pdf_name \
+        ]
+        print args
+        p = subprocess.Popen(args)
+        sts = os.waitpid(p.pid, 0)[1]
+        print 'sts = %s' % sts
 
 
-    def render_page(self, png_name_in, png_name, cmd_compress, cmd_interlace):
-        cmd = 'convert %s%s%s %s' % (cmd_compress, cmd_interlace, png_name_in, png_name)
-        print cmd
-        os.system(cmd)
+    def render_page(self, png_name_in, png_name, args_compress, args_interlace):
+        args = [ 'convert' ] + args_compress + args_interlace + [ png_name_in, png_name ]
+        print args
+        p = subprocess.Popen(args)
+        sts = os.waitpid(p.pid, 0)[1]
+        print 'sts = %s' % sts
 
 
-    def render_bigpage(self, png_names, png_name, zoom, w, h, background, cmd_compress, cmd_interlace):
-        cmd = \
-            'montage %s%s' % (cmd_compress, cmd_interlace) + \
-            '-background \%s ' % background  + \
-            '-geometry %dx%d+0+0 ' % (w, h) + \
-            '-tile %s ' % zoom + \
-            '%s%s' % (png_names, png_name)
-        print cmd
-        os.system(cmd)
+    def render_bigpage(self, png_names, png_name, zoom, w, h, background, args_compress, args_interlace):
+        args = \
+            [ 'montage' ] + args_compress + args_interlace + \
+            [ '-background', background, '-geometry', '%dx%d+0+0' % (w, h), '-tile', zoom ] + \
+            png_names + [ png_name ]
+        print args
+        p = subprocess.Popen(args)
+        sts = os.waitpid(p.pid, 0)[1]
+        print 'sts = %s' % sts
 
 
     def remove_file(self, file_name):
         print 'removing "%s"' % file_name
         os.remove(file_name)
+
+
+    def rename_file(self, file_name_old, file_name_new):
+        print 'rename "%s" -> "%s"' % (file_name_old, file_name_new)
+        os.rename(file_name_old, file_name_new)
 
 
     def get_name(self, pdf_fname, page, zoom, is_bigpage=False, opt_compress=False, opt_interlace=False):
@@ -53,28 +64,31 @@ class Renderer(object):
         return '%s_%s%s_%04d%s.png' % (pdf_fname, zoom, big, page, suffix)
 
 
-    def render(self, image_set):
+    def render(self, image_set, single_mode=False, the_page=0, the_zoom=''):
         cm_per_inch = 2.54
         din_a4_x = 21.0
         din_a4_y = 29.7
         background = '#dddddd'
         pdf_name = image_set.set_data.pdf_name
         pages = image_set.set_data.pages
+        print 'single_mode: %s' % single_mode
         opt_compress = image_set.set_data.opt_compress
         opt_interlace = image_set.set_data.opt_interlace
         if opt_compress:
-            cmd_compress = '-compress Zip -quality 100 '  # keep blank at end
+            args_compress = [ '-compress', 'Zip', '-quality', '100' ]
         else:
-            cmd_compress = ''
+            args_compress = [ ]
         if opt_interlace:
-            cmd_interlace = '-interlace PNG '  # keep blank at end
+            args_interlace = [ '-interlace', 'PNG' ]
         else:
-            cmd_interlace = ''
+            args_interlace = [ ]
         print 'opt_compress: %s' % opt_compress
         print 'opt_interlace: %s' % opt_interlace
         errors = 0
         storage = Storage()
         for zoom in image_set.images:
+            if single_mode and zoom != the_zoom:
+                continue
             print '%s: ' % zoom,
             width = image_set.render_set.width
             height = image_set.render_set.height
@@ -85,53 +99,71 @@ class Renderer(object):
             res_x = w / (din_a4_x / cm_per_inch)
             res_y = h / (din_a4_y / cm_per_inch)
             pdf_fname, _ = os.path.splitext(os.path.basename(pdf_name))
-            self.render_pages(pdf_name, pages, zoom, res_x, res_y, pdf_fname)
             if zoom == '1':
-                for page in image_set.images[zoom]:
-                    png_name = self.get_name(pdf_fname, page, zoom)
+                if single_mode:
+                    page_first = the_page
+                    page_last = page_first
+                else:
+                    page_first = 1
+                    page_last = pages
+                page_set = range(page_first, page_last + 1)
+                self.render_pages(pdf_name, page_first, page_last, '1', res_x, res_y, pdf_fname)
+                if single_mode and page_first > 1:
+                    name_old = self.get_name(pdf_fname, 1, '1')
+                    name_new = self.get_name(pdf_fname, page_first, '1')
+                    self.rename_file(name_old, name_new)
+                for page in page_set:
+                    png_name = self.get_name(pdf_fname, page, '1')
                     if opt_compress or opt_interlace:
                         png_name_in = png_name
-                        png_name = self.get_name(pdf_fname, page, zoom, False, opt_compress, opt_interlace)
-                        self.render_page(png_name_in, png_name, cmd_compress, cmd_interlace)
+                        png_name = self.get_name(pdf_fname, page, '1', False, opt_compress, opt_interlace)
+                        self.render_page(png_name_in, png_name, args_compress, args_interlace)
                         self.remove_file(png_name_in)
-                    retval = storage.store_page(image_set.id, page, zoom, png_name)
+                    retval = storage.store_page(image_set.id, page, '1', png_name)
                     if not retval:
                         print 'error: storage failed'
                         errors += 1
-                    else:
-                        self.remove_file(png_name)
+                    self.remove_file(png_name)
             else:
                 bigpages = image_set.render_set.get_bigpages(zoom, pages)
-                bigpage_set = range(1, bigpages + 1)
-                subpages = image_set.render_set.get_subpages(zoom, pages)
-                w = width / subpages_x
-                h = height / subpages_y
+                subpages = subpages_x * subpages_y
+                if single_mode:
+                    bigpage_first = the_page
+                    bigpage_last = bigpage_first
+                else:
+                    bigpage_first = 1
+                    bigpage_last = bigpages
+                    self.render_pages(pdf_name, 1, pages, zoom, res_x, res_y, pdf_fname)
+                bigpage_set = range(bigpage_first, bigpage_last + 1)
                 for bigpage in bigpage_set:
                     page_first = (bigpage - 1) * subpages + 1
                     page_last = min(bigpage * subpages, pages)
                     page_set = range(page_first, page_last + 1)
-                    png_names = ''
-                    for page in page_set:
-                        png_names += self.get_name(pdf_fname, page, zoom) + ' '
-                    png_name = self.get_name(pdf_fname, bigpage, zoom, True, opt_compress, opt_interlace)
-                    self.render_bigpage(png_names, png_name, zoom, w, h, background, cmd_compress, cmd_interlace)
+                    if single_mode:
+                        self.render_pages(pdf_name, page_first, page_last, zoom, res_x, res_y, pdf_fname)
+                        if page_first > 1:
+                            delta = page_last - page_first
+                            d_set = range(0, delta + 1)
+                            d_set.reverse()
+                            for d in d_set:
+                                name_old = self.get_name(pdf_fname, 1 + d, zoom)
+                                name_new = self.get_name(pdf_fname, page_first + d, zoom)
+                                self.rename_file(name_old, name_new)
+                    png_names = [ ]
                     for page in page_set:
                         name = self.get_name(pdf_fname, page, zoom)
+                        png_names.append(name)
+                    png_name = self.get_name(pdf_fname, bigpage, zoom, True, opt_compress, opt_interlace)
+                    self.render_bigpage(png_names, png_name, zoom, w, h, background, args_compress, args_interlace)
+                    for name in png_names:
                         self.remove_file(name)
                     retval = storage.store_page(image_set.id, bigpage, zoom, png_name)
                     if not retval:
                         print 'error: storage failed'
                         errors += 1
-                    else:
-                        self.remove_file(png_name)
+                    self.remove_file(png_name)
         if errors > 0:
             print '%d error(s)' % errors
             return False
         else:
             return True
-
-
-    def rerender_bigpage(self, id, bigpage, zoom):
-        # id -> pdf_name, imageset, opt_compress, opt_interlace
-        return
-        
