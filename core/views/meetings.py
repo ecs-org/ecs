774be249f33400419e02fa32ace5cbf50ca0016d
Marc van Woerkom <mvanwoerkom@acm.org>
2010-06-25 03:24:41 +0200

    sorting continued

--- core/views/meetings.py
+++ core/views/meetings.py
@@ -388,11 +388,11 @@ def votes_signing(request, meeting_pk=None):
         c = votes.count()
         assert(c < 2)
         if c is 0:
-            vote = None
+            vote = ''
         else:
             vote = votes[0]
-        votes_list.append((top, vote))
-    votes_list.sort()
+        votes_list.append({'top': str(top), 'vote': vote})
+    print 'votes_list: "%s"' % votes_list
     response = render(request, 'meetings/votes_signing.html', {
         'meeting': meeting,
         'votes_list': votes_list,
