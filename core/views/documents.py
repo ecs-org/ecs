0626596008839b5c1e8c85a0a6b17f061cf07145
Johannes Dollinger <emulbreh@googlemail.com>
2010-07-12 13:10:29 +0200

    pdfviewer search integration

--- core/views/documents.py
+++ core/views/documents.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django import forms
 from haystack.views import SearchView
 from haystack.forms import HighlightedSearchForm
 from haystack.query import SearchQuerySet
@@ -6,9 +7,37 @@ from haystack.query import SearchQuerySet
 from ecs.core.models import Page, Document
 
 class DocumentSearchView(SearchView):
+    session_key = 'docsearch:q'
+
     def __call__(self, request, document_pk=None):
         self.document_pk = document_pk
         return super(DocumentSearchView, self).__call__(request)
+        
+    def get_query(self):
+        q = super(DocumentSearchView, self).get_query()
+        self.request.session[self.session_key] = q
+        return q
+        
+    def build_form(self, form_kwargs=None): # mostly copied from haystack.forms
+        data = None
+        kwargs = {
+            'load_all': self.load_all,
+        }
+        if form_kwargs:
+            kwargs.update(form_kwargs)
+        
+        q = self.request.session.get(self.session_key, '')
+        
+        if len(self.request.GET):
+            data = self.request.GET
+            
+        if q and not data:
+            data = {'q': q}
+        
+        if self.searchqueryset is not None:
+            kwargs['searchqueryset'] = self.searchqueryset
+        
+        return self.form_class(data, **kwargs)
 
     def get_results(self):
         results = super(DocumentSearchView, self).get_results()
@@ -30,7 +59,7 @@ def search_view_factory(view_class=SearchView, *args, **kwargs):
 
 document_search = search_view_factory(
     view_class=DocumentSearchView,
-    template='search/search.html',
+    template='documents/search.html',
     searchqueryset=SearchQuerySet().models(Page).order_by('doc_pk', 'page'), # '-doc_date', 'doc_type_name', '-doc_version',
     form_class=HighlightedSearchForm,
 )
