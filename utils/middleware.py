48da6de8fe3443842391f0b315b7ef997c6e99d1
Johannes Dollinger <emulbreh@googlemail.com>
2010-07-22 09:37:01 +0200

    Fixed signed cookie middleware for anonymous users

--- utils/middleware.py
+++ utils/middleware.py
@@ -9,7 +9,7 @@ class SignedCookiesMiddleware(object):
         for name, signed_value in request.COOKIES.items():
             try:
                 signed_name, user_pk, value = signed.loads(signed_value, extra_key=SIGNED_COOKIE_SECRET)
-                if user_pk != request.user.pk or signed_name != name:
+                if user_pk != getattr(request.user, 'pk', None) or signed_name != name:
                     raise ValueError()
                 request.COOKIES[name] = value
             except (signed.BadSignature, ValueError):
@@ -20,7 +20,7 @@ class SignedCookiesMiddleware(object):
             if morsel['max-age'] == 0:
                 # Deleted cookies don't need to be signed
                 continue
-            response.set_cookie(name, signed.dumps((name, request.user.pk, morsel.value), extra_key=SIGNED_COOKIE_SECRET),
+            response.set_cookie(name, signed.dumps((name, getattr(request.user, 'pk', None), morsel.value), extra_key=SIGNED_COOKIE_SECRET),
                 max_age=morsel['max-age'],
                 expires=morsel['expires'],
                 path=morsel['path'],
