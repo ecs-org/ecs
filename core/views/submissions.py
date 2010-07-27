bc1d1feb4eb71ca2b39f8e9ce93f8024dc499d53
Johannes Dollinger <emulbreh@googlemail.com>
2010-07-27 18:27:53 +0200

    really serve pdf

--- core/views/submissions.py
+++ core/views/submissions.py
@@ -265,11 +265,11 @@ def view_submission_form(request, submission_form_pk=None):
 
 def submission_pdf(request, submission_form_pk=None):
     submission_form = get_object_or_404(SubmissionForm, pk=submission_form_pk)
-    return render(request, 'db/submissions/xhtml2pdf/view.html', {
-        'paper_form_fields': paper_forms.get_field_info_for_model(SubmissionForm),
-        'submission_form': submission_form,
-        'documents': submission_form.documents.filter(deleted=False).order_by('doctype__name', '-date'),
-    })
+    #return render(request, 'db/submissions/xhtml2pdf/view.html', {
+    #    'paper_form_fields': paper_forms.get_field_info_for_model(SubmissionForm),
+    #    'submission_form': submission_form,
+    #    'documents': submission_form.documents.filter(deleted=False).order_by('doctype__name', '-date'),
+    #})
     
     filename = 'ek-%s-Einreichung.pdf' % submission_form.submission.ec_number.replace('/','-')
     
