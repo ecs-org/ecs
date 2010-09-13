e854c8dc85878ac1c3dcee9d35d798cf7d7c10f3
elchaschab <elchaschab@baudolino.site>
2010-09-13 12:27:56 +0200

    renderer complete

--- mediaserver/renderer.py
+++ mediaserver/renderer.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
+from ecs.mediaserver.docshot import Docshot
 
 class Renderer(object):
-    def renderPDFMontage(self, pdfdata, first, last, width, tiles_x, tiles_y, aspect=1.41428, dpi=72, depth=8):
+    def renderPDFMontage(self, pdfdocument, width, tiles_x, tiles_y, aspect_ratio=1.41428, dpi=72, depth=8):
         margin_x = 0
         margin_y = 0
         
@@ -11,22 +12,25 @@ class Renderer(object):
             margin_x = 4
             margin_y = 4
 
-        height=width/aspect
+        height=width/aspect_ratio
 
         tile_width = (width / tiles_x) - margin_x
         tile_height = (height / tiles_y) - margin_y
 
-        args = 'montage -geometry %dx%d+%d+%d -tile %dx%d -density %d -depth %d -[%d-%d] PNG:-' % (tile_height, tile_width,margin_x, margin_y,tiles_x, tiles_y, dpi, depth, first, last)
-        print args
-
+        args = 'montage -geometry %dx%d+%d+%d -tile %dx%d -density %d -depth %d - PNG:-' % (tile_height, tile_width,margin_x, margin_y,tiles_x, tiles_y, dpi, depth)
         popen = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
-        
-        raw_pngstream, stderr = popen.communicate(pdfdata)
+        raw_pngstream, stderr = popen.communicate(pdfdocument.getData())
 
         if popen.returncode != 0:
-            raise IOError('ghostscript rendering document to images returned error code % i , stderr: % s' % (popen.returncode, stderr))
+            raise IOError('montage returned error code % i , stderr: % s' % (popen.returncode, stderr))
 
-        return self._split_raw_pngstream(raw_pngstream) 
+        docshots = []
+        pagenr = 0
+        for png_data in self._split_raw_pngstream(raw_pngstream):
+            docshots.append(Docshot(tiles_x, tiles_y, width, pagenr, uuid=pdfdocument.uuid, data=png_data))
+            pagenr = pagenr + 1
+
+        return docshots
 
     def _split_raw_pngstream(self, raw_pngstream):
         import binascii
@@ -50,4 +54,3 @@ class Renderer(object):
         raw_pngs.append(raw_pngstream[offsets[-1]:])
     
         return raw_pngs 
-
