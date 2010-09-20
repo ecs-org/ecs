7c97beb5c6181356d7ef05fd57da438bd4f283cb
amir hassan <amir@viel-zu.org>
2010-09-20 14:03:07 +0200

    fixed: uuids are always used in their hex representation introduced list_docshots

--- mediaserver/urls.py
+++ mediaserver/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns(
     '',
-    url(r'^(?P<uuid>[0-9a-f-]{36})/(?P<tiles_x>\d+)[xX](?P<tiles_y>\d+)/(?P<width>\d+)/(?P<pagenr>\d+)/$', 'ecs.mediaserver.views.docshot'),
-    url(r'^download/(?P<uuid>[0-9a-f-]{36})/$', 'ecs.mediaserver.views.download_pdf'),
-    
+    url(r'^(?P<uuid>[0-9a-f]{32})/(?P<tiles_x>\d+)[xX](?P<tiles_y>\d+)/(?P<width>\d+)/(?P<pagenr>\d+)/$', 'ecs.mediaserver.views.docshot'),
+    url(r'^download/(?P<uuid>[0-9a-f]{32})/$', 'ecs.mediaserver.views.download_pdf'),
+    url(r'^listdocshots/(?P<uuid>[0-9a-f]{32})/$', 'ecs.mediaserver.views.list_docshots'),
     url(r'^upload/$', 'ecs.mediaserver.views.upload_pdf'), # FIXME: TEST Function, not production
 )
