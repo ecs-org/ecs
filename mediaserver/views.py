ad38ce833158020e6f0ac1f74f15428dea22e763
amir hassan <amir@viel-zu.org>
2010-09-20 14:05:06 +0200

    adapted to new docprovider implemented s3 url checking and generation proper handling of http responses

--- mediaserver/views.py
+++ mediaserver/views.py
@@ -1,42 +1,67 @@
 # -*- coding: utf-8 -*-
 
-import email.utils
-import time
+from django.http import HttpResponse, HttpResponseNotFound, \
+    HttpResponseBadRequest
 
-from django.http import HttpResponse, HttpResponseNotFound
-from django.conf import settings
-
-from ecs.documents.models import Document
-# FIXME/mediaserver: from ecs.mediaserver.imageset import ImageSet
-# FIXME/mediaserver: from ecs.mediaserver.storage import Cache, SetData
-from ecs.utils import hashauth
 from ecs.mediaserver.documentprovider import DocumentProvider
-# FIXME/mediaserver: from ecs.mediaserver.docshot import Docshot
 from ecs.mediaserver.forms.pdfuploadform import PdfUploadForm
 from django.shortcuts import render_to_response
-# FIXME/mediaserver: from ecs.mediaserver.document import PdfDocument
+from ecs.mediaserver.cacheobjects import MediaBlob, Docshot
+from uuid import uuid4, UUID
+from ecs.utils import s3utils
+from time import time
+import json
 
 docprovider = DocumentProvider()
 
 def docshot(request, uuid, tiles_x, tiles_y, width, pagenr):
-    print 'docshot request: %s, %s, %s, %s, %s' % (uuid, tiles_x, tiles_y, width, pagenr)
+    if not s3utils.verifyExpiringUrlString(request.get_full_path()):
+        return HttpResponseBadRequest("Invalid expiring url");
     
-    f = docprovider.fetchDocshot(Docshot(tiles_x, tiles_y, width, pagenr, uuid=uuid))
-    
-    return HttpResponse(f.read(), mimetype='image/png')
-   
+    f = docprovider.getDocshot(Docshot(MediaBlob(UUID(uuid)), tiles_x, tiles_y, width, pagenr))
+
+    if f:
+        return HttpResponse(f.read(), mimetype='image/png')
+    else:
+        return HttpResponseNotFound()
+
+#FIXME remove test code
 def upload_pdf(request):
     if request.method == 'POST':
         form = PdfUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            file=request.FILES['pdffile']
-            doc = PdfDocument(data=file.read())
-            docprovider.store(doc, use_vault=True)
+            file = request.FILES['pdffile']
+            docprovider.addBlob(MediaBlob(uuid4()), file.read())
             return HttpResponse("saved")
     else:
         form = PdfUploadForm(request.POST, request.FILES)
+
     return render_to_response('mediaserver/pdfupload.html', {'form': form})
 
 def download_pdf(request, uuid):
-    f = docprovider.fetchDocument(PdfDocument(uuid=uuid))
-    return HttpResponse(f.read(), mimetype='application/pdf')
+    if not s3utils.verifyExpiringUrlString(request.get_full_path()):
+        return HttpResponseBadRequest("Invalid expiring url");
+    
+    f = docprovider.getBlob(MediaBlob(UUID(uuid)))
+
+    if f:
+        return HttpResponse(f.read(), mimetype='application/pdf')
+    else:
+        return HttpResponseNotFound()
+    
+def list_docshots(request, uuid):
+    tiles = [ 1, 3, 5 ]
+    width = [ 800, 768 ] 
+    links = [];
+
+    doc = MediaBlob(UUID(uuid));
+    for t in tiles:
+        for w in width:
+            pagenum = 1
+            while docprovider.getDocshot(Docshot(doc, t, t, w , pagenum)):
+                bucket = "/mediaserver/%s/%dx%d/%d/%d/" % (uuid, t, t, w, pagenum)
+                expiringUrl = s3utils.createExpiringUrl("http://localhost:8000", bucket, '', "LocalFileStorageVault", int(time()) + 60)
+                links.append(expiringUrl)
+                pagenum += 1
+    
+    return HttpResponse(json.dumps(links), mimetype='text/javascript')
