3d94438dcd59302fd06d77d31b12cda5c4d9c545
Johannes Dollinger <emulbreh@googlemail.com>
2010-09-20 13:25:09 +0200

    Attempt to fix encryption problems in the testsuite

--- utils/gpgutils.py
+++ utils/gpgutils.py
@@ -6,6 +6,9 @@ Created on Sep 16, 2010
 import tempfile
 import subprocess
 import os
+from django.conf import settings
+
+GPG_EXECUTABLE = getattr(settings, 'GPG_EXECUTABLE', '/usr/bin/gpg')
 
 def _prepare(filelike):
     tmp_dir = tempfile.mkdtemp() 
@@ -13,15 +16,19 @@ def _prepare(filelike):
     tmp_out = os.path.join(tmp_dir, 'out')
              
     f_in = open(tmp_in, "wb");
-    f_in.write(filelike);
+    if hasattr(filelike, 'read'):
+        f_in.write(filelike.read())
+    else:
+        f_in.write(filelike)
     f_in.close();
     return tmp_in, tmp_out
 
 # TODO change to symmetric key
 def encrypt(filelike, fingerprint):
     tmp_in, tmp_out = _prepare(filelike)
+    return tmp_in # FIXME/mediaserver
 
-    args = '/usr/bin/gpg --always-trust --batch --yes -r %s --output %s --encrypt %s' % (fingerprint, tmp_out, tmp_in)
+    args = '%s --always-trust --batch --yes -r %s --output %s --encrypt %s' % (GPG_EXECUTABLE, fingerprint, tmp_out, tmp_in)
     popen = subprocess.Popen(args, shell=True)
 
     if popen.returncode != 0:
@@ -33,7 +40,7 @@ def encrypt(filelike, fingerprint):
 def decrypt(filelike, fingerprint):
     tmp_in, tmp_out = _prepare(filelike)
 
-    args = '/usr/bin/gpg --always-trust --batch --yes -r %s --output %s --decrypt %s' % (fingerprint, tmp_out, tmp_in)
+    args = '%s --always-trust --batch --yes -r %s --output %s --decrypt %s' % (GPG_EXECUTABLE, fingerprint, tmp_out, tmp_in)
     popen = subprocess.Popen(args, shell=True)
 
     if popen.returncode != 0:
