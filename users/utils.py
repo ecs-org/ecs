602be089f4233b15e307b3658cebe8b4b60408e9
Johannes Dollinger <emulbreh@googlemail.com>
2010-09-20 13:28:27 +0200

    Added a sudo contextmanager

--- users/utils.py
+++ users/utils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from contextlib import contextmanager
 from django.contrib.auth.models import User
 
 from ecs.users.middleware import current_user_store
@@ -8,4 +8,13 @@ def get_current_user():
     if hasattr(current_user_store, 'user'):
         return current_user_store.user
     else:
+        # FIXME: what do we return during testing or management commands? Do we really query the user table every time?
         return User.objects.get(username='root')
+        
+@contextmanager
+def sudo(user):
+    from ecs.users.middleware import current_user_store
+    _previous_user = getattr(current_user_store, 'user', None)
+    current_user_store.user = user
+    yield
+    current_user_store = _previous_user
