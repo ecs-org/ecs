63ff43be4aa5dd2052c3a27628395edc7c33112c
Johannes Dollinger <emulbreh@googlemail.com>
2010-09-23 13:15:51 +0200

    Fixed storagevault import

--- utils/storagevault.py
+++ utils/storagevault.py
@@ -4,17 +4,17 @@ Created on Aug 19, 2010
 @author: amir
 '''
 
-
+from django.utils.importlib import import_module
+from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
 from ecs.utils.diskbuckets import DiskBuckets
 
 def getVault():
-    vault_path = settings.STORAGE_VAULT.split('.')
-    vault_module = __import__('.'.join(vault_path[:-1]), fromlist=vault_path[:-2])
-    vault = getattr(vault_module, vault_path[-1])
-            
-    if vault is None:
-        raise KeyError("No Vault implementation for name: %s" % (settings.STORAGE_VAULT))
+    module, class_name = settings.STORAGE_VAULT.rsplit('.', 1)
+    try:
+        vault = getattr(import_module(module), class_name)
+    except ImportError:
+        raise ImproperlyConfigured("No Vault implementation for name: %s" % (settings.STORAGE_VAULT))
     return vault()
     
 class LocalFileStorageVault(DiskBuckets):
