15c02e094f4778dea8bc889c4402ee90f936ad18
amir hassan <amir@viel-zu.org>
2010-09-23 12:21:39 +0200

    removed test code

--- mediaserver/urls.py
+++ mediaserver/urls.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     '',
     url(r'^(?P<uuid>[0-9a-f]{32})/(?P<tiles_x>\d+)[xX](?P<tiles_y>\d+)/(?P<width>\d+)/(?P<pagenr>\d+)/$', 'ecs.mediaserver.views.docshot'),
     url(r'^download/(?P<uuid>[0-9a-f]{32})/$', 'ecs.mediaserver.views.download_pdf'),
-    url(r'^listdocshots/(?P<uuid>[0-9a-f]{32})/$', 'ecs.mediaserver.views.list_docshots'),
-    url(r'^upload/$', 'ecs.mediaserver.views.upload_pdf'), # FIXME: TEST Function, not production
 )
+
--- mediaserver/views.py
+++ mediaserver/views.py
@@ -25,19 +25,6 @@ def docshot(request, uuid, tiles_x, tiles_y, width, pagenr):
     else:
         return HttpResponseNotFound()
 
-#FIXME remove test code
-def upload_pdf(request):
-    if request.method == 'POST':
-        form = PdfUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            file = request.FILES['pdffile']
-            docprovider.addBlob(MediaBlob(uuid4()), file.read())
-            return HttpResponse("saved")
-    else:
-        form = PdfUploadForm(request.POST, request.FILES)
-
-    return render_to_response('mediaserver/pdfupload.html', {'form': form})
-
 def download_pdf(request, uuid):
     if not s3utils.verifyExpiringUrlString(request.get_full_path()):
         return HttpResponseBadRequest("Invalid expiring url");
@@ -49,19 +36,3 @@ def download_pdf(request, uuid):
     else:
         return HttpResponseNotFound()
     
-def list_docshots(request, uuid):
-    tiles = [ 1, 3, 5 ]
-    width = [ 800, 768 ] 
-    links = [];
-
-    doc = MediaBlob(UUID(uuid));
-    for t in tiles:
-        for w in width:
-            pagenum = 1
-            while docprovider.getDocshot(Docshot(doc, t, t, w , pagenum)):
-                bucket = "/mediaserver/%s/%dx%d/%d/%d/" % (uuid, t, t, w, pagenum)
-                expiringUrl = s3utils.createExpiringUrl("http://localhost:8000", bucket, '', "LocalFileStorageVault", int(time()) + 60)
-                links.append(expiringUrl)
-                pagenum += 1
-    
-    return HttpResponse(json.dumps(links), mimetype='text/javascript')
