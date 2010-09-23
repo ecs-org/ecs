0f9da7fe7c9ec0bcc6d0f01f43121e2fe7c26303
Martin Natano <natano@natano.net>
2010-09-23 13:04:24 +0200

    fix class loading for storagevault

--- utils/storagevault.py
+++ utils/storagevault.py
@@ -9,7 +9,9 @@ from django.conf import settings
 from ecs.utils.diskbuckets import DiskBuckets
 
 def getVault():
-    vault = globals()[settings.STORAGE_VAULT]
+    vault_path = settings.STORAGE_VAULT.split('.')
+    vault_module = __import__('.'.join(vault_path[:-1]), fromlist=vault_path[:-2])
+    vault = getattr(vault_module, vault_path[-1])
             
     if vault is None:
         raise KeyError("No Vault implementation for name: %s" % (settings.STORAGE_VAULT))
