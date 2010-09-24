4d24fbe6ec170e5a03d6da05fafa47c95e2a1d4e
amir hassan <amir@viel-zu.org>
2010-09-24 18:03:27 +0200

    reenabled all tests

--- mediaserver/tests/__init__.py
+++ mediaserver/tests/__init__.py
@@ -1,3 +1,3 @@
-#from ecs.mediaserver.tests.pdfviewerinterface import PdfViewerInterface
+from ecs.mediaserver.tests.pdfviewerinterface import PdfViewerInterface
 from ecs.mediaserver.tests.renderer import RendererTest
-#from ecs.mediaserver.tests.diskbuckets import DiskBucketsTest
+from ecs.mediaserver.tests.diskbuckets import DiskBucketsTest
