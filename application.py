d45819031b22e0cca86e98e4a635f6d3a0f2b4d7
Martin Natano <natano@natano.net>
2010-10-03 21:10:31 +0200

    fix

--- application.py
+++ application.py
@@ -20,7 +20,8 @@ sprint7_bundle = """
 psycopg2:req:apt:apt-get:libpq-dev
 psycopg2:req:mac:macports:postgresql84-server
 psycopg2:req:suse:zypper:postgresql-devel
-psycopg2:req:openbsd:pkg:postgresql
+psycopg2:req:openbsd:pkg:postgresql-server
+psycopg2:req:openbsd:pkg:postgresql-client
 psycopg2:inst:!win:pypi:psycopg2
 psycopg2:instbin:win:http://www.stickpeople.com/projects/python/win-psycopg/psycopg2-2.0.13.win32-py2.6-pg8.4.1-release.exe
 
