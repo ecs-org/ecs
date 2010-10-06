78564622c17df56ab71a5869e6874447975e225c
amir hassan <amir@viel-zu.org>
2010-10-06 16:16:29 +0200

    whitewash test

---
+++ ecsmail/tests/persil.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 6, 2010
+
+@author: amir
+'''
+from ecs.ecsmail.persil import whitewash
+from ecs.utils.testcases import EcsTestCase
+
+class PersilTest(EcsTestCase):
+    testhtml = '<html><body><p>bla&auml;</p></body></html>'
+    result_decoded_entities = u'bla\xe4'
+    result_encoded_entities = u'bla&auml;'
+    
+    def testWhitewashing(self):
+        self.assertEqual(whitewash(self.testhtml, True), self.result_decoded_entities)
+        self.assertEqual(whitewash(self.testhtml, False), self.result_encoded_entities)
+    
