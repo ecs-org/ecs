a5aac7d2c385a3074192cb94587803fd4129caa3
Martin Natano <natano@natano.net>
2010-10-26 14:45:34 +0200

    some more communication tests

--- communication/tests.py
+++ communication/tests.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 
 from ecs.communication.models import Message, Thread
 from ecs.utils.testcases import EcsTestCase
+from ecs.core.models import Submission
+from ecs.communication.models import Thread
 
-class MessageTest(EcsTestCase):
+class CommunicationTest(EcsTestCase):
     def setUp(self):
         for name in ('alice', 'bob', ):
             user = User(username=name)
@@ -12,21 +14,32 @@ class MessageTest(EcsTestCase):
             user.save()
             setattr(self, name, user)
     
+        self.thread = Thread.objects.create(
+            subject='test',
+            sender=self.alice,
+            receiver=self.bob,
+            task=None,
+            submission=None,
+        )
+        self.thread.add_message(self.alice, 'test')
+
+        return super(CommunicationTest, self).setUp()
+    
     def test_send_message(self):
         self.client.login(username='alice', password='...')
 
         response = self.client.get(reverse('ecs.communication.views.send_message'))
         self.failUnlessEqual(response.status_code, 200)
         
+        message_count = Message.objects.count()
         response = self.client.post(reverse('ecs.communication.views.send_message'), {
             'subject': 'SUBJECT',
             'text': 'TEXT',
             'receiver': self.bob.pk,
         })
         self.failUnlessEqual(response.status_code, 302)
-        
-        self.failUnlessEqual(Message.objects.count(), 1)
-        message = Message.objects.get()
+        self.failUnlessEqual(Message.objects.count(), message_count+1)
+        message = Message.objects.all().order_by('-pk')[0]
         self.failUnlessEqual(message.sender, self.alice)
         self.failUnlessEqual(message.receiver, self.bob)
         
@@ -50,6 +63,19 @@ class MessageTest(EcsTestCase):
 
         self.client.logout()
 
+    def test_send_message_invalid_submission(self):
+        self.client.login(username='alice', password='...')
+        try:
+            non_existing_pk = Submission.objects.all().order_by('-pk')[0].pk+1
+        except IndexError:
+            non_existing_pk = 42
+        response = self.client.post(reverse('ecs.communication.views.send_message', kwargs={'submission_pk': non_existing_pk}), {
+            'subject': 'SUBJECT',
+            'text': 'TEXT',
+            'receiver': self.bob.pk,
+        })
+        self.failUnlessEqual(response.status_code, 404)
+
     def test_reply_message(self):
         thread = Thread.objects.create(subject='foo', sender=self.alice, receiver=self.bob)
         message = thread.add_message(self.alice, text="text")
@@ -64,5 +90,38 @@ class MessageTest(EcsTestCase):
         self.failUnlessEqual(message.receiver, self.alice)
         self.failUnlessEqual(message.sender, self.bob)
 
+    def test_read_thread(self):
+        self.client.login(username='alice', password='...')
+        response = self.client.get(reverse('ecs.communication.views.read_thread', kwargs={'thread_pk': self.thread.pk}))
+        self.failUnlessEqual(response.status_code, 200)
         
+        self.client.login(username='bob', password='...')
+        response = self.client.get(reverse('ecs.communication.views.read_thread', kwargs={'thread_pk': self.thread.pk}))
+        self.failUnlessEqual(response.status_code, 200)
         
+    def test_bump_message(self):
+        self.client.login(username='alice', password='...')
+        response = self.client.get(reverse('ecs.communication.views.bump_message', kwargs={'message_pk': self.thread.last_message.pk}))
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_close_thread(self):
+        self.client.login(username='alice', password='...')
+        response = self.client.get(reverse('ecs.communication.views.close_thread', kwargs={'thread_pk': self.thread.pk}))
+        self.failUnlessEqual(response.status_code, 200)
+        
+        self.client.login(username='bob', password='...')
+        response = self.client.get(reverse('ecs.communication.views.close_thread', kwargs={'thread_pk': self.thread.pk}))
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_incoming_message_widget(self):
+        self.client.login(username='alice', password='...')
+        response = self.client.get(reverse('ecs.communication.views.incoming_message_widget'))
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_outgoing_message_widget(self):
+        self.client.login(username='alice', password='...')
+        response = self.client.get(reverse('ecs.communication.views.outgoing_message_widget'))
+        self.failUnlessEqual(response.status_code, 200)
+
+        
+
