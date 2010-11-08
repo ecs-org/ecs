603459eaa81f424d32209ae5298c491b51874afa
amir hassan <amir@viel-zu.org>
2010-11-08 14:20:11 +0100

    removed demo urls

--- pdfsigner/urls.py
+++ pdfsigner/urls.py
@@ -5,10 +5,4 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns(
     '',
-    url(r'^demo$', 'ecs.pdfsigner.views.demo'),
-    url(r'^sign$', 'ecs.pdfsigner.views.demo_sign'),
-    url(r'^send$', 'ecs.pdfsigner.views.demo_send'),
-    url(r'^error$', 'ecs.pdfsigner.views.demo_error'),
-    url(r'^receive$', 'ecs.pdfsigner.views.demo_receive'),
-    url(r'^receive;jsessionid=(?P<jsessionid>[^?]*)$', 'ecs.pdfsigner.views.demo_receive'),
 )
