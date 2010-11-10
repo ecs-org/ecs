e36955c1c9574b93d25d16ea8f7399d17cbc7534
amir hassan <amir@viel-zu.org>
2010-11-10 12:24:02 +0100

    using django cache framework to deposit vote pdf files.

--- pdfsigner/UnsignedVoteDepot.py
+++ pdfsigner/UnsignedVoteDepot.py
@@ -7,26 +7,25 @@ Created on Nov 8, 2010
 from uuid import uuid4
 import tempfile
 from ecs.utils.pdfutils import pdf_barcodestamp
+from django.core.cache import cache
 
 class UnsignedVoteDepot(object):
-    depot = dict();
+    __DEFAULT_TIMEOUT_SEC=60
+    depot = dict()
    
     def __gen_id(self):
-        return uuid4().get_hex();        
+        return uuid4().get_hex()        
 
     def deposit(self, pdf_file, display_name):
-        pdf_id = self.__gen_id();
+        pdf_id = self.__gen_id()
 
         t_file = tempfile.NamedTemporaryFile(prefix='vote_sign_', suffix='.pdf', delete=False)
         pdf_barcodestamp(pdf_file, t_file, pdf_id)
         t_file.seek(0)
-
-        self.depot[id] = (t_file, display_name);
+        cache.set(id, (t_file, display_name), self.__DEFAULT_TIMEOUT_SEC)
         return id
     
     def pickup(self, id):
-        t_file, display_name = self.depot[id]
-        del self.depot[id]
+        t_file, display_name = cache.get(id)
+        cache.delete(id)
         return t_file, display_name
-    
-votesDepot = UnsignedVoteDepot();
