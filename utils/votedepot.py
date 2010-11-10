81cefbdd88e8a785b538f499fbf21c50090d3cc4
amir hassan <amir@viel-zu.org>
2010-11-10 14:01:04 +0100

    fixed temp pdf vote serialization

--- utils/votedepot.py
+++ utils/votedepot.py
@@ -16,16 +16,21 @@ class VoteDepot(object):
     def __gen_id(self):
         return uuid4().get_hex()        
 
-    def deposit(self, pdf_file, display_name):
+    def deposit(self, pdf_data, display_name):
         pdf_id = self.__gen_id()
 
-        t_file = tempfile.NamedTemporaryFile(prefix='vote_sign_', suffix='.pdf', delete=False)
-        pdf_barcodestamp(pdf_file, t_file, pdf_id)
-        t_file.seek(0)
-        cache.set(id, (t_file, display_name), self.__DEFAULT_TIMEOUT_SEC)
-        return id
+        t_in = tempfile.NamedTemporaryFile(prefix='vote_sign_', suffix='.pdf', delete=False)
+        t_out = tempfile.NamedTemporaryFile(prefix='vote_sign_stamped_', suffix='.pdf', delete=False)
+        t_in.write(pdf_data)
+        t_in.seek(0)
+        pdf_barcodestamp(t_in, t_out, pdf_id)
+        t_in.close();
+        t_out.seek(0)
+
+        cache.set(pdf_id, (t_out.name, display_name), self.__DEFAULT_TIMEOUT_SEC)
+        return pdf_id
     
     def pickup(self, id):
-        t_file, display_name = cache.get(id)
+        t_file_name, display_name = cache.get(id)
         cache.delete(id)
-        return t_file, display_name
+        return open(t_file_name,'rwb'), display_name
