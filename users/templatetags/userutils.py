a58e82f27bd8fcc95981846f53c93737c741f05a
Martin Natano <natano@natano.net>
2010-11-15 14:32:18 +0100

    tab filter for meeting detail view

--- users/templatetags/userutils.py
+++ users/templatetags/userutils.py
@@ -5,3 +5,8 @@ register = Library()
 @register.filter
 def has_flag(user, flag):
     return getattr(user.ecs_profile, flag, False)
+
+@register.filter
+def is_member_of(user, groupname):
+    return bool(user.groups.filter(name=groupname).count())
+
