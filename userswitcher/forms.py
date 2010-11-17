d8cbd65b9a28f15a37e1361a08c016c4e7ed8fbe
Ludwig Hammel <ludwig.hammel@gmail.com>
2010-11-17 17:46:05 +0100

    ecs.userswitcher I18N

--- userswitcher/forms.py
+++ userswitcher/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 class UserSwitcherForm(forms.Form):
     #user = forms.ModelChoiceField(User.objects.filter(username__contains="Testuser").order_by('username'), required=False, label=u'Benutzer')
     user = forms.ModelChoiceField(
         User.objects.filter(groups__name='userswitcher_target').order_by('username'), 
         required=False, 
-        label=u'Benutzer', 
+        label=_(u'user'), 
         widget=forms.Select(attrs={'id': 'userswitcher_input'})
     )
