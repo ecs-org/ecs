0d4f16ad3829ff87431c96f73f077ca7a8a2bde8
Ludwig Hammel <ludwig.hammel@gmail.com>
2010-11-17 17:22:15 +0100

    ecs.tasks I18N

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 class DeclineTaskForm(forms.Form):
     message = forms.CharField(required=False)
     
 
-TASK_MANAGEMENT_CHOICES = ('delegate', 'message',)
-TASK_QUESTION_TYPE = ('callback', 'somebody', 'related', )
+TASK_MANAGEMENT_CHOICES = [('delegate', _('delegate')),('message', _('message'))]
+TASK_QUESTION_TYPE = [('callback', _('callback')), ('somebody', _('somebody')), ('related', _('related') )]
 
 class TaskChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, task):
         return u"%s (%s)" % (task.assigned_to, task)
 
 class ManageTaskForm(forms.Form):
-    action = forms.ChoiceField(choices=zip(TASK_MANAGEMENT_CHOICES, TASK_MANAGEMENT_CHOICES))
-    question_type = forms.ChoiceField(required=False, choices=zip(TASK_QUESTION_TYPE, TASK_QUESTION_TYPE))
-    assign_to = forms.ModelChoiceField(queryset=User.objects.all(), required=False, empty_label='<Gruppe>')
+    action = forms.ChoiceField(choices=TASK_MANAGEMENT_CHOICES)
+    question_type = forms.ChoiceField(required=False, choices=TASK_QUESTION_TYPE)
+    assign_to = forms.ModelChoiceField(queryset=User.objects.all(), required=False, empty_label=_('<group>'))
     question = forms.CharField(required=False, widget=forms.Textarea())
     receiver = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
     
@@ -30,7 +31,7 @@ class ManageTaskForm(forms.Form):
         if task.choices:
             self.fields['action'].choices += [('complete_%s' % i, choice[i]) for i, choice in enumerate(task.choices)]
         else:
-            self.fields['action'].choices += [('complete', 'complete')]
+            self.fields['action'].choices += [('complete', _('complete'))]
         
     def get_choice(self):
         if not hasattr(self, 'choice_index'):
@@ -52,20 +53,20 @@ class ManageTaskForm(forms.Form):
         action = cd.get('action')
         if action == 'delegate':
             if 'assign_to' not in cd:
-                self._errors['assign_to'] = self.error_class([u'Sie müssen einen Benutzer auswählen.'])
+                self._errors['assign_to'] = self.error_class([_(u'You must select a user.')])
         elif action == 'message':
             question_type = cd.get('question_type')
             if question_type == 'callback':
                 if not cd.get('callback_task'):
-                    self._errors['callback_task'] = self.error_class([u'Sie müssen einen Benutzer auswählen.'])
+                    self._errors['callback_task'] = self.error_class([_(u'You must select a user.')])
             elif question_type == 'somebody':
                 if not cd.get('receiver'):
-                    self._errors['receiver'] = self.error_class([u'Sie müssen einen Benutzer auswählen.'])
+                    self._errors['receiver'] = self.error_class([_(u'You must select a user.')])
             elif question_type == 'related':
                 if not cd.get('related_task'):
-                    self._errors['related_task'] = self.error_class([u'Sie müssen einen Benutzer auswählen.'])
+                    self._errors['related_task'] = self.error_class([_(u'You must select a user.')])
             elif not question_type:
-                self._errors['question_type'] = self.error_class([u'Sie müssen einen Adressaten auswählen'])
+                self._errors['question_type'] = self.error_class([_(u'You must select a recipient.')])
 
         return cd
 
@@ -81,8 +82,8 @@ class TaskListFilterForm(forms.Form):
     proxy = forms.BooleanField(required=False)
 
     sorting = forms.ChoiceField(required=False, choices=(
-        ('deadline', 'Deadline'),
-        ('oldest', 'Oldest'),
-        ('newest', 'Newest'),
+        ('deadline', _('Deadline')),
+        ('oldest', _('Oldest')),
+        ('newest', _('Newest')),
     ))
 
