e9809bca8f3043567f7adabd21780ecfe892fef1
amir hassan <amir@viel-zu.org>
2010-11-18 10:43:20 +0100

    prevent mediaserver from barcode stamping votes. reenable barcodestamping of votes.

--- core/views/votes.py
+++ core/views/votes.py
@@ -112,16 +112,13 @@ def vote_sign(request, meeting_pk=None, vote_pk=None):
     t_out = tempfile.NamedTemporaryFile(prefix='vote_sign_stamped_', suffix='.pdf', delete=False)
     t_in.write(pdf_data)
 
-    #t_in.seek(0)
-    #pdf_barcodestamp(t_in, t_out, document_uuid)
-    #t_in.close();
+    t_in.seek(0)
+    pdf_barcodestamp(t_in, t_out, document_uuid)
+    t_in.close();
 
-    #t_out.seek(0)
-    #pdf_data_stamped = t_out.read()
-    #t_out.close();
-
-    t_in.seek(0);
-    pdf_data_stamped = t_in.read()
+    t_out.seek(0)
+    pdf_data_stamped = t_out.read()
+    t_out.close();
 
     pdfas_id = votesDepot.deposit(pdf_data_stamped, html_preview, document_uuid, pdf_name)
     return sign(request, pdfas_id, len(pdf_data_stamped), pdf_name)
@@ -175,7 +172,7 @@ def vote_sign_receive(request, meeting_pk=None, vote_pk=None, jsessionid=None):
          
         d = datetime.datetime.now()
         doctype = DocumentType.objects.create(name="Votum", identifier="votes")
-        document = Document.objects.create(uuid_document=votedoc["uuid"], parent_object=vote, doctype=doctype, file=File(t_pdfas), original_file_name=votedoc["name"], date=d)
+        document = Document.objects.create(uuid_document=votedoc["uuid"], parent_object=vote, branding='n', doctype=doctype, file=File(t_pdfas), original_file_name=votedoc["name"], date=d)
         t_pdfas.close()
         os.remove(t_pdfas.name)
         
