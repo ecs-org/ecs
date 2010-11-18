ea4a61c69aad7c1fe4cc1f87321cc73b4242b71c
Johannes Dollinger <emulbreh@googlemail.com>
2010-11-18 16:12:29 +0100

    mark developer views

--- billing/urls.py
+++ billing/urls.py
@@ -5,11 +5,6 @@ from django.conf import settings
 urlpatterns = patterns('ecs.billing.views',
     url(r'^submissions/$', 'submission_billing'),
     url(r'^external_review/$', 'external_review_payment'),
+    url(r'^submissions/reset/$', 'reset_submissions'),
+    url(r'^external_review/reset/$', 'reset_external_review_payment'),
 )
-
-if settings.DEBUG:
-    # development-only views
-    urlpatterns += patterns('ecs.billing.views',
-        url(r'^submissions/reset/$', 'reset_submissions'),
-        url(r'^external_review/reset/$', 'reset_external_review_payment'),
-    )
