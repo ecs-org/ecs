dc61c6dfe5a9aa8bdffe412b3933b4587d8bbb51
Johannes Dollinger <emulbreh@googlemail.com>
2010-11-19 10:37:30 +0100

    Fixed checklist blueprint bootstrap typo

--- core/bootstrap.py
+++ core/bootstrap.py
@@ -398,7 +398,7 @@ def auth_ec_staff_users():
     staff_users = ()
     
     for blueprint in blueprints:
-        b, _ = ChecklistBlueprint.objects.get_or_create(name=blueprint['slug'])
+        b, _ = ChecklistBlueprint.objects.get_or_create(slug=blueprint['slug'])
         changed = False
         for name, value in blueprint.items():
             if getattr(b, name) != value:
