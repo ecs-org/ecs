e7bfa1d0d960f587dd2c3ea97449c93a8cb2f7fc
Martin Natano <natano@natano.net>
2011-03-08 18:09:37 +0100

    fix #1916

--- boilerplate/forms.py
+++ boilerplate/forms.py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+
 from ecs.boilerplate.models import Text
 
 class TextForm(forms.ModelForm):
+    slug = forms.CharField(max_length=Text._meta.get_field('slug').max_length, label=_('Shorthand Symbol'))
+
     class Meta:
         model = Text
         fields = ('slug', 'text')
