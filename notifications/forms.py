bcca4aaf7ddfaccefee1f25cc3bd3cdcdf8c31c7
Ludwig Hammel <ludwig.hammel@gmail.com>
2011-04-22 20:03:37 +0200

    translations for #1948, #1946

--- notifications/forms.py
+++ notifications/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 from ecs.notifications.models import NotificationAnswer
 
 
 class NotificationAnswerForm(forms.ModelForm):
+    valid = forms.BooleanField(label=_('valid'))
     class Meta:
         model = NotificationAnswer
         fields = ('valid', 'text',)
