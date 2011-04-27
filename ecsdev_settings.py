9637542211a9b17bc4cdbf40b41efae9038c90f6
Felix Erkinger <felix@erkinger.at>
2011-04-27 20:00:07 +0200

    fix for refactored ecsdev settings (msclient: same_host_on_server)

--- ecsdev_settings.py
+++ ecsdev_settings.py
@@ -47,6 +47,7 @@ if user in conf_dict.keys():
         'bucket': '/mediaserver/',
         'key_id': ms_key_id,
         'key_secret': ms_key_secret,
+        "same_host_as_server": True,
     }
     
     if not any(word in sys.argv for word in set(['test', 'runserver','runconcurrentserver', 'testmaker'])):
