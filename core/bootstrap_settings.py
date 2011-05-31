577d7bf0d5f641552530beff21f6ea3a319b47be
Martin Natano <natano@natano.net>
2011-05-31 14:23:42 +0200

    fix #2447

--- core/bootstrap_settings.py
+++ core/bootstrap_settings.py
@@ -375,7 +375,6 @@ commissions = [{
 checklist_questions = {
     u'thesis_review': [
         ('1', u'Geben Sie eine positive Empfehlung ab?', u''),  # change this number with care
-        ('2', u'42?', u''),
     ],
     u'statistic_review': [
         ('1', u'Ist das Studienziel ausreichend definiert?',u''),
