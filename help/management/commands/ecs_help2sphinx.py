93c3ca318fc1a4b080115548ccd3cb895ccece0a
Ludwig Hammel <ludwig.hammel@gmail.com>
2011-06-03 21:14:49 +0200

    #2077 wip, added autogenerated index from slug 'index', :doc: directives work now, because filenames are just slugnames

--- help/management/commands/ecs_help2sphinx.py
+++ help/management/commands/ecs_help2sphinx.py
@@ -21,8 +21,40 @@ class Command(BaseCommand):
             pass
 
         for page in Page.objects.all():
-            name = (slugify(page.slug) or ("page_%03d" % page.pk)) + '.rst'
+            #use the slug as filename, that way the :doc: directives work
+            name = page.slug + '.rst'
             with open(os.path.join(src_dir, name), 'w') as f:
-                f.write(linker.link(page.text).encode('utf-8'))
+                #write title of the page into the rst file, otherwise information is lost
+                pagetitle = u"%s%s" % (len(page.title)*"#", os.linesep)
+                pagetitle += u"%s%s" % (page.title, os.linesep)
+                pagetitle += u"%s%s%s%s" % (len(page.title)*"#", os.linesep, os.linesep, os.linesep)
+                
+                #generate toc tree directive for sphinx:
+                #FIXME some paths are hardcoded here and this is not really nicely done
+                if page.slug.lower() == 'index':
+                    doclist = []
+                    for line in page.text.splitlines():
+                        if ":doc:`" in line:
+                            doc = u"ecs-oha/_from_database/src/%s" % line[line.find("`")+1:line.find(" ",line.find("`")+1)]
+                            doclist.append(doc)
+                    fulltext = pagetitle+page.text
+                    intro=u"""##################
+Inhaltsverzeichnis
+##################
+
+.. include:: ../../../_latex_head.rst
+.. include:: ../../../_latex_tail.rst
+
+.. toctree::
+   :maxdepth: 2
+   
+   """                 
+                    indent = '%s   ' % os.linesep
+                    toctree = indent.join(doclist)
+                    fulltext = intro + toctree 
+                    f.write(linker.link(fulltext).encode('utf-8'))
+                else:
+                    fulltext = pagetitle+page.text
+                    f.write(linker.link(fulltext).encode('utf-8'))
         
 
