506901847f30443b813419f54310ba92bf839d72
Felix Erkinger <felix@erkinger.at>
2011-06-15 04:05:54 +0200

    added singleton decorator (no better idea how to implement temporarystoragevault)

--- utils/decorators.py
+++ utils/decorators.py
@@ -17,3 +17,23 @@ def developer(func):
     return _inner
 
 
+def singleton(cls):
+    """ Use a class as singleton. """
+
+    cls.__new_original__ = cls.__new__
+
+    @wraps(cls.__new__)
+    def singleton_new(cls, *args, **kw):
+        it =  cls.__dict__.get('__it__')
+        if it is not None:
+            return it
+
+        cls.__it__ = it = cls.__new_original__(cls, *args, **kw)
+        it.__init_original__(*args, **kw)
+        return it
+
+    cls.__new__ = singleton_new
+    cls.__init_original__ = cls.__init__
+    cls.__init__ = object.__init__
+
+    return cls
