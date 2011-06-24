565d13a16289b9f3385e66b01fe9a50773c84d65
Martin Natano <natano@natano.net>
2011-06-24 11:42:32 +0200

    scratchpad with random fortune is empty

--- scratchpad/models.py
+++ scratchpad/models.py
@@ -1,8 +1,22 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 
 from ecs.core.models.submissions import Submission
+
+SCRATCHPAD_FORTUNES = (
+    u'Wilkommen!',
+    u'Alea iacta est.',
+    u'Eat an apple on going to bed, and you’ll keep the doctor from earning his bread.',
+    u'Auge um Auge lässt die Welt erblinden. (Gandhi)',
+    u'Zwei Dinge sind unendlich: das All und die menschliche Dummheit. Bei letzterem bin ich mir noch nicht ganz sicher. (Einstein)',
+    u'Ave Caesar, morituri te salutant',
+    u'Zu Diensten!',
+    u'Zu Risiken und Nebenwirkungen lesen Sie die Packungsbeilage und fragen Sie Ihren Arzt oder Apotheker.',
+)
+
 
 class ScratchPad(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,3 +27,6 @@ class ScratchPad(models.Model):
 
     class Meta:
         unique_together = (('owner', 'submission'),)
+
+    def is_empty(self):
+        return not self.text or self.text in SCRATCHPAD_FORTUNES
--- scratchpad/views.py
+++ scratchpad/views.py
@@ -1,23 +1,11 @@
-# -*- coding: utf-8 -*-
 import random
 
 from django.shortcuts import get_object_or_404
 
 from ecs.utils.viewutils import render
 from ecs.scratchpad.forms import ScratchPadForm
-from ecs.scratchpad.models import ScratchPad
+from ecs.scratchpad.models import ScratchPad, SCRATCHPAD_FORTUNES
 from ecs.core.models.submissions import Submission
-
-fortunes = (
-    u'Wilkommen!',
-    u'Alea iacta est.',
-    u'Eat an apple on going to bed, and you’ll keep the doctor from earning his bread.',
-    u'Auge um Auge lässt die Welt erblinden. (Gandhi)',
-    u'Zwei Dinge sind unendlich: das All und die menschliche Dummheit. Bei letzterem bin ich mir noch nicht ganz sicher. (Einstein)',
-    u'Ave Caesar, morituri te salutant',
-    u'Zu Diensten!',
-    u'Zu Risiken und Nebenwirkungen lesen Sie die Packungsbeilage und fragen Sie Ihren Arzt oder Apotheker.',
-)
 
 def popup(request, scratchpad_pk=None):
     submission = None
@@ -29,7 +17,7 @@ def popup(request, scratchpad_pk=None):
         scratchpad = get_object_or_404(ScratchPad, owner=request.user, pk=scratchpad_pk)
         submission = scratchpad.submission
     else:
-        scratchpad, created = ScratchPad.objects.get_or_create(owner=request.user, submission=submission, defaults={'text': random.choice(fortunes)})
+        scratchpad, created = ScratchPad.objects.get_or_create(owner=request.user, submission=submission, defaults={'text': random.choice(SCRATCHPAD_FORTUNES)})
 
     form = ScratchPadForm(request.POST or None, instance=scratchpad)
     if request.method == 'POST' and form.is_valid():
