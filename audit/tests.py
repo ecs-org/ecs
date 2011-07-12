9fe552bb2b543e96e541acd0b02578b486b5af5a
Ludwig Hammel <ludwig.hammel@gmail.com>
2011-07-12 18:24:28 +0200

    audittrail tests docstrings

--- audit/tests.py
+++ audit/tests.py
@@ -10,24 +10,36 @@ from ecs.users.utils import create_user
 
 
 class BasicTests(EcsTestCase):
+    '''Tests for most basic audit functionality'''
+    
     def test_settings(self):
+        '''Tests that settings for audit trail exist'''
+        
         self.failUnless(hasattr(settings, 'ENABLE_AUDIT_TRAIL'))
         self.failUnless(hasattr(settings, 'AUDIT_TRAIL_IGNORED_MODELS'))
            
     def test_create_user(self):
+        '''Makes sure that the audit trail of the system grows when objects (e.g. Users) are created and added to the system.'''
+        
         audit_trail_entries_count = AuditTrail.objects.count()
         create_user('audit_trail_test_user@example.com')  # there are being created some objects (User,UserProfile,UserSettings)
         self.assertNotEqual(audit_trail_entries_count, AuditTrail.objects.count())
     
     def test_line_formatting(self):
+        '''FIXME check if correct: Tests the audit trail log formatting.'''
+        
         a = AuditTrail.objects.all()[0]
         a.get_log_line()
 
     def test_unicode(self):
+        '''FIXME check if correct: Tests unicode support of the audit trail'''
+        
         a = AuditTrail.objects.all()[0]
         unicode(a)
 
 class ViewTests(EcsTestCase):
+    '''Test'''
+    
     def setUp(self, *args, **kwargs):
         inspector = create_user('inspector@example.com')
         inspector.set_password('4223')
@@ -43,33 +55,45 @@ class ViewTests(EcsTestCase):
         return super(ViewTests, self).setUp(*args, **kwargs)
 
     def test_log_view_txt(self):
+        '''Tests that the audit trail is viewable via browser in format text/plain'''
+        
         self.client.login(email='inspector@example.com', password='4223')
         response = self.client.get(reverse('ecs.audit.views.log', args=('txt',)))
         self.failUnlessEqual(response.status_code, 200)
         self.failUnlessEqual(response['Content-Type'], 'text/plain')
 
     def test_log_view_html(self):
+        '''Tests that the audit trail is viewable via browser in format text/html'''
+        
         self.client.login(email='inspector@example.com', password='4223')
         response = self.client.get(reverse('ecs.audit.views.log', args=('html',)))
         self.failUnlessEqual(response.status_code, 200)
         self.failUnlessEqual(response['Content-Type'], 'text/html')
 
     def test_log_view_txt_unauthorized(self):
+        '''Makes sure that unauthorized access to the audit trail in plaintext format is blocked'''
+        
         self.client.login(email='unauthorized@example.com', password='4223')
         response = self.client.get(reverse('ecs.audit.views.log', args=('txt',)))
         self.failUnlessEqual(response.status_code, 302) # redirect to login page
 
     def test_log_view_html_unauthorized(self):
+        '''Makes sure that unauthorized access to the audit trail in html format is blocked'''
+        
         self.client.login(email='unauthorized@example.com', password='4223')
         response = self.client.get(reverse('ecs.audit.views.log', args=('html',)))
         self.failUnlessEqual(response.status_code, 302)  # redirect to login page
 
     def test_log_view_foo(self):
+        '''a 'foo' version of the audit trail must not exist.'''
+        
         self.client.login(email='inspector@example.com', password='4223')
         response = self.client.get(reverse('ecs.audit.views.log', args=('foo',)))
         self.failUnlessEqual(response.status_code, 404)  # foo is not a valid log format
 
     def test_paging(self):
+        '''Creates 100 entries in the audit trail. Test that first and last entry are reachable via audit trail paging logic.'''
+        
         for i in xrange(100): # create a lot of audit trail entries
             create_user('audittrailpagingtest{0}@example.com'.format(i))
 
