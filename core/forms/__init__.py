5c34ab91be53633323f6996b5b549161a6d1aec9
Martin Natano <natano@natano.net>
2011-07-25 20:33:50 +0200

    whoops

--- core/forms/__init__.py
+++ core/forms/__init__.py
@@ -1,5 +1,5 @@
 from ecs.core.forms.forms import (NotificationForm, CompletionReportNotificationForm, ProgressReportNotificationForm, SubmissionFormForm,
     SubmissionEditorForm, ForeignParticipatingCenterFormSet, NonTestedUsedDrugFormSet, MeasureFormSet, RoutineMeasureFormSet,
     InvestigatorFormSet, InvestigatorEmployeeFormSet, MultiNotificationForm, SubmissionImportForm,
-    SubmissionFilterForm, SubmissionWidgetFilterForm, SubmissionListFilterForm, SubmissionListFullFilterForm,
+    SubmissionFilterForm, SubmissionMinimalFilterForm, SubmissionWidgetFilterForm, SubmissionListFilterForm, SubmissionListFullFilterForm,
 )
