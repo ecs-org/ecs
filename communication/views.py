16c5428ddb18b30de478962d09ea29e819ac7651
Martin Natano <natano@natano.net>
2011-10-06 19:26:30 +0200

    fixed #2963: prefix PageNr. with "Seite" eg. "Seite 1 von 1"

--- communication/views.py
+++ communication/views.py
@@ -210,7 +210,7 @@ def list_threads(request):
         queryset=queryset_stage2.order_by('-last_message__timestamp'),
         session_prefix='messages:unified',
         user_sort='receiver__username',
-        page_size=10,
+        page_size=15,
         extra_context={'filterform': filterform},
     )
 
