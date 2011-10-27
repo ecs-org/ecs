9f61bb8b897a3c7e6c8ed1a9250ce6161e4a6618
Johannes Dollinger <emulbreh@googlemail.com>
2011-10-27 20:11:52 +0200

    Removed remaining ecs.documents.views.document_search reference

--- documents/urls.py
+++ documents/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ecs.documents.views',
     url(r'^document/(?P<document_pk>\d+)/download/$', 'download_document'),
-    url(r'^document/(?P<document_pk>\d+)/search/$', 'document_search'),
     url(r'^document/(?P<document_pk>\d+)/search/json/$', 'document_search_json'),
 )
