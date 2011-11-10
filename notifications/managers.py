addda4ebf4b5006c51f7e89b5765e4e549b94cb5
Johannes Dollinger <emulbreh@googlemail.com>
2011-11-10 19:54:14 +0100

    Fixed #3344

--- notifications/managers.py
+++ notifications/managers.py
@@ -2,16 +2,12 @@ from django.db import models
 from ecs.authorization.managers import AuthorizationManager
 
 
-def _get_answer_q():
-    return models.Q(answer__isnull=False) | models.Q(safetynotification__is_acknowledged=True)
-
-
 class NotificationQuerySet(models.query.QuerySet):
     def answered(self):
-        return self.filter(_get_answer_q())
+        return self.filter(models.Q(answer__isnull=False) | models.Q(safetynotification__is_acknowledged=True))
         
     def unanswered(self):
-        return self.exclude(_get_answer_q())
+        return self.filter(models.Q(answer__isnull=True) & (models.Q(safetynotification__isnull=True) | models.Q(safetynotification__is_acknowledged=False)))
 
 
 class NotificationManager(AuthorizationManager):
