35f61d6d3a51e62c0e43064fb397966193f3c77b
tombo <devnull@localhost>
2011-11-15 16:28:19 +0100

    #3360: email input length, little css corrections

--- static/js/ecs/utils.js
+++ static/js/ecs/utils.js
@@ -696,3 +696,27 @@ ecs.windmill_upload = function(filename) {
     evt.initEvent('UploadAssistantSetValue', true, false);
     element.dispatchEvent(evt);
 };
+
+var W3CDOM = (document.createElement && document.getElementsByTagName);
+
+function initFileUploads() {
+  if (!W3CDOM) return;
+  var fakeFileUpload = document.createElement('div');
+  fakeFileUpload.className = 'fakefile';
+  fakeFileUpload.appendChild(document.createElement('input'));
+  var image = document.createElement('img');
+  image.src='pix/button_select.gif';
+  fakeFileUpload.appendChild(image);
+  var x = document.getElementsByTagName('input');
+  for (var i=0;i<x.length;i++) {
+    if (x[i].type != 'file') continue;
+    if (x[i].parentNode.className != 'FileField') continue;
+    x[i].className = 'file hidden';
+    var clone = fakeFileUpload.cloneNode(true);
+    x[i].parentNode.appendChild(clone);
+    x[i].relatedElement = clone.getElementsByTagName('input')[0];
+    x[i].onchange = x[i].onmouseout = function () {
+      this.relatedElement.value = this.value;
+    }
+  }
+}
