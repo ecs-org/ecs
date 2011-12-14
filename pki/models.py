e91317af93f96b8701b43881f44fa9f7baaab71a
Johannes Dollinger <emulbreh@googlemail.com>
2011-12-14 18:42:05 +0100

    Increased Certificate.fingerprint field length

--- pki/models.py
+++ pki/models.py
@@ -5,6 +5,6 @@ class Certificate(models.Model):
     user = models.ForeignKey(User, related_name='certificates')
     cn = models.CharField(max_length=100)
     subject = models.TextField()
-    fingerprint = models.CharField(max_length=50)
+    fingerprint = models.CharField(max_length=60)
     is_revoked = models.BooleanField(default=False)
     
