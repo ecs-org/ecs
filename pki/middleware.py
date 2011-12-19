491d684840997a9203e78773f5db40c59c4a5307
Johannes Dollinger <emulbreh@googlemail.com>
2011-12-19 17:49:36 +0100

    Disabled client cert redirects for unittests

--- pki/middleware.py
+++ pki/middleware.py
@@ -1,10 +1,13 @@
 from urllib import urlencode
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 class ClientCertMiddleware(object):
     def process_request(self, request):
         url = reverse('ecs.pki.views.authenticate')
+        if not getattr(settings, 'ECS_REQUIRE_CLIENT_CERTS', True):
+            return
         if request.path != url and request.user.is_authenticated() and request.user.ecs_profile.is_internal and not request.session.get('ecs_pki_authenticated', False):
             return HttpResponseRedirect('%s?%s' % (url, urlencode({'next': request.get_full_path()})))
         
