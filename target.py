e63c3cb91ac0f0ec8c605d885abef19ef2e47bdc
Felix Erkinger <felix@erkinger.at>
2011-12-21 23:29:25 +0100

    typo

--- target.py
+++ target.py
@@ -207,17 +207,17 @@ class SetupTarget(SetupTargetObject):
 
     def catalina_config(self):
         write_regex_replace(
-            os.path.join(get_pythonenv(), "tomcat-6", 'conf', 'server.xml'),
+            os.path.join(get_pythonenv(), 'tomcat-6', 'conf', 'server.xml'),
             r'(^[ \t]+<!--[ \t]*$)?'+
             r'(^[ \t]+<Connector port="8009".protocol="AJP/1.3" redirectPort="8443" />[ \t]*$)'+
             r'(^[ \t]+-->[ \t]*$)?',
             r'\2', multiline=True)
         write_regex_replace(
-            os.path.join(get_pythonenv(), 'conf', 'pdf-as', 'cfg', 'config.properties'),
+            os.path.join(get_pythonenv(), 'tomcat-6', 'conf', 'pdf-as', 'cfg', 'config.properties'),
             r'(moc.sign.url=)(http[s]?://[^/]+)(/bkuonline/http-security-layer-request)',
             r'\1https://{0}\3'.format(self.config['host']))
         write_regex_replace(
-            os.path.join(get_pythonenv(), 'conf', 'pdf-as', 'cfg', 'pdf-as-web.properties'),
+            os.path.join(get_pythonenv(), 'tomcat-6', 'conf', 'pdf-as', 'cfg', 'pdf-as-web.properties'),
             r'([#]?)(retrieve_signature_data_url_override=)(http[s]?://[^/]+)(/pdf-as/RetrieveSignatureData)',
             r'\2http://{0}\4'.format(self.config['host']))
 
