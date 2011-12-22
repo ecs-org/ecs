5208e41b9d253b2972de5400a353d52ec4b53db4
Felix Erkinger <felix@erkinger.at>
2011-12-22 10:50:47 +0100

    disable user testecs overrides in ecsdev_settings (test is managed different now) added new services for testecs in update_targets

--- ecsdev_settings.py
+++ ecsdev_settings.py
@@ -28,7 +28,7 @@ if user in DBPWD_DICT:
 
 conf_dict = {
     'shredder': ('s.ecsdev.ep3.at', 8833, 'Skj45A6R2z36gVKF17i2', 'SfMS0teNT7E2yD6GVVK6JH0xwfkeykw'),
-    'testecs': ('test.ecsdev.ep3.at', 8843, 'GHz36o6OJHOm8uKmYiD1', 'dwvKMtJmRUiXeaMWGCHnEJZjD4CDEh6'),
+    #'testecs': ('test.ecsdev.ep3.at', 8843, 'GHz36o6OJHOm8uKmYiD1', 'dwvKMtJmRUiXeaMWGCHnEJZjD4CDEh6'),
     'chipper': ('doc.ecsdev.ep3.at', 8853, 'Edoij38So9js7SEiu982', 'ESDOFK934JSDFihsnu3w4SDOJFuihwi'),
 }
 
@@ -55,10 +55,6 @@ if user in conf_dict.keys():
 
     ABSOLUTE_URL_PREFIX = "http://{0}".format(domain)
 
-if user == 'testecs':
-    # FIXME: Disabled, will use mock interface default
-    # PDFAS_SERVICE = 'http://test.ecsdev.ep3.at:4780/pdf-as/'
-    pass
 
 # Mediaserver Server Access
 MS_SERVER_OVERRIDE = {
@@ -67,13 +63,9 @@ MS_SERVER_OVERRIDE = {
     'render_memcache_host': 11211,
 }
 
-if user in ['testecs', 'chipper']:
+if user in ['chipper',]:
     DEBUG = False                               # do not show django debug messages
     TEMPLATE_DEBUG = True                       # but sentry does show template errors
     CELERY_SEND_TASK_ERROR_EMAILS = True        # send errors of tasks via email to admins
 
-if user == 'testecs':
-    # fulltext search engine override (testecs uses solr instead of whoosh)
-    HAYSTACK_SEARCH_ENGINE = 'solr'
-    HAYSTACK_SOLR_URL = 'http://localhost:8099/solr'
 
