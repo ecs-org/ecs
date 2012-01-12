2382fbd587dd747e2d0000e6c15cbd3f4d9a6253
Martin Natano <natano@natano.net>
2012-01-12 17:56:28 +0100

    ClientCertMiddleware debug help

--- pki/middleware.py
+++ pki/middleware.py
@@ -2,10 +2,11 @@ from urllib import urlencode
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.conf import settings
+from ecs.pki.views import authenticate
 
 class ClientCertMiddleware(object):
     def process_request(self, request):
-        url = reverse('ecs.pki.views.authenticate')
+        url = reverse(authenticate)
         if not getattr(settings, 'ECS_REQUIRE_CLIENT_CERTS', True):
             return
         if request.path != url and request.user.is_authenticated() and request.user.ecs_profile.is_internal and not request.session.get('ecs_pki_authenticated', False):
