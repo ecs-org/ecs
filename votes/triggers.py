57f22c64febe5a24ed79c9b18cbefbf0082e0c50
Martin Natano <natano@natano.net>
2012-01-17 17:52:12 +0100

    fixed #3799, #3800, #3801

--- votes/triggers.py
+++ votes/triggers.py
@@ -10,7 +10,7 @@ def on_vote_published(sender, **kwargs):
     vote = kwargs['vote']
     if vote.submission_form and not vote.submission_form.is_categorized_multicentric_and_local:
         parties = vote.submission_form.get_presenting_parties()
-        parties.send_message(_('Publication of {vote}').format(vote=unicode(vote)), 'submissions/vote_publish.txt',
+        parties.send_message(_('Vote {ec_number}').format(ec_number=vote.get_ec_number()), 'submissions/vote_publish.txt',
             {'vote': vote}, submission=vote.submission_form.submission, cc_groups=settings.ECS_VOTE_RECEIVER_GROUPS)
 
 @connect(signals.on_vote_expiry)
