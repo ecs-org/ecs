ffda18f359642e267e0028ab40f25749d1463675
Felix Erkinger <felix@erkinger.at>
2012-01-18 13:20:10 +0100

    use_sudo in write_template

--- target.py
+++ target.py
@@ -72,10 +72,10 @@ class SetupTarget(SetupTargetObject):
             data = f.read()
         return data
     
-    def write_config_template(self, template, dst, context=None, filemode=None):
+    def write_config_template(self, template, dst, context=None, filemode=None, use_sudo=False):
         if context is None:
             context = self.config
-        write_template(os.path.join(self.dirname, 'templates', 'config', template), dst, context=context)
+        write_template(os.path.join(self.dirname, 'templates', 'config', template), dst, context=context, use_sudo=use_sudo)
         
     def help(self, *args, **kwargs):
         print('''{0} targets
