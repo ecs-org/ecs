a4ccbd18239d6d2f9e5a84223152306298148125
Felix Erkinger <felix@erkinger.at>
2012-01-23 00:54:13 +0100

    cleanup urls comments

--- urls.py
+++ urls.py
@@ -83,9 +83,6 @@ if 'ecs.userswitcher' in settings.INSTALLED_APPS:
         
 if 'sentry' in settings.INSTALLED_APPS:
     urlpatterns += patterns('',
-        # redirect sentry login/logout pages to standard pages because we dont want sentry handle login
-        #url(r'^sentry/login$', 'django.views.generic.simple.redirect_to', {'url': reverse("ecs.users.views.login")}),
-        #url(r'^sentry/logout$', 'django.views.generic.simple.redirect_to', {'url': reverse("ecs.users.views.logout")}),
         url(r'^sentry/', include('sentry.web.urls')),
     )
     
