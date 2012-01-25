d68d751a7774d2192f78df4a98e5681c494aca56
Felix Erkinger <felix@erkinger.at>
2012-01-25 04:09:25 +0100

    idempotent mailqueue creation for undeliverable dir

--- ecsmail/bootstrap.py
+++ ecsmail/bootstrap.py
@@ -1,12 +1,16 @@
 import os
-
+   
 from django.conf import settings
-
 from ecs import bootstrap
 
 @bootstrap.register()
 def create_undeliverable_queue_dir():
-    workdir = settings.ECSMAIL ['undeliverable_queue_dir']
-    if not os.path.isdir(workdir):
-        os.makedirs(workdir, mode=0722)
+    basedir = settings.ECSMAIL ['undeliverable_queue_dir']
+
+    if not os.path.isdir(basedir):
+        os.makedirs(basedir, mode=0722)
     
+    for node in ("tmp", "new", "cur"):
+        nodedir = os.path.join(basedir, node)
+        if not os.path.isdir(nodedir):
+            os.mkdir(nodedir, 0700)
