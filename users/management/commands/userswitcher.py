fb1babad2dd688ef182084cd81da9dd7abd098dc
Martin Natano <natano@natano.net>
2012-02-10 17:23:13 +0100

    fixed #3942: create management command to add all is_internal users to the userswitcher group

---
+++ users/management/commands/userswitcher.py
@@ -0,0 +1,50 @@
+import sys
+from optparse import make_option
+from django.core.management.base import BaseCommand, CommandError
+
+from django.contrib.auth.models import User, Group
+from ecs.users.utils import get_user
+
+class Command(BaseCommand):
+    help = "add or remove users from the userswitcher"
+    option_list = BaseCommand.option_list + (
+        make_option('-i', action='store_true', dest='activate_internal', help='add is_internal users to the userswitcher'),
+        make_option('-I', action='store_false', dest='activate_internal', help='remove is_internal users from the userswitcher'),
+        make_option('-t', action='store_true', dest='activate_testusers', help='add testusers to the userswitcher'),
+        make_option('-T', action='store_false', dest='activate_testusers', help='remove testusers from the userswitcher'),
+        make_option('-r', action='store_true', dest='remove_user', help='remove users from the userswitcher'),
+    )
+    def handle(self, *emails, **options):
+        verbosity = int(options['verbosity'])
+        activate_internal = options['activate_internal']
+        activate_testusers = options['activate_testusers']
+        remove_user = options['remove_user']
+
+        if activate_internal is None and activate_testusers is None and not emails:
+            raise CommandError("nothing to do")
+
+        if remove_user and not emails:
+            raise CommandError("nobody to remove")
+
+        group = Group.objects.get(name='userswitcher_target')
+        internal_users = User.objects.filter(ecs_profile__is_internal=True).exclude(ecs_profile__is_testuser=True)
+        testusers = User.objects.filter(ecs_profile__is_testuser=True)
+
+        def _toggle(user, activate):
+            if verbosity >= 2:
+                print u'  {0}{1}'.format(u'+' if activate else u'-', user.email)
+            (group.user_set.add if activate else group.user_set.remove)(user)
+
+        def _toggle_set(name, users, activate):
+            if not activate is None:
+                if verbosity >= 1:
+                    print (u'+' if activate else u'-') + name
+                for user in users:
+                    _toggle(user, activate)
+
+        _toggle_set('internal_users', internal_users, activate_internal)
+        _toggle_set('testusers', testusers, activate_testusers)
+
+        for email in emails:
+            user = get_user(email)
+            _toggle(user, not remove_user)
