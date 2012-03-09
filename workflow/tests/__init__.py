dcfd100ef42b720fca7ecd41aa997ae7f45a6324
Martin Natano <natano@natano.net>
2012-03-09 19:22:58 +0100

    small fix

--- workflow/tests/__init__.py
+++ workflow/tests/__init__.py
@@ -1,7 +1,4 @@
 from ecs.workflow.tests.base import WorkflowTestCase
 
 from ecs.workflow.tests.flow import FlowTest
-from ecs.workflow.tests.deadlines import DeadlineTest
 from ecs.workflow.tests.repeatability import RepeatabilityTest
-
-
