763e8bdbbbf4aca5ea92ec541a818c40e94e4b54
Martin Natano <natano@natano.net>
2012-03-13 10:39:10 +0100

    fixed #4188: for resident board member the staus tab does not show all submissions

--- meetings/views.py
+++ meetings/views.py
@@ -764,7 +764,8 @@ def meeting_details(request, meeting_pk=None, active=None):
             vote = votes[0]
         votes_list.append({'top_index': top.index, 'top': str(top), 'vote': vote})
     
-    submissions = meeting.submissions.order_by('ec_number')
+    with sudo():
+        submissions = meeting.submissions.order_by('ec_number')
 
     return render(request, 'meetings/details.html', {
         'cumulative_count': submissions.distinct().count(),
