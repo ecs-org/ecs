846ca144fa06177d9adf3dec8a890b71623a4d8e
Martin Natano <natano@natano.net>
2012-03-15 16:29:09 +0100

    fixed #4202: sort userswitcher by last_name (display lastname, firstname if existent else email)

--- userswitcher/forms.py
+++ userswitcher/forms.py
@@ -4,13 +4,22 @@ from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+class _UserSwitcherChoiceField(forms.ModelChoiceField):
+    def __init__(self, *args, **kwargs):
+        queryset = User.objects.filter(groups__name='userswitcher_target', is_active=True).select_related('ecs_profile').order_by('last_name', 'first_name', 'email')
+        defaults = {
+            'required': False,
+            'widget': forms.Select(attrs={'id': 'userswitcher_input'}),
+        }
+        for k, v in defaults.iteritems():
+            kwargs.setdefault(k, v)
+        super(_UserSwitcherChoiceField, self).__init__(queryset, *args, **kwargs)
+
+    def label_from_instance(self, user):
+        label = unicode(user.email)
+        if user.first_name and user.last_name:
+            label = u'{0}, {1}'.format(user.last_name, user.first_name)
+        return label
 
 class UserSwitcherForm(forms.Form):
-    user = forms.ModelChoiceField(
-        User.objects.filter(groups__name='userswitcher_target').select_related('ecs_profile').order_by('email'),
-        required=False, 
-        label=_(u'user'), 
-        widget=forms.Select(attrs={'id': 'userswitcher_input'})
-    )
-
-
+    user = _UserSwitcherChoiceField()
