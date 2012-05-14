6345567952ca5cdd7f4581e6485ae1e063b4a6d5
Martin Natano <natano@natano.net>
2012-05-14 15:59:20 +0200

    fixed #4364: find and resolve performance issues related to audit trail

--- audit/models.py
+++ audit/models.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
-import hmac
-import hashlib
 
 from django.db import models
 from django.contrib.contenttypes import generic
@@ -32,11 +29,10 @@ class AuditTrail(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     instance = generic.GenericForeignKey()
-    created_at = models.DateTimeField(db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     description = models.CharField(max_length=100, null=False, blank=False)
     user = models.ForeignKey('auth.User', null=False, blank=False)
     data = models.TextField(null=False, blank=False)
-    hash = models.CharField(max_length=64, null=False, blank=False)
     is_instance_created = models.BooleanField(default=False)
 
     objects = AuditTrailManager()
@@ -49,29 +45,11 @@ class AuditTrail(models.Model):
         else:
             return ''
 
-    def _get_log_line(self):
+    def get_log_line(self):
         line = u'[%s] %s\n' % (self.created_at.strftime('%b %d %Y %H:%M:%S'), self.description)
         line += u'    User: %s %s (%s) <%s>\n' % (self.user.first_name, self.user.last_name, self.user.username, self.user.email)
         line += u'>>>> old data\n%s\n====\n%s\n<<<< new data\n' % (self.old_data, self.data)
         return line
-    
-    def get_log_line(self):
-        line = self._get_log_line()
-        line += u'    Hash: %s\n' % self.hash
-        return line
-    
+
     def __unicode__(self):
         return self.description
-
-    def save(self, *args, **kwargs):
-        previous_entry = AuditTrail.objects.get_last_change()
-        if previous_entry:
-            last_hash = previous_entry.hash
-        else:
-            last_hash = ''
-        
-        self.created_at = datetime.now()
-        self.hash = hmac.new(str(last_hash), self._get_log_line().encode('utf8'), hashlib.sha256).hexdigest()
-        rval = super(AuditTrail, self).save(*args, **kwargs)
-        # TODO: log with rsyslogd
-        return rval
