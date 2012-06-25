463bd64d60a78cf96b547af1c4836c3370a1113d
Martin Natano <natano@natano.net>
2012-06-25 11:10:28 +0200

    fixed #4548: sent vote expiration message to presenting parties not only to submitter

--- votes/tasks.py
+++ votes/tasks.py
@@ -18,36 +18,35 @@ from ecs.votes.signals import on_vote_expiry
 
 
 def send_vote_expired(vote):
-    recipients_q = Q(email=settings.ECSMAIL['postmaster'])
-    if vote.submission_form.submitter:
-        recipients_q |= Q(pk=vote.submission_form.submitter.pk)
-    if vote.submission_form.submitter_email:
-        recipients_q |= Q(email=vote.submission_form.submitter_email)
-
-    recipients = User.objects.filter(recipients_q)
+    recipients = vote.submission_form.get_presenting_parties().get_users()
+    recipients.add(User.objects.get(email=settings.ECSMAIL['postmaster']))
 
     url = reverse('readonly_submission_form', kwargs={ 'submission_form_pk': vote.submission_form.pk })
-    text = _(u'Das Votum für die Studie <a href="#" onclick="window.parent.location.href=\'%(url)s\';" >EK-Nr. %(ec_number)s</a> vom %(meeting_date)s ist abgelaufen.\n') % {
+    if vote.top:
+        vote_date = vote.top.meeting.start
+    else:
+        vote_date = vote.published_at
+    text = _(u'Das Votum für die Studie <a href="#" onclick="window.parent.location.href=\'%(url)s\';" >EK-Nr. %(ec_number)s</a> vom %(vote_date)s ist abgelaufen.\n') % {
         'url': url,
         'ec_number': vote.submission_form.submission.get_ec_number_display(),
-        'meeting_date': vote.top.meeting.start.strftime('%d.%m.%Y'),
+        'vote_date': vote_date.strftime('%d.%m.%Y'),
     }
 
     subject = _(u'Ablauf des Votums für die Studie EK-Nr. %s') % vote.submission_form.submission.get_ec_number_display()
     send_submission_message(vote.submission_form.submission, subject, text, recipients)
 
 def send_vote_reminder_submitter(vote):
-    recipients = User.objects.none()
-    if vote.submission_form.submitter:
-        recipients |= User.objects.filter(pk=vote.submission_form.submitter.pk)
-    if vote.submission_form.submitter_email:
-        recipients |= User.objects.filter(email=vote.submission_form.submitter_email)
+    recipients = vote.submission_form.get_presenting_parties().get_users()
 
     url = reverse('readonly_submission_form', kwargs={ 'submission_form_pk': vote.submission_form.pk })
-    text = _(u'Das Votum für die Studie <a href="#" onclick="window.parent.location.href=\'%(url)s\';" >EK-Nr. %(ec_number)s</a> vom %(meeting_date)s läuft in drei Wochen ab.\n') % {
+    if vote.top:
+        vote_date = vote.top.meeting.start
+    else:
+        vote_date = vote.published_at
+    text = _(u'Das Votum für die Studie <a href="#" onclick="window.parent.location.href=\'%(url)s\';" >EK-Nr. %(ec_number)s</a> vom %(vote_date)s läuft in drei Wochen ab.\n') % {
         'url': url,
         'ec_number': vote.submission_form.submission.get_ec_number_display(),
-        'meeting_date': vote.top.meeting.start.strftime('%d.%m.%Y'),
+        'vote_date': vote_date.strftime('%d.%m.%Y'),
     }
 
     subject = _(u'Ablauf des Votums für die Studie EK-Nr. %s') % vote.submission_form.submission.get_ec_number_display()
@@ -57,10 +56,14 @@ def send_vote_reminder_office(vote):
     recipients = User.objects.filter(email=settings.ECSMAIL['postmaster'])
 
     url = reverse('readonly_submission_form', kwargs={ 'submission_form_pk': vote.submission_form.pk })
-    text = _(u'Das Votum für die Studie <a href="#" onclick="window.parent.location.href=\'%(url)s\';" >EK-Nr. %(ec_number)s</a> vom %(meeting_date)s läuft in einer Woche ab.\n') % {
+    if vote.top:
+        vote_date = vote.top.meeting.start
+    else:
+        vote_date = vote.published_at
+    text = _(u'Das Votum für die Studie <a href="#" onclick="window.parent.location.href=\'%(url)s\';" >EK-Nr. %(ec_number)s</a> vom %(vote_date)s läuft in einer Woche ab.\n') % {
         'url': url,
         'ec_number': vote.submission_form.submission.get_ec_number_display(),
-        'meeting_date': vote.top.meeting.start.strftime('%d.%m.%Y'),
+        'vote_date': vote_date.strftime('%d.%m.%Y'),
     }
 
     subject = _(u'Ablauf des Votums für die Studie EK-Nr. %s') % vote.submission_form.submission.get_ec_number_display()
