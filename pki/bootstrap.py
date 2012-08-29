e3b49e4021b8929eab455f6a901016522beee0be
Felix Erkinger <felix@erkinger.at>
2012-08-29 15:18:02 +0200

    creation of ecs-ca/openssl-ca.cnf on dummy-ca creation, contributes to #4622

--- pki/bootstrap.py
+++ pki/bootstrap.py
@@ -36,5 +36,13 @@ def setup_ca():
 
         subject = ''.join('/%s=%s' % bit for bit in subject_bits)
         ca = get_ca()
+        os.makedirs(settings.ECS_CA_ROOT)
+        
+        with open(settings.ECS_CA_CONFIG, "w+b") as outputfile:
+            with open(os.path.join(settings.PROJECT_DIR, 'templates', 'config', 'openssl-ca.cnf')) as inputfile:
+                text = inputfile.read()
+            text = text % ca.__dict__
+            outputfile.write(text)
+        
         ca.setup(subject)
         print ca.ca_fingerprint
