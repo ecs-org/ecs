fde400931c3f01c4d57b0f49907e251618b681af
Felix Erkinger <felix@erkinger.at>
2013-09-01 19:53:28 +0200

    added -M -m to userswitcher (activate/deactivate boardmember to user_switcher), added meeting-hammer.py (hammer meeting urls)

--- users/management/commands/userswitcher.py
+++ users/management/commands/userswitcher.py
@@ -12,23 +12,28 @@ class Command(BaseCommand):
         make_option('-I', action='store_false', dest='activate_internal', help='remove is_internal users from the userswitcher'),
         make_option('-t', action='store_true', dest='activate_testusers', help='add testusers to the userswitcher'),
         make_option('-T', action='store_false', dest='activate_testusers', help='remove testusers from the userswitcher'),
+        make_option('-m', action='store_true', dest='activate_board', help='add board member users to the userswitcher'),
+        make_option('-M', action='store_false', dest='activate_board', help='remove board member users from the userswitcher'),
         make_option('-r', action='store_true', dest='remove_user', help='remove users from the userswitcher'),
     )
     def handle(self, *emails, **options):
         verbosity = int(options['verbosity'])
         activate_internal = options['activate_internal']
         activate_testusers = options['activate_testusers']
+        activate_boardmember = options['activate_board']
         remove_user = options['remove_user']
 
-        if activate_internal is None and activate_testusers is None and not emails:
+        if activate_internal is None and activate_testusers is None and activate_boardmember is None and not emails:
             raise CommandError("nothing to do")
 
         if remove_user and not emails:
             raise CommandError("nobody to remove")
 
         group = Group.objects.get(name='userswitcher_target')
+        boardmember_group = Group.objects.get(name="EC-Board Member")
         internal_users = User.objects.filter(ecs_profile__is_internal=True).exclude(ecs_profile__is_testuser=True)
         testusers = User.objects.filter(ecs_profile__is_testuser=True)
+        boardmembers = User.objects.filter(groups=boardmember_group.id)
 
         def _toggle(user, activate):
             if verbosity >= 2:
@@ -44,6 +49,7 @@ class Command(BaseCommand):
 
         _toggle_set('internal_users', internal_users, activate_internal)
         _toggle_set('testusers', testusers, activate_testusers)
+        _toggle_set('boardmembers', boardmembers, activate_boardmember)
 
         for email in emails:
             user = get_user(email)
