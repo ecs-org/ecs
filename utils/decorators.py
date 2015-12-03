b76ad16f8af96a98663ec2d71bb29968b3ee1778
Martin Natano <natano@natano.net>
2015-12-03 17:44:44 +0100

    Remove utils.decorators.print_duration.
    If you need time measuring functionality, use python's builtin timeit
    module instead.

--- utils/decorators.py
+++ utils/decorators.py
@@ -38,37 +38,3 @@ def singleton(cls):
     cls.__init__ = object.__init__
 
     return cls
-
-
-class print_duration(object):
-    def __init__(self, name=None):
-        self.name = name
-        self.args = ()
-        self.kwargs = {}
-
-    def __enter__(self):
-        self.t1 = datetime.now()
-
-    def __exit__(self, *exc):
-        self.t2 = datetime.now()
-        d = self.t2 - self.t1
-        s = '{0:.2f}s {1}'.format(d.total_seconds(), self.name or '<anonymous>')
-        if self.args or self.kwargs:
-            bits = []
-            for arg in self.args:
-                bits += [repr(arg)]
-            for k, v in self.kwargs.iteritems():
-                bits += ['{0}={1}'.format(k, repr(v))]
-            s += '(' + ', '.join(bits) + ') '
-        print s
-
-    def __call__(self, func):
-        if self.name is None:
-            self.name = '{0}.{1}'.format(func.__module__, func.__name__)
-        @wraps(func)
-        def _inner(*args, **kwargs):
-            self.args = args
-            self.kwargs = kwargs
-            with self:
-                return func(*args, **kwargs)
-        return _inner
