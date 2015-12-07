307c77f22f3d55959668a2a1b04465c86aac3f79
Martin Natano <natano@natano.net>
2015-12-07 14:54:12 +0100

    Add scripts to mirror python packages & install from mirror.

---
+++ install-dep.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ -z "$PACKAGES_DIR" ]; then
+	PACKAGES_DIR="$(cd ~/ecs-external/packages/ && pwd)"	# absolute path
+fi
+
+pip install --no-index --find-links "$PACKAGES_DIR" "$@"
---
+++ mirror-dep.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ -z "$PACKAGES_DIR" ]; then
+	PACKAGES_DIR="$(cd ~/ecs-external/packages/ && pwd)"	# absolute path
+fi
+
+pip install -d "$PACKAGES_DIR" --no-use-wheel "$@"
