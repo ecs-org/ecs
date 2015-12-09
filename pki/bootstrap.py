6d4caf7546c1b16200e8350b16b73cc7ef02dc55
Martin Natano <natano@natano.net>
2015-12-09 12:19:13 +0100

    Remove upgrade hack for old installations.

--- pki/bootstrap.py
+++ pki/bootstrap.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 from django.conf import settings
 
 from ecs import bootstrap
@@ -9,40 +8,28 @@ from ecs.pki.utils import get_ca
 @bootstrap.register()
 def setup_ca():
     if os.path.exists(settings.ECS_CA_ROOT):
-        oldcnf = os.path.join(settings.ECS_CONFIG_DIR, 'openssl-ca.cnf')
-        newcnf = settings.ECS_CA_CONFIG
+        return
 
-        if not os.path.exists(newcnf):
-            if os.path.exists(oldcnf):
-                print("Warning: Upgrading: Moving openssl-ca.cnf from old to new location")
-                shutil.copy(oldcnf, newcnf)
-                if os.path.exists(oldcnf+ '.old'):
-                    os.remove(oldcnf+ '.old')
-                os.rename(oldcnf, oldcnf+".old")
-            else:
-                print("Error: ECS_CA_ROOT exists, but ECS_CA_CONFIG not found")
+    print('creating dummy CA')
+    subject_bits = (
+        ('CN', 'testecs.local'),
+        ('C', 'AT'),
+        ('ST', 'Vienna'),
+        ('localityName', 'Vienna'),
+        ('O', 'ep3 Software & System House'),
+        ('OU', 'Security'),
+        ('emailAddress', 'admin@testecs.local'),
+    )
 
-    else:
-        print('creating dummy CA')
-        subject_bits = (
-            ('CN', 'testecs.local'),
-            ('C', 'AT'),
-            ('ST', 'Vienna'),
-            ('localityName', 'Vienna'),
-            ('O', 'ep3 Software & System House'),
-            ('OU', 'Security'),
-            ('emailAddress', 'admin@testecs.local'),
-        )
+    subject = ''.join('/%s=%s' % bit for bit in subject_bits)
+    ca = get_ca()
+    os.makedirs(settings.ECS_CA_ROOT)
 
-        subject = ''.join('/%s=%s' % bit for bit in subject_bits)
-        ca = get_ca()
-        os.makedirs(settings.ECS_CA_ROOT)
-        
-        with open(settings.ECS_CA_CONFIG, "w+b") as outputfile:
-            with open(os.path.join(settings.PROJECT_DIR, 'templates', 'config', 'openssl-ca.cnf')) as inputfile:
-                text = inputfile.read()
-            text = text % ca.__dict__
-            outputfile.write(text)
-        
-        ca.setup(subject)
-        print ca.ca_fingerprint
+    with open(settings.ECS_CA_CONFIG, "w+b") as outputfile:
+        with open(os.path.join(settings.PROJECT_DIR, 'templates', 'config', 'openssl-ca.cnf')) as inputfile:
+            text = inputfile.read()
+        text = text % ca.__dict__
+        outputfile.write(text)
+
+    ca.setup(subject)
+    print ca.ca_fingerprint
