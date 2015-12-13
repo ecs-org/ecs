cc21aec1df1092495c82a3054ae189f06d06ffdc
Martin Natano <natano@natano.net>
2015-12-13 13:40:28 +0100

    Remove the unused EMLEmailBackend.

--- utils/emailbackends.py
+++ utils/emailbackends.py
@@ -1,23 +1,8 @@
-import tempfile
-import subprocess
 import logging
 
 from django.core.mail.backends.base import BaseEmailBackend
 from django.conf import settings
 from sentry.client.handlers import SentryHandler
-
-class EMLEmailBackend(BaseEmailBackend):
-    def send_messages(self, email_messages):
-        try:
-            for message in email_messages:
-                with tempfile.NamedTemporaryFile(suffix='.eml') as tmp:
-                    tmp.write(message.message().as_string())
-                    tmp.flush()
-                    subprocess.call(['open', tmp.name])
-        except:
-            if not self.fail_silently:
-                raise
-        return len(email_messages)
 
 
 logger = logging.getLogger(__name__)
