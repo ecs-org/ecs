934b15c206ea31ced64b64f4dbc65aad2d72083c
Martin Natano <natano@natano.net>
2015-12-20 07:58:35 +0100

    Remove old compat redirect that points from /core/ to /dashboard/.

--- ecs/core/urls/__init__.py
+++ ecs/core/urls/__init__.py
@@ -1,8 +1,6 @@
 from django.conf.urls import *
-from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url='/dashboard/', permanent=True)),
     url(r'^fieldhistory/(?P<model_name>[^/]+)/(?P<pk>\d+)/$', 'ecs.core.views.field_history'),
     url(r'^advanced_settings/$', 'ecs.core.views.advanced_settings'),
 
