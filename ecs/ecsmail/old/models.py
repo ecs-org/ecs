e6241163561545e00167360197b6f2c6a6d15c20
Martin Natano <natano@natano.net>
2016-01-05 12:07:33 +0100

    Remove ecsmail/old/.

--- ecs/ecsmail/old/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class RawMail(models.Model):
-    message_digest_hex = models.CharField(max_length=32) 
-    ecshash = models.CharField(max_length=32, db_index=True)
-    data = models.TextField()
