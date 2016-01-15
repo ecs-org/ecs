fa7ec53014aad601a5c11d4a09841bbc6d3f005e
Martin Natano <natano@natano.net>
2016-01-15 13:23:28 +0100

    Convert the dashboard to jquery.

--- ecs/dashboard/views.py
+++ ecs/dashboard/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render
-from django.core.urlresolvers import reverse
 
 from ecs.utils.security import readonly
 
 
 @readonly()
 def view_dashboard(request):
-    widgets = [
-        reverse('ecs.communication.views.outgoing_message_widget'),
-        reverse('ecs.communication.views.incoming_message_widget'),
-        reverse('ecs.core.views.submissions.submission_widget'),
-    ]
-
-    if request.user.profile.has_explicit_workflow():
-        widgets.append(reverse('ecs.tasks.views.my_tasks'))
-
-    return render(request, 'dashboard/dashboard.html', {
-        'widgets': widgets,
-    })
-
+    return render(request, 'dashboard/dashboard.html')
