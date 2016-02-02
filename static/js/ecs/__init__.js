b7704e8bdb7247ced8718dead0dc479457a7d5ac
Martin Natano <natano@natano.net>
2016-02-02 20:31:12 +0100

    Remove the now unused Roar library.

--- static/js/ecs/__init__.js
+++ static/js/ecs/__init__.js
@@ -1,9 +1,5 @@
-if(!window.console){
+if (!window.console) {
     window.console = {log: function() {}};
 }
 
-var ecs = window.ecs = {
-    messages: new Roar({
-        duration: 10000
-    })
-};
+window.ecs = {};
