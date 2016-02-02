f74ec2439be801cd37228e91fe5ba74931c3fe8d
Martin Natano <natano@natano.net>
2016-02-02 18:31:38 +0100

    Remove new message/task notifications.

--- ecs/communication/templatetags/communication.py
+++ ecs/communication/templatetags/communication.py
@@ -1,38 +1,7 @@
-from django.template import Library, Node, TemplateSyntaxError
-from django.core.cache import cache
-from django.utils import timezone
+from django.template import Library
 
-from ecs.communication.models import Message
 
 register = Library()
-
-
-class FetchMessagesNode(Node):
-    def __init__(self, varname):
-        super(FetchMessagesNode, self).__init__()
-        self.varname = varname
-
-    def render(self, context):
-        user = context['request'].user
-        cache_key = "ecs.communication:last_pull:%s" % user.pk
-        last_pull = cache.get(cache_key)
-
-        messages = Message.objects.filter(receiver=user, unread=True)
-        if last_pull:
-            messages = messages.filter(timestamp__gt=last_pull)
-
-        cache.set(cache_key, timezone.now(), 7*24*3600)
-        context[self.varname] = messages
-        return ''
-
-@register.tag
-def fetch_messages(parser, token):
-    try:
-        name, as_, varname = token.split_contents()
-    except ValueError:
-        raise TemplateSyntaxError('{% fetch_messages as VAR %} expected')
-    return FetchMessagesNode(varname)
-
 
 
 @register.filter
@@ -60,5 +29,3 @@ def preview(message, chars):
         return text[:chars-3] + '...'
     else:
         return text
-
-
