1b107be961357755ec71bc60890e3872e9311a19
Martin Natano <natano@natano.net>
2016-02-02 18:10:37 +0100

    Convert task form to jQuery.

--- static/js/ecs/tasks.js
+++ static/js/ecs/tasks.js
@@ -1,38 +1,22 @@
-ecs.widgets.TaskWidget = new Class({
-    Implements: Events,
-    initialize: function(el, options){
-        options = options || {};
-        this.element = $(el);
-        this.element.addClass('ecs-Widget');
-        this.element.store('ecs.widgets.TaskWidget', this);
-        this.dataForm = $$('form.bound_to_task')[0];
-        this.setup();
-    },
-    setup: function(){
-        var self = this;
-        this.element.scrollTo(0, 0);
+ecs.init_task_form = function() {
+    var headerworkflow = jQuery('#headerworkflow');
+    var form = headerworkflow.find('form');
+    var data_form = jQuery('form.bound_to_task');
 
-        var form = this.element.getElement('form');
-        if (self.dataForm) {
-            form.getElement('input[name=task_management-save]').show();
-        }
-        var submit = function(){
-            if (self.dataForm) {
-                var input = form.getElement('input[name=task_management-post_data]');
-                input.setAttribute('value', self.dataForm.toQueryString());
-            }
-            return true;
-        };
-        form.addEvent('submit', submit);
-        // NOTE: we have to monkeypatch submit(), because the js method does not fire an onsubmit event.
-        form.submit = submit;
-
-        self.updateContentHeight();
-    },
-    updateContentHeight: function(){
-        $('content').setStyle('top', $('header').getHeight() + 'px');
-    },
-    dispose: function(){
-        this.element.eliminate('ecs.widgets.TaskWidget');
+    if (data_form.length) {
+        form.find('input[name=task_management-save]').show();
+        form.submit(function(ev) {
+            var input = form.find('input[name=task_management-post_data]');
+            input.val(data_form.serialize());
+        });
     }
-});
+
+    var toggle_headerworkflow = jQuery('#toggle_headerworkflow');
+    toggle_headerworkflow.click(function(ev) {
+        ev.preventDefault();
+        var visible = headerworkflow.is(':visible');
+        headerworkflow.toggle();
+        toggle_headerworkflow.toggleClass('collapsed', !visible);
+        toggle_headerworkflow.html(visible ? '&gt;' : '&lt;');
+    });
+};
