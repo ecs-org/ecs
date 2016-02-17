5af7f306418226c872215dc3bc2f029ee055dda2
Martin Natano <natano@natano.net>
2016-02-17 11:55:17 +0100

    Redirect exception in celery to sentry.

--- ecs/celery.py
+++ ecs/celery.py
@@ -4,5 +4,12 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecs.settings')
 from django.conf import settings
 
+if hasattr(settings, 'RAVEN_CONFIG'):
+    import raven
+    from raven.contrib.celery import register_signal, register_logger_signal
+    client = raven.Client(**settings.RAVEN_CONFIG)
+    register_logger_signal(client)
+    register_signal(client)
+
 app = Celery('ecs.celery')
 app.config_from_object('django.conf:settings')
