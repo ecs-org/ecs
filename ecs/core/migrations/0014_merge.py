e37cd1789e678a33300e5b1c6273c122db8e410c
Martin Natano <natano@natano.net>
2016-03-21 21:53:33 +0100

    Merge migrations (branch conflict).

---
+++ ecs/core/migrations/0014_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0011_merge'),
+        ('core', '0013_remove_expedited_review_group'),
+    ]
+
+    operations = [
+    ]
