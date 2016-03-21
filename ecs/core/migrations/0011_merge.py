f0406caf2f839890ebc8b9ab41bfe4cbe71d53fe
Martin Natano <natano@natano.net>
2016-03-21 21:40:25 +0100

    Merge migrations (branch conflict).

---
+++ ecs/core/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0010_auto_20160314_1127'),
+        ('core', '0010_remove_submissionform_sponsor_agrees_to_publishing'),
+    ]
+
+    operations = [
+    ]
