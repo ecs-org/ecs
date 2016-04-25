e7b2425f61080f5892f9a0de27ea2fb21282c97d
Martin Natano <natano@natano.net>
2016-04-25 13:27:19 +0200

    Unbreak docstash unit tests by using the new decorator api.

--- ecs/docstash/tests/urls.py
+++ ecs/docstash/tests/urls.py
@@ -2,13 +2,14 @@ import json
 
 from django.conf.urls import url
 from django.http import HttpResponse
-from ecs.docstash.decorators import with_docstash_transaction
+from ecs.docstash.decorators import with_docstash
 
 
-@with_docstash_transaction
+@with_docstash()
 def simple_post_view(request):
     if request.method == 'POST':
-        request.docstash.value = request.POST
+        request.docstash.value = request.POST.dict()
+        request.docstash.save()
     return HttpResponse(json.dumps(request.docstash.value), content_type='text/json')
 
 urlpatterns = (
