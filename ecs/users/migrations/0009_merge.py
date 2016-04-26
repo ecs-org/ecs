398f842f5d031b149cb2bff1df85dca27abd0b3d
Martin Natano <natano@natano.net>
2016-04-26 17:21:18 +0200

    Merge migrations.

---
+++ ecs/users/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0008_fix_useradministration_filter'),
+        ('users', '0008_remove_userprofile_is_thesis_reviewer'),
+    ]
+
+    operations = [
+    ]
