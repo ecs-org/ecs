e91032d5bd1567ef5e741339bdf50de36794df34
Martin Natano <natano@natano.net>
2016-04-26 17:58:00 +0200

    Merge migrations.

---
+++ ecs/users/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0012_remove_amendment_receiver_group'),
+        ('users', '0009_merge'),
+    ]
+
+    operations = [
+    ]
