68cf4c4f6f2fab28954aea64d59e6bec7075d69d
Felix Erkinger <felix@erkinger.at>
2016-05-03 20:07:47 +0200

    rsync externals is deprecated, removing traces of it

--- scripts/install-py-dep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -z "$PACKAGES_DIR" ]; then
-	PACKAGES_DIR="$(cd ~/ecs-external/packages/ && pwd)"	# absolute path
-fi
-
-pip install --no-index --find-links "$PACKAGES_DIR" "$@"
--- scripts/mirror-py-dep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -z "$PACKAGES_DIR" ]; then
-	PACKAGES_DIR="$(cd ~/ecs-external/packages/ && pwd)"	# absolute path
-fi
-
-pip install -d "$PACKAGES_DIR" --no-use-wheel "$@"
