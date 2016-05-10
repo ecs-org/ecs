4c4e4951c4f299782d1cad3feda397392ca4392d
Martin Natano <natano@natano.net>
2016-05-10 19:48:10 +0200

    Use apps.get_model() to import the specified model.
    Note: the app name must be specified at the command line, i.e. like so:
    
    	$ ./manage.py ecs_workflow_dot core.Submission

--- ecs/integration/management/commands/ecs_workflow_dot.py
+++ ecs/integration/management/commands/ecs_workflow_dot.py
@@ -1,22 +1,15 @@
-from optparse import make_option
 from django.core.management.base import BaseCommand
+from django.apps import apps
 
 from ecs.workflow.models import Graph
 from ecs.workflow.utils import make_dot
-from ecs.core.models import Submission
-from ecs.votes.models import Vote
-from ecs.meetings.models import Meeting
-from ecs.notifications.models import Notification
-from ecs.users.models import UserProfile
+
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option('--model', dest='model', action='store', default=False, 
-            help="target model, can be one of Submission,Vote,Meeting,Notification,UserProfile"),
-    )
+    def add_arguments(self, parser):
+        parser.add_argument('model', help='The model name in app.Model syntax')
 
-    def handle(self, model=None, **options):
-        model = {'Submission': Submission, 'Vote': Vote, 'Meeting': Meeting, 
-            'Notification': Notification, 'UserProfile': UserProfile}.get(model)
+    def handle(self, **options):
+        model = apps.get_model(options['model'])
         g = Graph.objects.get(model=model, auto_start=True)
         print(make_dot(g))
