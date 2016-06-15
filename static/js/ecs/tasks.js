bca048df5d3f2847c55ffa051397e629c4a5b4b0
Martin Natano <natano@natano.net>
2016-06-15 18:21:51 +0200

    Use FA icons for the task management form.

--- static/js/ecs/tasks.js
+++ static/js/ecs/tasks.js
@@ -4,7 +4,7 @@ ecs.init_task_form = function() {
     var data_form = $('form.bound_to_task');
 
     if (data_form.length) {
-        form.find('input[name=task_management-save]').show();
+        form.find('input[name=task_management-save]').removeAttr('hidden');
         form.submit(function(ev) {
             var input = form.find('input[name=task_management-post_data]');
             input.val(data_form.serialize());
