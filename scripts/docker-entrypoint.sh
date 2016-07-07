1e3a01d285ef14bdc3961515ee8d96156c9ad774
Felix Erkinger <felix@erkinger.at>
2016-07-07 14:13:55 +0200

    cleanup docker-entrypoint to use case and [[ ~= constructs

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-set -e
-echo "docker-entrypoint.sh called with:\"$@\""
 
-realpath=`dirname $(readlink -e "$0")`
-. $realpath/database.include
-
-if test -z "$1"; then
+usage(){
     cat << EOF
 Usage: "$0 cmd"
 
     web         = supervisor controlled web service (nginx + uWSGI)
-    combined    = combined web,worker,beat,smtpd container
     simple      = as "web", but no automatic migrations
+    combined    = combined web,worker,beat,smtpd container
     worker      = celery worker daemon
     beat        = celery beat daemon
     smtpd       = smtp daemon
 
 one time commands:
     migrate     = run database migration
-    run \$@     = \$@ as user app in workdir with activated python environment
-    \$@         = \$@ executed as typed as user root
+    run \$@     = execute \$@ as user app in workdir with activated python environment
+    \$@         = execute \$@ as user root
 
 Environment:
     ECS_MIGRATE_AUTO = true
@@ -33,85 +28,96 @@ Examples:
 
 EOF
     exit 1
+}
+
+
+set -e
+echo "docker-entrypoint.sh called with:\"$@\""
+realpath=`dirname $(readlink -e "$0")`
+. $realpath/database.include
+
+if test -z "$1"; then
+    usage
 fi
 
-
-if [[ "web combined simple worker beat smtpd migrate run _prepare _user" =~ $1 ]]; then
-    cmd=$1
-    shift
-
-    if test "$cmd" = "web"; then
-        # call @app user, _prepare
-        gosu app $0 _user _prepare
-        echo "execute supervisor (web)"
-        exec /usr/bin/supervisord -c /app/supervisord-web.conf
-
-    elif test "$cmd" = "combined"; then
-        # call @app user, _prepare
-        gosu app $0 _user _prepare
-        echo "execute supervisor (combined)"
-        exec /usr/bin/supervisord -c /app/supervisord-combined.conf
-
-    elif test "$cmd" = "simple"; then
-        echo "execute supervisor (web)"
-        exec /usr/bin/supervisord -c /app/supervisord-web.conf
-
-    elif [[ "worker beat smtpd migrate run _prepare" =~ $cmd ]]; then
-        # needs running as app user
-        exec gosu app $0 _user $cmd "$@"
-
-    elif test "$cmd" = "_user"; then
-        cmd=$1
-        shift
-
-        echo "activate environment"
-        cd /app/ecs
-        . /app/env/bin/activate
-
-        if test ! -f ./ecs/version.py; then
-            echo "no version.py found, trying to create a version.py from git-rev/branch"
-            ./scripts/create-version-file.sh ./ ./ecs/version.py
-        fi
-
-        if test "$cmd" = "worker"; then
-            echo "execute celery worker"
-            exec /app/env/bin/celery --events -A ecs worker -l warning
-
-        elif test "$cmd" = "beat"; then
-            echo "execute celery beat"
-            exec /app/env/bin/celery -A ecs beat -l warning
-
-        elif test "$cmd" = "smtpd"; then
-            echo "execute smtpd"
-            exec /app/ecs/manage.py smtpd
-
-        elif test "$cmd" = "migrate"; then
-            prepare_database
-
-        elif test "$cmd" = "run"; then
-            exec "$@"
-
-        elif test "$cmd" = "_prepare"; then
-            # get number of proctype container we are running"
-            dyno_clean=$(printf "%s" "$DYNO" | tr -d \'\")
-            dyno_count="${dyno_clean##*.}"
-
-            if test -z "$dyno_count" -o "$dyno_count" = "1"; then
-                if test "$ECS_MIGRATE_AUTO" = "true"; then
-                    # prepare_database makes bootstrap as last step
-                    prepare_database
-                else
-                    echo "manage.py bootstrap"
-                    ./manage.py bootstrap
-                fi
-            else
-                if test "$ECS_MIGRATE_AUTO" = "true"; then
-                    echo "container number $dyno_count >1, skipping automatic migration"
-                fi
-                echo "container number $dyno_count >1, skipping manage.py bootstrap"
-            fi
-        fi
-    fi
-else
+if [[ ! "$1" =~ ^(web|combined|simple|worker|beat|smtpd|migrate|run|_prepare|_user)$ ]]; then
     exec "$@"
 fi
+
+cmd=$1
+shift
+
+if [[ "$cmd" =~ ^(worker|beat|smtpd|migrate|run|_prepare)$ ]]; then
+    # needs running as app user
+    exec gosu app $0 _user $cmd "$@"
+fi
+
+case "$cmd" in
+web)
+    gosu app $0 _user _prepare
+    echo "execute supervisor (web)"
+    exec /usr/bin/supervisord -c /app/supervisord-web.conf
+    ;;
+combined)
+    gosu app $0 _user _prepare
+    echo "execute supervisor (combined)"
+    exec /usr/bin/supervisord -c /app/supervisord-combined.conf
+    ;;
+simple)
+    echo "execute supervisor (web)"
+    exec /usr/bin/supervisord -c /app/supervisord-web.conf
+    ;;
+_user)
+    sub=$1
+    shift
+
+    echo "activate environment"
+    cd /app/ecs
+    . /app/env/bin/activate
+
+    if test ! -f ./ecs/version.py; then
+        echo "no version.py found, trying to create a version.py from git-rev/branch"
+        ./scripts/create-version-file.sh ./ ./ecs/version.py
+    fi
+
+    case "$sub" in
+    worker)
+        echo "execute celery worker"
+        exec /app/env/bin/celery --events -A ecs worker -l warning
+        ;;
+    beat)
+        echo "execute celery beat"
+        exec /app/env/bin/celery -A ecs beat -l warning
+        ;;
+    smtpd)
+        echo "execute smtpd"
+        exec /app/ecs/manage.py smtpd
+        ;;
+    migrate)
+        prepare_database
+        ;;
+    run)
+        exec "$@"
+        ;;
+    _prepare)
+        # get number of proctype container we are running"
+        dyno_clean=$(printf "%s" "$DYNO" | tr -d \'\")
+        dyno_count="${dyno_clean##*.}"
+
+        if test -z "$dyno_count" -o "$dyno_count" = "1"; then
+            if test "$ECS_MIGRATE_AUTO" = "true"; then
+                # prepare_database makes bootstrap as last step
+                prepare_database
+            else
+                echo "manage.py bootstrap"
+                ./manage.py bootstrap
+            fi
+        else
+            if test "$ECS_MIGRATE_AUTO" = "true"; then
+                echo "container number $dyno_count >1, skipping automatic migration"
+            fi
+            echo "container number $dyno_count >1, skipping manage.py bootstrap"
+        fi
+        ;;
+    esac
+esac
