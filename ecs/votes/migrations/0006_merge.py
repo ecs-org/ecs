47149551c4b7f55a7716ba42618ef594b0828e5c
Martin Natano <natano@natano.net>
2016-07-20 10:54:46 +0200

    merge migrations

---
+++ ecs/votes/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('votes', '0005_remove_vote_insurance_review_required'),
+        ('votes', '0005_auto_20160712_1725'),
+    ]
+
+    operations = [
+    ]
