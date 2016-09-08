dcfd1d45ced3428838c6daf5eb9c255250ad703a
Martin Natano <natano@natano.net>
2016-09-08 14:40:46 +0200

    Remove the import_ecx and export_ecx commands.

--- ecs/core/management/commands/import_ecx.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from ecs.core.serializer import Serializer
-
-class Command(BaseCommand):
-    def handle(self, file, **options):
-        serializer = Serializer()
-        with open(file, 'rb') as f:
-            serializer.read(f)
