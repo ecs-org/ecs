9f64620fee335328258e5993cfa0fe776be14db9
Martin Natano <natano@natano.net>
2016-10-03 15:33:44 +0200

    Merge migrations.

---
+++ ecs/core/migrations/0038_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0037_auto_20160926_2319'),
+        ('core', '0035_advancedsettings_require_internal_vote_review'),
+    ]
+
+    operations = [
+    ]
