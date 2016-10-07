0ef0bddbcf175af1dacae1386a4a618afb79752a
Martin Natano <natano@natano.net>
2016-10-07 12:01:30 +0200

    Move vote reminder messages to templates.

--- ecs/votes/tasks.py
+++ ecs/votes/tasks.py
@@ -10,130 +10,59 @@ from celery.schedules import crontab
 from ecs.votes.models import Vote
 from ecs.core.models.constants import SUBMISSION_LANE_LOCALEC
 from ecs.users.utils import get_user, get_office_user
-from ecs.communication.utils import send_message
+from ecs.communication.utils import send_message_template
 from ecs.votes.constants import PERMANENT_VOTE_RESULTS
 
 
-def send_submission_message(submission, subject, text, recipients):
+def send_vote_reminder(vote, subject, template, recipients):
     sender = get_user('root@system.local')
+    submission = vote.get_submission()
+    subject = subject.format(ec_number=submission.get_ec_number_display())
+    template = 'votes/messages/{}'.format(template)
     for recipient in recipients:
-        send_message(sender, recipient, subject, text, submission=submission)
+        send_message_template(sender, recipient, subject, template, {
+            'vote': vote,
+            'submission': submission,
+        }, submission=submission)
 
 
 def send_vote_expired(vote):
     sf = vote.get_submission().current_submission_form
     recipients = sf.get_presenting_parties().get_users()
     recipients.add(get_office_user())
-    submission = vote.get_submission()
 
-    if vote.top:
-        vote_date = vote.top.meeting.start
-    else:
-        vote_date = vote.published_at
-    text = _('Das Votum für die Studie EK-Nr. %(ec_number)s vom %(vote_date)s ist abgelaufen.\n') % {
-        'ec_number': submission.get_ec_number_display(),
-        'vote_date': vote_date.strftime('%d.%m.%Y'),
-    }
+    subject = _('Vote for Submission {ec_number} has expired')
+    send_vote_reminder(vote, subject, 'expired.txt', recipients)
 
-    subject = _('Vote for Submission {ec_number} has expired').format(
-        ec_number=submission.get_ec_number_display())
-    send_submission_message(submission, subject, text, recipients)
 
 def send_vote_reminder_submitter(vote):
     sf = vote.get_submission().current_submission_form
     recipients = sf.get_presenting_parties().get_users()
-    submission = vote.get_submission()
 
-    if vote.top:
-        vote_date = vote.top.meeting.start
-    else:
-        vote_date = vote.published_at
-    text = _('''Das Votum für die Studie EK-Nr. %(ec_number)s vom %(vote_date)s läuft in drei Wochen ab.
-Stellen Sie bitte zeitgerecht den Antrag auf Verlängerung des Votums.
-Steigen Sie dazu ins ECS ein, gehen Sie im Seitenmenü rechts auf „Studien Meldungen“ > „Neue Meldung“,
-wählen Sie dann „Verlängerung der Gültigkeit des Votums“ und machen Sie die erforderlichen Angaben.
+    subject = _('Vote for Submission {ec_number} will expire in three weeks')
+    send_vote_reminder(vote, subject, 'reminder_submitter.txt', recipients)
 
-Achtung: Sollte es sich bei Ihrer Studie um eine multizentrische Arzneimittelprüfung handeln,
-bei der die Ethikkommission nicht als Leit-Ethikkommission, sondern als lokale Ethikkommission
-fungiert, dann können Sie diese Aufforderung ignorieren. In solchen Fällen ist die
-Leit-Ethikkommission für die Votumsverlängerung zuständig.
-
-Mit freundlichen Grüßen,
-
-das Team der Ethik-Kommission
-    ''') % {
-        'ec_number': submission.get_ec_number_display(),
-        'vote_date': vote_date.strftime('%d.%m.%Y'),
-    }
-
-    subject = _('Vote for Submission {ec_number} will expire in three weeks').format(
-        ec_number=submission.get_ec_number_display())
-    send_submission_message(submission, subject, text, recipients)
     
 def send_vote_reminder_office(vote):
     recipients = [get_office_user()]
-    submission = vote.get_submission()
 
-    if vote.top:
-        vote_date = vote.top.meeting.start
-    else:
-        vote_date = vote.published_at
-    text = _('Das Votum für die Studie EK-Nr. %(ec_number)s vom %(vote_date)s läuft in einer Woche ab.\n') % {
-        'ec_number': submission.get_ec_number_display(),
-        'vote_date': vote_date.strftime('%d.%m.%Y'),
-    }
-
-    subject = _('Vote for Submission {ec_number} will expire in one week').format(
-        ec_number=submission.get_ec_number_display())
-    send_submission_message(submission, subject, text, recipients)
+    subject = _('Vote for Submission {ec_number} will expire in one week')
+    send_vote_reminder(vote, subject, 'reminder_office.txt', recipients)
 
 
 def send_temporary_vote_reminder_submitter(vote):
     sf = vote.get_submission().current_submission_form
     recipients = sf.get_presenting_parties().get_users()
-    submission = vote.get_submission()
 
-    if vote.top:
-        vote_date = vote.top.meeting.start
-    else:
-        vote_date = vote.published_at
-
-    text = _('''There is still no permanent vote for submission {ec_number}. The current
-temporary vote was published on the {vote_date}. Please make sure you've
-sent all necessary documents to the ethics commission.
-
-Yours sincerely,
-
-the team of the ethics commission
-    ''').format(
-        ec_number=submission.get_ec_number_display(),
-        vote_date=vote_date.strftime('%d.%m.%Y')
-    )
-
-    subject = _('Temporary vote for Submission {ec_number}').format(
-        ec_number=submission.get_ec_number_display())
-    send_submission_message(submission, subject, text, recipients)
+    subject = _('Temporary vote for Submission {ec_number}')
+    send_vote_reminder(vote, subject, 'temporary_reminder_submitter.txt', recipients)
 
 
 def send_temporary_vote_reminder_office(vote):
     recipients = [get_office_user()]
-    submission = vote.get_submission()
 
-    if vote.top:
-        vote_date = vote.top.meeting.start
-    else:
-        vote_date = vote.published_at
-
-    text = _('''There is still no permanent vote for submission {ec_number}. The current
-temporary vote was published on the {vote_date}.
-    ''').format(
-        ec_number=submission.get_ec_number_display(),
-        vote_date=vote_date.strftime('%d.%m.%Y')
-    )
-
-    subject = _('Temporary vote for Submission {ec_number}').format(
-        ec_number=submission.get_ec_number_display())
-    send_submission_message(submission, subject, text, recipients)
+    subject = _('Temporary vote for Submission {ec_number}')
+    send_vote_reminder(vote, subject, 'temporary_reminder_office.txt', recipients)
 
 
 @periodic_task(run_every=timedelta(days=1))
