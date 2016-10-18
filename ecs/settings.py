ae5c5e6a279c123e1ef0fa568c0dd699ecc292d7
Martin Natano <natano@natano.net>
2016-10-18 14:49:05 +0200

    Send agenda and protocol to omniscient board members.

--- ecs/settings.py
+++ ecs/settings.py
@@ -241,8 +241,13 @@ ECS_TRACKING_ENABLED = False
 ETHICS_COMMISSION_UUID = '23d805c6b5f14d8b9196a12005fd2961'
 
 # users in these groups receive messages even when they are not related to studies
-ECS_MEETING_AGENDA_RECEIVER_GROUPS = ('Resident Board Member',)
-ECS_MEETING_PROTOCOL_RECEIVER_GROUPS = ('Meeting Protocol Receiver', 'Resident Board Member')
+ECS_MEETING_AGENDA_RECEIVER_GROUPS = (
+    'Resident Board Member', 'Omniscient Board Member',
+)
+ECS_MEETING_PROTOCOL_RECEIVER_GROUPS = (
+    'Meeting Protocol Receiver', 'Resident Board Member',
+    'Omniscient Board Member',
+)
 
 ECS_AMG_MPG_VOTE_RECEIVERS = ('BASG.EKVoten@ages.at',)
 
