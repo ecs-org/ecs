7bdb1459b9eb9f924ad1ef2b0dc592b81c8cbdc4
Martin Natano <natano@natano.net>
2016-10-19 17:55:37 +0200

    merge migrations

---
+++ ecs/docstash/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('docstash', '0011_fix_categorization'),
+        ('docstash', '0012_remove_checklist_docstashes'),
+    ]
+
+    operations = [
+    ]
