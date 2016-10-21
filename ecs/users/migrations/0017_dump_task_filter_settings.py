faad672b11724eb719f0d9c80319906a861bf1fc
Martin Natano <natano@natano.net>
2016-10-21 17:00:23 +0200

    Throw away task filters from user settings.
    There is no clean way to migrate the old filters to new ones.

---
+++ ecs/users/migrations/0017_dump_task_filter_settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0016_remove_userprofile_is_insurance_reviewer'),
+    ]
+
+    operations = [
+        migrations.RunSQL('''
+            update users_usersettings set task_filter = null;
+        '''),
+    ]
