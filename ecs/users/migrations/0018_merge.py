163f60ac37ab97c4ae4ec37c5bf8466068dd9a49
Martin Natano <natano@natano.net>
2016-11-02 09:56:53 +0100

    Merge migrations.

---
+++ ecs/users/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0017_clear_communication_filter'),
+        ('users', '0017_dump_task_filter_settings'),
+    ]
+
+    operations = [
+    ]
