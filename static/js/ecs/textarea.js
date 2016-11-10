fcb9247e29b2a8ac81a288c9685d6707a4177260
Martin Natano <natano@natano.net>
2016-11-10 17:17:22 +0100

    Remove fullscreen editor from autogrowing fields.

--- static/js/ecs/textarea.js
+++ static/js/ecs/textarea.js
@@ -1,45 +1,9 @@
 ecs.textarea = {
-    fullscreenEditor: {
-        textarea: null,
-        screen: null,
-        sourceTextarea: null,
-        toolbar: null,
-        show: function(textarea) {
-            if (!this.screen) {
-                this.screen = $('<div/>', {'class': 'ecs-FullscreenEditor'});
-                this.screen.css('display', 'none');
-                this.textarea = $('<textarea/>');
-                this.screen.append(this.textarea);
-                this.textarea.keydown((function(ev){
-                    if (ev.altKey && ev.key == 'Enter') {
-                        ev.preventDefault();
-                        this.sourceTextarea.val(this.textarea.val());
-                        this.sourceTextarea.prop('selectionStart',
-                            this.textarea.prop('selectionStart'));
-                        this.sourceTextarea.prop('selectionEnd',
-                            this.textarea.prop('selectionEnd'));
-                        this.screen.css('display', 'none');
-                        this.sourceTextarea.focus();
-                    }
-                }).bind(this));
-                $(document.body).append(this.screen);
-            }
-            if (this.toolbar)
-                this.toolbar.remove();
-            this.sourceTextarea = textarea;
-            this.textarea.val(textarea.val());
-            this.textarea.prop('selectionStart', textarea.prop('selectionStart'));
-            this.textarea.prop('selectionEnd', textarea.prop('selectionEnd'));
-            this.toolbar = ecs.textarea.installToolbar(textarea, null, this.textarea);
-            this.screen.css('display', 'block');
-            this.textarea.focus();
-        }
+    installToolbar: function(el, items) {
+        return $(el).data('textarea').installToolbar(items);
     },
-    installToolbar: function(el, items, target) {
-        return $(el).data('textarea').installToolbar(items, target);
-    },
-    installToolbarItem: function(el, item, target) {
-        return $(el).data('textarea').installToolbarItem(item, target);
+    installToolbarItem: function(el, item) {
+        return $(el).data('textarea').installToolbarItem(item);
     }
 };
 
@@ -49,12 +13,6 @@ ecs.textarea.TextArea = function(textarea, items, options) {
     this.options = $.extend({
         update_height: true
     }, options);
-    this.textarea.on('keydown', (function(ev) {
-        if (ev.altKey && ev.key == 'Enter') {
-            ev.preventDefault();
-            ecs.textarea.fullscreenEditor.show(this.textarea);
-        }
-    }).bind(this));
     this.textarea.data('textarea', this);
     if (items)
         this.installToolbar(items);
@@ -74,13 +32,11 @@ ecs.textarea.TextArea.prototype = {
     updateHeight: function() {
         this.textarea.height(0).height(this.textarea.prop('scrollHeight'));
     },
-    installToolbarItem: function(item, ta) {
-        ta = ta || this.textarea;
+    installToolbarItem: function(item) {
         this.toolbar.push(item);
-        this.toolbarElement.append(item(ta));
+        this.toolbarElement.append(item(this.textarea));
     },
-    installToolbar: function(items, ta) {
-        ta = ta || this.textarea;
+    installToolbar: function(items) {
         items = items || this.toolbar;
         if (!items)
             return null;
@@ -88,9 +44,9 @@ ecs.textarea.TextArea.prototype = {
         this.toolbar = [];
         var toolbar = this.toolbarElement = $('<div/>', {'class': 'ecs-TextAreaToolbar'});
         items.forEach(function(item){
-            this.installToolbarItem(item, ta);
+            this.installToolbarItem(item);
         }, this);
-        ta.before(toolbar);
+        this.textarea.before(toolbar);
         return toolbar;
     }
 };
