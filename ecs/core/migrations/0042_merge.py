8d27cb0c1363316b134a939d77364428ad597e08
Martin Natano <natano@natano.net>
2016-11-16 12:36:32 +0100

    merge migrations

---
+++ ecs/core/migrations/0042_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0041_remove_admin'),
+        ('core', '0041_remove_help'),
+    ]
+
+    operations = [
+    ]
---
+++ ecs/meetings/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('meetings', '0007_auto_20161025_1740'),
+        ('meetings', '0007_meeting_expert_assignment_user'),
+    ]
+
+    operations = [
+    ]
