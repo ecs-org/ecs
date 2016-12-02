543d8fc6492cc968211a8cf29d62001533c9b08b
Martin Natano <natano@natano.net>
2016-12-02 13:37:58 +0100

    whitespace; no functional change

--- ecs/auth_conf.py
+++ ecs/auth_conf.py
@@ -20,14 +20,24 @@ class SubmissionQFactory(authorization.QFactory):
             return self.make_q()
 
         q = self.make_q(id__in=
-            MySubmission.objects.filter(user_id=user.id).values('submission_id'))
+            MySubmission.objects.filter(user_id=user.id)
+                .values('submission_id')
+        )
         
         ### explicit temporary permissions
-        q |= self.make_q(id__in=TemporaryAuthorization.objects.active(user=user).values('submission_id').query)
+        q |= self.make_q(id__in=
+            TemporaryAuthorization.objects.active(user=user)
+                .values('submission_id')
+        )
 
         ### permissions until final vote is published
-        until_vote_q = self.make_q(pk__in=Checklist.objects.values('submission__pk').query)
-        until_vote_q |= self.make_q(pk__in=Task.objects.filter(content_type=ContentType.objects.get_for_model(Submission)).values('data_id').query)
+        until_vote_q = self.make_q(
+            pk__in=Checklist.objects.values('submission__pk'))
+        until_vote_q |= self.make_q(pk__in=
+            Task.objects.filter(
+                content_type=ContentType.objects.get_for_model(Submission),
+            ).values('data_id')
+        )
         q |= until_vote_q & ~self.make_q(current_published_vote__result__in=PERMANENT_VOTE_RESULTS)
 
         notification_cts = map(ContentType.objects.get_for_model,
@@ -49,7 +59,9 @@ authorization.register(ParticipatingCenterNonSubject, lookup='submission_form__s
 
 class VoteQFactory(authorization.QFactory):
     def get_q(self, user):
-        q = self.make_q(submission_form__submission__pk__in=Submission.objects.values('pk').query)
+        q = self.make_q(
+            submission_form__submission__pk__in=Submission.objects.values('pk')
+        )
         if not user.profile.is_internal:
             q &= self.make_q(published_at__isnull=False)
         return q
@@ -79,19 +91,25 @@ authorization.register(Task, factory=TaskQFactory)
 
 class NotificationQFactory(authorization.QFactory):
     def get_q(self, user):
-        return self.make_q(submission_forms__submission__in=Submission.objects.values('pk').query)
+        return self.make_q(
+            submission_forms__submission__in=Submission.objects.values('pk')
+        )
 
 for cls in NOTIFICATION_MODELS:
     authorization.register(cls, factory=NotificationQFactory)
 
 class NotificationAnswerQFactory(authorization.QFactory):
     def get_q(self, user):
-        q = self.make_q(notification__in=Notification.objects.values('pk').query)
+        q = self.make_q(notification__in=Notification.objects.values('pk'))
         if not user.profile.is_internal:
             q &= self.make_q(published_at__isnull=False)
         for cls in NOTIFICATION_MODELS:
             ct = ContentType.objects.get_for_model(cls)
-            q |= self.make_q(notification__pk__in=Notification.objects.filter(pk__in=Task.objects.filter(content_type=ct).values('data_id').query).values('pk').query)
+            q |= self.make_q(notification__pk__in=
+                Notification.objects.filter(
+                    pk__in=Task.objects.filter(content_type=ct).values('data_id')
+                ).values('pk')
+            )
         return q
 
 authorization.register(NotificationAnswer, factory=NotificationAnswerQFactory)
@@ -101,7 +119,11 @@ class ChecklistQFactory(authorization.QFactory):
         if user.profile.is_internal:
             return self.make_q()
 
-        involved_by_task = self.make_q(submission__pk__in=Task.objects.filter(content_type=ContentType.objects.get_for_model(Submission)).values('data_id').query)
+        involved_by_task = self.make_q(submission__pk__in=
+            Task.objects.filter(
+                content_type=ContentType.objects.get_for_model(Submission),
+            ).values('data_id')
+        )
         involved_as_presenting_party = (
             self.make_q(submission__current_submission_form__sponsor=user) |
             self.make_q(submission__current_submission_form__submitter=user) |
@@ -110,7 +132,10 @@ class ChecklistQFactory(authorization.QFactory):
             self.make_q(submission__susar_presenter=user)
         )
 
-        temporary_authorized = self.make_q(submission__in=TemporaryAuthorization.objects.active(user=user).values('submission_id').query)
+        temporary_authorized = self.make_q(submission__in=
+            TemporaryAuthorization.objects.active(user=user)
+                .values('submission_id')
+        )
 
         q = self.make_q(last_edited_by=user)
         q |= (involved_by_task | temporary_authorized) & ~involved_as_presenting_party & self.make_q(status__in=['completed', 'review_ok'])
