b2be1c6db41a9f5c03e7acf6a04046dee03c15c0
Felix Erkinger <felix@erkinger.at>
2020-06-24 16:55:14 +0200

    reformat (non functional change) commissions list

--- ecs/core/bootstrap_settings.py
+++ ecs/core/bootstrap_settings.py
@@ -8,97 +8,129 @@
 # 2020-05-31 changed representation of uuid: e4dcd05a31ad475ca72dea7b84ef030e to
 #            Ethikkommission der Medizinischen Fakultät der JKU
 
-commissions = [{
-    'uuid': '23d805c6b5f14d8b9196a12005fd2961',
-    'name': 'Ethikkommission der Medizinischen Universit\u00e4t Wien',
-}, {
-    'uuid': '7b51f38bde8a4161a0dc34647fc7e654',
-    'name': 'Ethikkommission Krankenhaus Barmh.Br\u00fcder - Wien',
-}, {
-    'uuid': '85dc386061584fbe8549ce4e4d828fbd',
-    'name': 'Ethikkommission der Stadt Wien gem\u00e4\u00df KAG, AMG und MPG',
-}, {
-    'uuid': 'd6a22c635a584521b107481ac18318f6',
-    'name': 'Ethikkommission Forschungsinstitut des Wiener Roten Kreuzes',
-}, {
-    'uuid': '55ae93ec9df04d6abfc8d233ec5ccf8e',
-    'name': 'Nicht verwenden: Ethikkommission Krankenhaus Barmh.Schwestern - Wien',
-}, {
-    'uuid': '7cd6d52120b3474ba502931b9f60a5f3',
-    'name': 'Ethikkommission Confraternit\u00e4t-Priv.Klin. Josefstadt und Priv.Klin. D\u00f6bling',
-}, {
-    'uuid': '7df9ebaf15434709b09c3def9a6c8769',
-    'name': 'Ethikkommission St.Anna Kinderspital',
-}, {
-    'uuid': 'f122f144616541d391fde2dcc761aff4',
-    'name': 'Ethikkommission \u00d6sterreichische Arbeitsgemeinschaft f\u00fcr Klinische Pharmakologie',
-}, {
-    'uuid': '25b6744780434a3f96a1e43b405d3848',
-    'name': 'Ethikkommission Privatkrankenanstalt Rudolfinerhaus',
-}, {
-    'uuid': 'd542994ced34403db841786a1c1ab892',
-    'name': 'Ethikkommission Rheuma-Zentrum Wien-Oberlaa',
-}, {
-    'uuid': '5615dfbaf8c8445d960d1e2cd9c00dc3',
-    'name': 'Nicht verwenden: Ethikkommission Krankenhaus des G\u00f6ttlichen Heilandes',
-}, {
-    'uuid': '4d3a2d5f138940f293ee87fe6ec1d5b2',
-    'name': 'Ethikkommission Evangelisches Krankenhaus',
-}, {
-    'uuid': '8d2950e3a0294f68bde647a54df6d823',
-    'name': 'Ethikkommission der Allgemeinen Unfallversicherungsanstalt',
-}, {
-    'uuid': '9f6b509e716e413f865d95bdd630e9bc',
-    'name': 'Ethikkommission Immunologische Tagesklinik',
-}, {
-    'uuid': 'b17f32f604fa4452b5ff3a2baa9e0704',
-    'name': 'Ethikkommission des Landes Nieder\u00f6sterreich',
-}, {
-    'uuid': '6688ce16a3b84d42b1531389e6039891',
-    'name': 'Ethikkommission Krankenhaus Elisabethinen',
-}, {
-    'uuid': 'e4dcd05a31ad475ca72dea7b84ef030e',
-    'name': 'Ethikkommission der Medizinischen Fakult\u00e4t der JKU',
-}, {
-    'uuid': 'e269491bb9c040aaad6a5f11df343f38',
-    'name': 'Ethikkommission Krankenhaus Barmh.Br\u00fcder - Linz',
-}, {
-    'uuid': '1cca34032077445d95dabf7802fade28',
-    'name': 'Ethikkommission Krankenhaus Barmh.Schwestern - Linz',
-}, {
-    'uuid': '39cbb589ef044d27bceb6ee5ac796ae7',
-    'name': 'Ethikkommission f\u00fcr das Bundesland Salzburg',
-}, {
-    'uuid': '280414583b894c809a9baa8134d7fe4b',
-    'name': 'Ethikkommission der Medizinischen Universit\u00e4t Innsbruck',
-}, {
-    'uuid': '183881da8200493aa7edd8bebeea75b9',
-    'name': 'Ethikkommission des Landes Vorarlberg',
-}, {
-    'uuid': '95821eba88f34b2195f96e747d7f6b16',
-    'name': 'Ethikkommission des Landes Burgenland gem\u00e4\u00df KAG, AMG und MPG',
-}, {
-    'uuid': '6e7cfab5f8cd40df83c9de4fac9bb20f',
-    'name': 'Ethikkommission des Landes Steiermark gem\u00e4\u00df AMG und MPG',
-}, {
-    'uuid': '75b5a9714f354a5b842aa01029148036',
-    'name': 'Ethikkommission Krankenhaus Barmh.Br\u00fcder - Graz',
-}, {
-    'uuid': '9287fb0878b94f9f9d90b5582d1cefca', # XXX not in forum list anymore
-    'name': 'Ethikkommission Krankenhaus Barmh.Br\u00fcder - Eggenberg',
-}, {
-    'uuid': 'e17ee744e4e840778f316cf3f79de6b4',
-    'name': 'Ethikkommission der Medizinischen Universit\u00e4t Graz',
-}, {
-    'uuid': 'ec9f179a52a04a7aa52446232c3fd4bd',
-    'name': 'Ethikkommission des Landes K\u00e4rnten',
-}, {
-    'uuid': 'c890205dcb7543c8a76bf324512c5f81',
-    'name': 'Nicht verwenden: Ethikkommission des Krankenhaus St. Josef',
-}, {
-    'uuid': 'dc1b115d9809461ba3ea9450b079ddd6',
-    'name': 'Kommission für Scientific Integrity und Ethik der Karl Landsteiner Privatuniversität',
-}, {
-    'uuid': '50dba0126a0746dc8802e6c0e0199dad',
-    'name': 'Ethik-Kommission der Vinzenz Gruppe Wien',
-}]
+commissions = [
+    {
+        "uuid": "23d805c6b5f14d8b9196a12005fd2961",
+        "name": "Ethikkommission der Medizinischen Universit\u00e4t Wien",
+    },
+    {
+        "uuid": "7b51f38bde8a4161a0dc34647fc7e654",
+        "name": "Ethikkommission Krankenhaus Barmh.Br\u00fcder - Wien",
+    },
+    {
+        "uuid": "85dc386061584fbe8549ce4e4d828fbd",
+        "name": "Ethikkommission der Stadt Wien gem\u00e4\u00df KAG, AMG und MPG",
+    },
+    {
+        "uuid": "d6a22c635a584521b107481ac18318f6",
+        "name": "Ethikkommission Forschungsinstitut des Wiener Roten Kreuzes",
+    },
+    {
+        "uuid": "55ae93ec9df04d6abfc8d233ec5ccf8e",
+        "name": "Nicht verwenden: Ethikkommission Krankenhaus Barmh.Schwestern - Wien",
+    },
+    {
+        "uuid": "7cd6d52120b3474ba502931b9f60a5f3",
+        "name": "Ethikkommission Confraternit\u00e4t-Priv.Klin. Josefstadt und Priv.Klin. D\u00f6bling",
+    },
+    {
+        "uuid": "7df9ebaf15434709b09c3def9a6c8769",
+        "name": "Ethikkommission St.Anna Kinderspital",
+    },
+    {
+        "uuid": "f122f144616541d391fde2dcc761aff4",
+        "name": "Ethikkommission \u00d6sterreichische Arbeitsgemeinschaft f\u00fcr Klinische Pharmakologie",
+    },
+    {
+        "uuid": "25b6744780434a3f96a1e43b405d3848",
+        "name": "Ethikkommission Privatkrankenanstalt Rudolfinerhaus",
+    },
+    {
+        "uuid": "d542994ced34403db841786a1c1ab892",
+        "name": "Ethikkommission Rheuma-Zentrum Wien-Oberlaa",
+    },
+    {
+        "uuid": "5615dfbaf8c8445d960d1e2cd9c00dc3",
+        "name": "Nicht verwenden: Ethikkommission Krankenhaus des G\u00f6ttlichen Heilandes",
+    },
+    {
+        "uuid": "4d3a2d5f138940f293ee87fe6ec1d5b2",
+        "name": "Ethikkommission Evangelisches Krankenhaus",
+    },
+    {
+        "uuid": "8d2950e3a0294f68bde647a54df6d823",
+        "name": "Ethikkommission der Allgemeinen Unfallversicherungsanstalt",
+    },
+    {
+        "uuid": "9f6b509e716e413f865d95bdd630e9bc",
+        "name": "Ethikkommission Immunologische Tagesklinik",
+    },
+    {
+        "uuid": "b17f32f604fa4452b5ff3a2baa9e0704",
+        "name": "Ethikkommission des Landes Nieder\u00f6sterreich",
+    },
+    {
+        "uuid": "6688ce16a3b84d42b1531389e6039891",
+        "name": "Ethikkommission Krankenhaus Elisabethinen",
+    },
+    {
+        "uuid": "e4dcd05a31ad475ca72dea7b84ef030e",
+        "name": "Ethikkommission der Medizinischen Fakult\u00e4t der JKU",
+    },
+    {
+        "uuid": "e269491bb9c040aaad6a5f11df343f38",
+        "name": "Ethikkommission Krankenhaus Barmh.Br\u00fcder - Linz",
+    },
+    {
+        "uuid": "1cca34032077445d95dabf7802fade28",
+        "name": "Ethikkommission Krankenhaus Barmh.Schwestern - Linz",
+    },
+    {
+        "uuid": "39cbb589ef044d27bceb6ee5ac796ae7",
+        "name": "Ethikkommission f\u00fcr das Bundesland Salzburg",
+    },
+    {
+        "uuid": "280414583b894c809a9baa8134d7fe4b",
+        "name": "Ethikkommission der Medizinischen Universit\u00e4t Innsbruck",
+    },
+    {
+        "uuid": "183881da8200493aa7edd8bebeea75b9",
+        "name": "Ethikkommission des Landes Vorarlberg",
+    },
+    {
+        "uuid": "95821eba88f34b2195f96e747d7f6b16",
+        "name": "Ethikkommission des Landes Burgenland gem\u00e4\u00df KAG, AMG und MPG",
+    },
+    {
+        "uuid": "6e7cfab5f8cd40df83c9de4fac9bb20f",
+        "name": "Ethikkommission des Landes Steiermark gem\u00e4\u00df AMG und MPG",
+    },
+    {
+        "uuid": "75b5a9714f354a5b842aa01029148036",
+        "name": "Ethikkommission Krankenhaus Barmh.Br\u00fcder - Graz",
+    },
+    {
+        "uuid": "9287fb0878b94f9f9d90b5582d1cefca",  # XXX not in forum list anymore
+        "name": "Ethikkommission Krankenhaus Barmh.Br\u00fcder - Eggenberg",
+    },
+    {
+        "uuid": "e17ee744e4e840778f316cf3f79de6b4",
+        "name": "Ethikkommission der Medizinischen Universit\u00e4t Graz",
+    },
+    {
+        "uuid": "ec9f179a52a04a7aa52446232c3fd4bd",
+        "name": "Ethikkommission des Landes K\u00e4rnten",
+    },
+    {
+        "uuid": "c890205dcb7543c8a76bf324512c5f81",
+        "name": "Nicht verwenden: Ethikkommission des Krankenhaus St. Josef",
+    },
+    {
+        "uuid": "dc1b115d9809461ba3ea9450b079ddd6",
+        "name": "Kommission für Scientific Integrity und Ethik der Karl Landsteiner Privatuniversität",
+    },
+    {
+        "uuid": "50dba0126a0746dc8802e6c0e0199dad",
+        "name": "Ethik-Kommission der Vinzenz Gruppe Wien",
+    },
+]
