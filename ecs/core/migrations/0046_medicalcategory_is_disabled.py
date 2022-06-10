c0de2490459db7d5ccbef472671d3afed3aa23d1
halilbahar <halil.s.bahar@gmail.com>
2022-06-10 06:21:35 +0200

    Fix migration order after merging

--- ecs/core/migrations/0046_medicalcategory_is_disabled.py
+++ ecs/core/migrations/0046_medicalcategory_is_disabled.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0044_auto_20170203_1351'),
+        ('core', '0045_submissionform_medtech_is_new_law'),
     ]
 
     operations = [
