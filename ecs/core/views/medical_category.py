a2fa5fe2e92e0f47fd125fc1d05869694d6efc8c
halilbahar <halil.s.bahar@gmail.com>
2022-07-09 21:41:44 +0200

    Only EC-Executive can manage medical_category

--- ecs/core/views/medical_category.py
+++ ecs/core/views/medical_category.py
@@ -5,8 +5,7 @@ from ecs.core.models.core import MedicalCategory
 from ecs.core.models.medical_category import MedicalCategoryCreationForm
 from ecs.users.utils import user_group_required
 
-# @user_group_required('EC-Office', 'EC-Executive')
-@user_group_required('EC-Office')
+@user_group_required('EC-Executive')
 def administration(request):
     limit = 20
     page = request.GET.get('page', 1)
@@ -22,7 +21,7 @@ def administration(request):
         'medical_category': medical_category
     })
 
-@user_group_required('EC-Office')
+@user_group_required('EC-Executive')
 def create_medical_category(request):
     form = MedicalCategoryCreationForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():
@@ -41,7 +40,7 @@ def create_medical_category(request):
         'form': form
     })
 
-@user_group_required('EC-Office')
+@user_group_required('EC-Executive')
 def update_medical_category(request, pk):
     if request.method == 'POST' and request.POST:
         form = MedicalCategoryCreationForm(request.POST or None)
@@ -64,7 +63,7 @@ def update_medical_category(request, pk):
         'form': form
     })
 
-@user_group_required('EC-Office')
+@user_group_required('EC-Executive')
 def toggle_disabled(request, pk):
     medical_category = MedicalCategory.objects.filter(id=pk).first()
     if medical_category is not None:
