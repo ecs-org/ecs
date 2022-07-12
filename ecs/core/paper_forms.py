3068cc503d3e30e380704866cd0af07dcfe2c51f
halilbahar <halil.s.bahar@gmail.com>
2022-07-12 16:20:51 +0200

    Show help text for submission_type

--- ecs/core/paper_forms.py
+++ ecs/core/paper_forms.py
@@ -167,7 +167,7 @@ FormInfo(SubmissionForm, fields=(
     FieldInfo('2.1.16', 'project_type_non_interventional_study', _('Non-interventional Study (NIS)')),
     FieldInfo('2.1.17', 'project_type_gender_medicine', _('Gender medicine')),
     FieldInfo('2.2', 'specialism', _('special field')),
-    FieldInfo(None, 'submission_type', _('Submit as')),
+    FieldInfo(None, 'submission_type', _('Submit as'), help_text=_('submission_type_help')),
     # 2.3 Arzneimittelstudie (wenn zutreffend)
     FieldInfo('2.3.1', 'pharma_checked_substance', _('Test substances')),
     FieldInfo('2.3.2', 'pharma_reference_substance', _('Reference substance')),
